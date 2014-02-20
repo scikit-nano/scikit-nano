50ed2c03cd8405ccffd2ffc310df09654d7ece23
Andrew Merrill <androomerrill@gmail.com>
2014-02-20 08:59:38 -0500

    adding AtomAttributes dummpy class to __all__ list

--- sknano/chemistry/_atom.py
+++ sknano/chemistry/_atom.py
@@ -15,7 +15,7 @@ import numpy as np
 from pkshared.tools.refdata import atomic_masses, atomic_mass_symbol_map, \
     atomic_numbers, atomic_number_symbol_map, element_symbols
 
-__all__ = ['Atom']
+__all__ = ['Atom', 'AtomAttributes']
 
 
 class AtomAttributes(object):
