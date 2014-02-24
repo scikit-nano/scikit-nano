666d177307e2a09f3d49dcb76fdda08773aa0641
Andrew Merrill <androomerrill@gmail.com>
2014-02-24 11:28:20 -0500

    whitespace cleanup

--- sknano/tools/refdata/_constants.py
+++ sknano/tools/refdata/_constants.py
@@ -12,5 +12,4 @@ from __future__ import division, print_function, absolute_import
 C_C = CCbond = 1.421  # angstroms
 C_H = CHbond = 1.09  # angstroms
 
-__all__ = ['C_C', 'CCbond',
-           'C_H', 'CHbond']
+__all__ = ['C_C', 'CCbond', 'C_H', 'CHbond']
