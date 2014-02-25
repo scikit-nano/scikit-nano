c0543a38d016eea63e8e6e923ddde0e5c7c11229
Andrew Merrill <androomerrill@gmail.com>
2014-02-24 22:40:48 -0500

    starting new package for data analysis

---
+++ sknano/analysis/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+===================================================
+Data analysis sub-package (:mod:`sknano.analysis`)
+===================================================
+
+.. currentmodule:: sknano.analysis
+
+Contents
+========
+
+.. autosummary::
+   :toctree: generated/
+
+"""
+from __future__ import division, print_function, absolute_import
+
+__docformat__ = 'restructuredtext'
+
+__all__ = [s for s in dir() if not s.startswith('_')]
