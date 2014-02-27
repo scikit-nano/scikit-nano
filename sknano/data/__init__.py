b7b55e22cf2d18cc3e38fe49d03aa8e5173a515a
Andrew Merrill <androomerrill@gmail.com>
2014-02-27 17:11:53 -0500

    adding new subpackage to hold json encoded structure data

---
+++ sknano/data/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+===========================================
+Preset structure data (:mod:`sknano.data`)
+===========================================
+
+.. currentmodule:: sknano.data
+
+Contents
+========
+
+"""
+from __future__ import division, print_function, absolute_import
+__docformat__ = 'restructuredtext'
+
+__all__ = [s for s in dir() if not s.startswith('_')]
