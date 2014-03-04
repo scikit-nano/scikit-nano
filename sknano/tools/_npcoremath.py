5fe05270705b6f6d9ed81fa683078cef490f9e0b
Andrew Merrill <androomerrill@gmail.com>
2014-03-04 13:25:35 -0500

    adding new module to play with creating new data structures that subclass ndarray

---
+++ sknano/tools/_npcoremath.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+==================================================================
+Custom NumPy data structures (:mod:`sknano.tools._npcoremath`)
+==================================================================
+
+.. currentmodule:: sknano.tools._npcoremath
+
+"""
+from __future__ import division, print_function, absolute_import
+
+import numpy as np
+
+#from ._corefuncs import check_type
+
+__all__ = ['NPPoint'] #, 'NPVector', 'NPQuaternion']
+
+
+class NPPoint(np.ndarray):
+    """Create a point in :math:`R^3`
+
+    Parameters
+    ----------
+    x, y, z : float, optional
+        :math:`x, y, z` coordinates of point in :math:`R^3` space.
+    units : {None, str}, optional
+        Units of coordinates.
+
+    """
+    def __new__(cls, x=None, y=None, z=None, units=None):
+        self._p = np.zeros(3, dtype=float)
+        self._units = units
+        for i, pi in enumerate((x, y, z)):
+            if pi is not None:
+                self._p[i] = pi
+
