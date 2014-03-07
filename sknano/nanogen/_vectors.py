d1299f8df6b493f3d5cfea64dbfafe216505721c
Andrew Merrill <androomerrill@gmail.com>
2014-03-07 13:12:56 -0500

    adding new module to develop code for unit cell vector data structures

---
+++ sknano/nanogen/_vectors.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+=============================================================
+Unit cell vectors (:mod:`sknano.nanogen._vectors`)
+=============================================================
+
+.. currentmodule:: sknano.nanogen._vectors
+
+"""
+from __future__ import division, print_function, absolute_import
+__docformat__ = 'restructuredtext'
+
+__all__ = []
