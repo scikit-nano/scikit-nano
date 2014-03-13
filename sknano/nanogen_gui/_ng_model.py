aec0c652606bf4219fd2679576212722778d4e7a
Andrew Merrill <androomerrill@gmail.com>
2014-03-13 14:21:33 -0400

    replace setting attributes with setting variables of superclass

--- sknano/nanogen_gui/_ng_model.py
+++ sknano/nanogen_gui/_ng_model.py
@@ -20,7 +20,7 @@ class NGModel(NanotubeBundle):
     def __init__(self):
 
         self.observers = []
-        super(NGModel, self).__init__(n=10, m=10, verbose=True)
+        super(NGModel, self).__init__(n=10, m=10)
 
     def init(self):
         self.notify_observers()
@@ -32,49 +32,53 @@ class NGModel(NanotubeBundle):
     @property
     def n(self):
         """Chiral index :math:`n`"""
-        return self.n
+        return self._n
 
     @n.setter
     def n(self, value):
-        self.n = value
+        self._n = value
         self._compute_bundle_params()
+        #self.notify_observers()
 
     @property
     def m(self):
         """Chiral index :math:`m`"""
-        return self.m
+        return self._m
 
     @m.setter
     def m(self, value):
-        self.m = value
+        self._m = value
         self._compute_bundle_params()
+        #self.notify_observers()
 
     #@property
     #def bond(self):
-    #    return self.bond
+    #    return self._bond
 
     #@bond.setter
     #def bond(self, value):
-    #    self.bond = value
+    #    self._bond = value
     #    self._compute_bundle_params()
 
     @property
     def Lz(self):
-        return self.Lz
+        return self._Lz
 
     @Lz.setter
     def Lz(self, value):
-        self.Lz = value
+        self._Lz = value
         self._compute_bundle_params()
+        #self.notify_observers()
 
     @property
     def nz(self):
-        return self.nz
+        return self._nz
 
     @nz.setter
     def nz(self, value):
-        self.nz = value
+        self._nz = value
         self._compute_bundle_params()
+        #self.notify_observers()
 
     def register_observer(self, observer):
         self.observers.append(observer)
