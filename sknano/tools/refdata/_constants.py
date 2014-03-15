c81c141c398eadd665e528f7e5c4c3868a14ac05
Andrew Merrill <androomerrill@gmail.com>
2014-03-15 12:12:30 -0400

    adding constant for van der Walls distance

--- sknano/tools/refdata/_constants.py
+++ sknano/tools/refdata/_constants.py
@@ -11,5 +11,6 @@ from __future__ import division, print_function, absolute_import
 
 C_C = CCbond = 1.421  # angstroms
 C_H = CHbond = 1.09  # angstroms
+dVDW = 3.4  # angstroms
 
-__all__ = ['C_C', 'CCbond', 'C_H', 'CHbond']
+__all__ = ['C_C', 'CCbond', 'C_H', 'CHbond', 'dVDW']
