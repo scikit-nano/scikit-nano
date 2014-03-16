4f203b372038104c8994c80d14552acadec87398
Andrew Merrill <androomerrill@gmail.com>
2014-03-15 22:03:14 -0400

    updates and additions to UI and MVC design pattern

--- sknano/nanogen_gui/_ng_model.py
+++ sknano/nanogen_gui/_ng_model.py
@@ -10,88 +10,169 @@ NanoGen model (:mod:`sknano.nanogen_gui._ng_model`)
 from __future__ import absolute_import, division, print_function
 __docformat__ = 'restructuredtext en'
 
-from ..nanogen import NanotubeBundle
+from ..nanogen import NanotubeBundle, Graphene
 
 __all__ = ['NGModel']
 
 
-class NGModel(NanotubeBundle):
+class NGModel(object):
     """:mod:`~sknano.nanogen_gui` MVC model class."""
     def __init__(self):
         self._observers = []
-        super(NGModel, self).__init__(n=10, m=10)
+        self._swntbundle = NanotubeBundle(n=10, m=10)
+        self._graphene = Graphene(width=1, length=10, edge='AC')
 
     @property
     def n(self):
         """Chiral index :math:`n`"""
-        return self._n
+        return self._swntbundle.n
 
     @n.setter
     def n(self, value):
-        self._n = value
+        self._swntbundle.n = value
         self.notify_observers()
 
     @property
     def m(self):
         """Chiral index :math:`m`"""
-        return self._m
+        return self._swntbundle.m
 
     @m.setter
     def m(self, value):
-        self._m = value
+        self._swntbundle.m = value
         self.notify_observers()
 
     @property
-    def bond(self):
-        return self._bond
+    def nanotube_bond(self):
+        return self._swntbundle.bond
 
-    @bond.setter
-    def bond(self, value):
-        self._bond = value
+    @nanotube_bond.setter
+    def nanotube_bond(self, value):
+        self._swntbundle.bond = value
+        self.notify_observers()
+
+    @property
+    def nanotube_element1(self):
+        return self._swntbundle.element1
+
+    @nanotube_element1.setter
+    def nanotube_element1(self, value):
+        self._swntbundle.element1 = value
+        self.notify_observers()
+
+    @property
+    def nanotube_element2(self):
+        return self._swntbundle.element2
+
+    @nanotube_element2.setter
+    def nanotube_element2(self, value):
+        self._swntbundle.element2 = value
         self.notify_observers()
 
     @property
     def Lx(self):
-        return self._Lx
+        return self._swntbundle.Lx
 
     @property
     def Ly(self):
-        return self._Ly
+        return self._swntbundle.Ly
 
     @property
     def Lz(self):
-        return self._Lz
+        return self._swntbundle.Lz
 
     @Lz.setter
     def Lz(self, value):
-        self._Lz = value
+        self._swntbundle.Lz = value
         self.notify_observers()
 
     @property
     def nx(self):
-        return self._nx
+        return self._swntbundle.nx
 
     @nx.setter
     def nx(self, value):
-        self._nx = value
+        self._swntbundle.nx = value
         self.notify_observers()
 
     @property
     def ny(self):
-        return self._ny
+        return self._swntbundle.ny
 
     @ny.setter
     def ny(self, value):
-        self._ny = value
+        self._swntbundle.ny = value
         self.notify_observers()
 
     @property
     def nz(self):
-        return self._nz
+        return self._swntbundle.nz
 
     @nz.setter
     def nz(self, value):
-        self._nz = value
+        self._swntbundle.nz = value
+        self.notify_observers()
+
+    @property
+    def width(self):
+        return self._graphene.width
+
+    @width.setter
+    def width(self, value):
+        self._graphene.width = value
+        self.notify_observers()
+
+    @property
+    def length(self):
+        return self._graphene.length
+
+    @length.setter
+    def length(self, value):
+        self._graphene.length = value
+        self.notify_observers()
+
+    @property
+    def edge(self):
+        return self._graphene.edge
+
+    @edge.setter
+    def edge(self, value):
+        self._graphene.edge = value
+
+    @property
+    def nlayers(self):
+        return self._graphene.nlayers
+
+    @nlayers.setter
+    def nlayers(self, value):
+        self._graphene.nlayers = value
+        self.notify_observers()
+
+    @property
+    def graphene_bond(self):
+        return self._graphene.bond
+
+    @graphene_bond.setter
+    def graphene_bond(self, value):
+        self._graphene.bond = value
+        self.notify_observers()
+
+    @property
+    def graphene_element1(self):
+        return self._graphene.element1
+
+    @graphene_element1.setter
+    def graphene_element1(self, value):
+        self._graphene.element1 = value
+        self.notify_observers()
+
+    @property
+    def graphene_element2(self):
+        return self._graphene.element2
+
+    @graphene_element2.setter
+    def graphene_element2(self, value):
+        self._graphene.element2 = value
         self.notify_observers()
 
     def register_observer(self, observer):
@@ -101,6 +182,7 @@ class NGModel(NanotubeBundle):
         self._observers.remove(observer)
 
     def notify_observers(self):
-        super(NGModel, self).compute_bundle_params()
+        self._swntbundle.compute_bundle_params()
+        self._graphene.compute_layer_params()
         for observer in self._observers[:]:
             observer.update_app_view()
--- sknano/nanogen_gui/_ng_view.py
+++ sknano/nanogen_gui/_ng_view.py
@@ -41,21 +41,31 @@ class NGView(QMainWindow, Ui_NanoGen):
         super(NGView, self).__init__()
         self.setupUi(self)
 
+    # Nanotube Generator slots/signals
+
+    @pyqtSlot()
+    def on_n_spinBox_editingFinished(self):
+        self.model.n = self.n_spinBox.value()
+
     @pyqtSlot(int)
     def on_n_spinBox_valueChanged(self, value):
         self.model.n = value
+
+    @pyqtSlot()
+    def on_m_spinBox_editingFinished(self):
+        self.model.m = self.m_spinBox.value()
 
     @pyqtSlot(int)
     def on_m_spinBox_valueChanged(self, value):
         self.model.m = value
 
     @pyqtSlot()
-    def on_bond_doubleSpinBox_editingFinished(self):
-        self.model.bond = self.bond_doubleSpinBox.value()
+    def on_nanotube_bond_doubleSpinBox_editingFinished(self):
+        self.model.nanotube_bond = self.nanotube_bond_doubleSpinBox.value()
 
     @pyqtSlot(float)
-    def on_bond_doubleSpinBox_valueChanged(self, value):
-        self.model.bond = value
+    def on_nanotube_bond_doubleSpinBox_valueChanged(self, value):
+        self.model.nanotube_bond = value
 
     @pyqtSlot()
     def on_nx_spinBox_editingFinished(self):
@@ -90,6 +100,22 @@ class NGView(QMainWindow, Ui_NanoGen):
         else:
             self.bundle_generator_checkBox.setCheckable(True)
 
+    @pyqtSlot(str)
+    def on_nanotube_element1_comboBox_currentIndexChanged(self, value):
+        self.model.nanotube_element1 = str(value)
+
+    @pyqtSlot(str)
+    def on_nanotube_element2_comboBox_currentIndexChanged(self, value):
+        self.model.nanotube_element2 = str(value)
+
+    @pyqtSlot(str)
+    def on_graphene_element1_comboBox_currentIndexChanged(self, value):
+        self.model.graphene_element1 = str(value)
+
+    @pyqtSlot(str)
+    def on_graphene_element2_comboBox_currentIndexChanged(self, value):
+        self.model.graphene_element2 = str(value)
+
     @pyqtSlot()
     def on_Lz_doubleSpinBox_editingFinished(self):
         self.model.Lz = self.Lz_doubleSpinBox.value()
@@ -98,63 +124,151 @@ class NGView(QMainWindow, Ui_NanoGen):
     def on_Lz_doubleSpinBox_valueChanged(self, value):
         self.model.Lz = value
 
+    # Graphene Generator slots/signals
+
+    #@pyqtSlot(int)
+    #def on_stacking_order_buttonGroup_buttonClicked(self):
+    #    if self.nlayer_spinBox.value() == 1:
+    #        pass
+
+    @pyqtSlot()
+    def on_length_doubleSpinBox_editingFinished(self):
+        self.model.length = self.length_doubleSpinBox.value()
+
+    @pyqtSlot(float)
+    def on_length_doubleSpinBox_valueChanged(self, value):
+        self.model.length = value
+
+    @pyqtSlot()
+    def on_width_doubleSpinBox_editingFinished(self):
+        self.model.width = self.width_doubleSpinBox.value()
+
+    @pyqtSlot(float)
+    def on_width_doubleSpinBox_valueChanged(self, value):
+        self.model.width = value
+
+    @pyqtSlot()
+    def on_nlayers_spinBox_editingFinished(self):
+        self.model.nlayers = self.nlayers_spinBox.value()
+        #self.update_stacking_order_buttonGroup()
+
+    @pyqtSlot(int)
+    def on_nlayers_spinBox_valueChanged(self, value):
+        self.model.nlayers = value
+        #self.update_stacking_order_buttonGroup()
+
+    #def update_stacking_order_buttonGroup(self):
+    #    if self.nlayers_spinBox.value() == 1:
+    #        for rb in (self.AA_stacking_radioButton,
+    #                   self.AB_stacking_radioButton):
+    #            rb.setCheckable(False)
+    #    else:
+    #        for rb in (self.AA_stacking_radioButton,
+    #                   self.AB_stacking_radioButton):
+    #            rb.setCheckable(True)
+
+    @pyqtSlot()
+    def on_graphene_bond_doubleSpinBox_editingFinished(self):
+        self.model.graphene_bond = self.graphene_bond_doubleSpinBox.value()
+
+    @pyqtSlot(float)
+    def on_graphene_bond_doubleSpinBox_valueChanged(self, value):
+        self.model.graphene_bond = value
+
     @pyqtSlot()
     def on_generate_pushButton_clicked(self):
-        if self.nanotube_generator_radioButton.isChecked():
-            if self.bundle_generator_checkBox.isChecked():
-                generator = NanotubeBundleGenerator(
+        generator_tab = \
+            str(self.nanogen_tabWidget.tabText(
+                self.nanogen_tabWidget.currentIndex()))
+
+        if generator_tab == 'Nanotubes':
+            if self.nanotube_generator_radioButton.isChecked():
+                if self.bundle_generator_checkBox.isChecked():
+                    generator = NanotubeBundleGenerator(
+                        n=self.n_spinBox.value(),
+                        m=self.m_spinBox.value(),
+                        nx=self.nx_spinBox.value(),
+                        ny=self.ny_spinBox.value(),
+                        nz=self.nz_spinBox.value(),
+                        bond=self.nanotube_bond_doubleSpinBox.value())
+                else:
+                    generator = NanotubeGenerator(
+                        n=self.n_spinBox.value(),
+                        m=self.m_spinBox.value(),
+                        nz=self.nz_spinBox.value(),
+                        bond=self.nanotube_bond_doubleSpinBox.value())
+            elif self.mwnt_generator_radioButton.isChecked():
+                if self.bundle_generator_checkBox.isChecked():
+                    generator = MWNTBundleGenerator(
+                        n=self.n_spinBox.value(),
+                        m=self.m_spinBox.value(),
+                        nx=self.nx_spinBox.value(),
+                        ny=self.ny_spinBox.value(),
+                        nz=self.nz_spinBox.value(),
+                        bond=self.nanotube_bond_doubleSpinBox.value())
+                else:
+                    generator = MWNTGenerator(
+                        n=self.n_spinBox.value(),
+                        m=self.m_spinBox.value(),
+                        nz=self.nz_spinBox.value(),
+                        bond=self.nanotube_bond_doubleSpinBox.value())
+            elif self.unrolled_nanotube_generator_radioButton.isChecked():
+                generator = UnrolledNanotubeGenerator(
                     n=self.n_spinBox.value(),
                     m=self.m_spinBox.value(),
                     nx=self.nx_spinBox.value(),
-                    ny=self.ny_spinBox.value(),
                     nz=self.nz_spinBox.value(),
-                    bond=self.bond_doubleSpinBox.value())
-            else:
-                generator = NanotubeGenerator(
-                    n=self.n_spinBox.value(),
-                    m=self.m_spinBox.value(),
-                    nz=self.nz_spinBox.value(),
-                    bond=self.bond_doubleSpinBox.value())
-        elif self.mwnt_generator_radioButton.isChecked():
-            if self.bundle_generator_checkBox.isChecked():
-                generator = MWNTBundleGenerator(
-                    n=self.n_spinBox.value(),
-                    m=self.m_spinBox.value(),
-                    nx=self.nx_spinBox.value(),
-                    ny=self.ny_spinBox.value(),
-                    nz=self.nz_spinBox.value(),
-                    bond=self.bond_doubleSpinBox.value())
-            else:
-                generator = MWNTGenerator(
-                    n=self.n_spinBox.value(),
-                    m=self.m_spinBox.value(),
-                    nz=self.nz_spinBox.value(),
-                    bond=self.bond_doubleSpinBox.value())
-        elif self.unrolled_nanotube_generator_radioButton.isChecked():
-            generator = UnrolledNanotubeGenerator(
-                n=self.n_spinBox.value(),
-                m=self.m_spinBox.value(),
-                nx=self.nx_spinBox.value(),
-                nz=self.nz_spinBox.value(),
-                bond=self.bond_doubleSpinBox.value())
+                    bond=self.nanotube_bond_doubleSpinBox.value())
+        elif generator_tab == 'Graphene':
+            edge = 'AC'
+            if self.ZZ_edge_radioButton.isChecked():
+                edge = 'ZZ'
+            stacking_order = None
+            if self.AA_stacking_radioButton.isChecked():
+                stacking_order = 'AA'
+            elif self.AB_stacking_radioButton.isChecked():
+                stacking_order = 'AB'
+            generator = GrapheneGenerator(
+                width=self.width_doubleSpinBox.value(),
+                length=self.length_doubleSpinBox.value(),
+                edge=edge,
+                bond=self.graphene_bond_doubleSpinBox.value(),
+                nlayers=self.nlayers_spinBox.value(),
+                stacking_order=stacking_order)
 
         structure_format = \
             str(self.structure_format_comboBox.itemText(
                 self.structure_format_comboBox.currentIndex()))
         if structure_format.endswith('data'):
             structure_format = 'data'
+
         generator.save_data(structure_format=structure_format)
 
     def update_app_view(self):
         self.n_spinBox.setValue(self.model.n)
         self.m_spinBox.setValue(self.model.m)
-        self.bond_doubleSpinBox.setValue(self.model.bond)
+        self.nanotube_bond_doubleSpinBox.setValue(self.model.nanotube_bond)
         self.nx_spinBox.setValue(self.model.nx)
         self.ny_spinBox.setValue(self.model.ny)
         self.nz_spinBox.setValue(self.model.nz)
-        self.Lx_doubleSpinBox.setValue(self.model.Lx)
-        self.Ly_doubleSpinBox.setValue(self.model.Ly)
+        self.Lx_lineEdit.setText('{:.4f} nm'.format(self.model.Lx))
+        self.Ly_lineEdit.setText('{:.4f} nm'.format(self.model.Ly))
         self.Lz_doubleSpinBox.setValue(self.model.Lz)
+
+        self.width_doubleSpinBox.setValue(self.model.width)
+        self.length_doubleSpinBox.setValue(self.model.length)
+        self.nlayers_spinBox.setValue(self.model.nlayers)
+        self.graphene_bond_doubleSpinBox.setValue(self.model.graphene_bond)
+
+        nanotube_e1e2_bond_label_text = \
+            '-'.join((self.model.nanotube_element1,
+                      self.model.nanotube_element2)) + ' bond ='
+        self.nanotube_e1e2_bond_label.setText(nanotube_e1e2_bond_label_text)
+
+        graphene_e1e2_bond_label_text = \
+            '-'.join((self.model.graphene_element1,
+                      self.model.graphene_element2)) + ' bond ='
+        self.graphene_e1e2_bond_label.setText(graphene_e1e2_bond_label_text)
 
         #self.Ch_lineEdit.setText('{:.3f}'.format(self.model.Ch))
         #self.dt_lineEdit.setText('{:.3f}'.format(self.model.dt))
