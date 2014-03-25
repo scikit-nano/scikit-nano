e61f6f2f7f77001a09c08d4cd6965224eb2d9b29
Andrew Merrill <androomerrill@gmail.com>
2014-03-25 14:36:57 -0400

    adding imports for XYZAtom and XYZAtoms classes

--- sknano/structure_io/atoms/__init__.py
+++ sknano/structure_io/atoms/__init__.py
@@ -23,14 +23,18 @@ Contents
    Atoms
    LAMMPSAtom
    LAMMPSAtoms
+   XYZAtom
+   XYZAtoms
 
 """
-from __future__ import division, print_function, absolute_import
+from __future__ import absolute_import, division, print_function
 __docformat__ = 'restructuredtext en'
 
 from ._atom import *
 from ._atoms import *
 from ._lammps_atom import *
 from ._lammps_atoms import *
+from ._xyz_atom import *
+from ._xyz_atoms import *
 
 __all__ = [s for s in dir() if not s.startswith('_')]
