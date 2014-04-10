0f94716ccd834b5286e3f26df03f745c3390925a
Andrew Merrill <androomerrill@gmail.com>
2014-04-09 23:16:20 -0400

    adding new module of conversion factors

---
+++ sknano/tools/refdata/_conversion_factors.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+=====================================================================
+Conversion factors (:mod:`sknano.tools.refdata._conversion_factors`)
+=====================================================================
+
+.. currentmodule:: sknano.tools.refdata._conversion_factors
+
+"""
+from __future__ import absolute_import, division, print_function
+
+grams_per_amu = grams_per_Da = 1.6605e-24
+
+__all__ = ['grams_per_amu', 'grams_per_Da']
