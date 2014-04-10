b73871e57c1bfefa3797b059000ed5bf786a59f3
Andrew Merrill <androomerrill@gmail.com>
2014-04-09 23:15:55 -0400

    changing CCbond to 1.42 angstroms

--- sknano/tools/refdata/_constants.py
+++ sknano/tools/refdata/_constants.py
@@ -7,9 +7,9 @@ Reference data (:mod:`sknano.tools.refdata._constants`)
 .. currentmodule:: sknano.tools.refdata._constants
 
 """
-from __future__ import division, print_function, absolute_import
+from __future__ import absolute_import, division, print_function
 
-C_C = CCbond = 1.421  # angstroms
+C_C = CCbond = 1.42  # angstroms
 C_H = CHbond = 1.09  # angstroms
 dVDW = 3.4  # angstroms
 
