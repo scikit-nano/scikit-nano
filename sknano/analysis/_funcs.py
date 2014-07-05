f268cccf7304d9f45dc8251264fb1e5f8b42bae4
Andrew Merrill <androomerrill@gmail.com>
2014-07-05 18:02:29 -0400

    fix bug which caused error if search radius was too small

--- sknano/analysis/_funcs.py
+++ sknano/analysis/_funcs.py
@@ -14,7 +14,7 @@ import numpy as np
 __all__ = ['find_target_atom']
 
 
-def find_target_atom(atoms, target_coords=None, search_radius=float,
+def find_target_atom(atoms, target_coords=None, search_radius=2.5,
                      nearest_target=False):
     """Search for atom closest to target location.
 
@@ -31,20 +31,31 @@ def find_target_atom(atoms, target_coords=None, search_radius=float,
 
     Returns
     -------
-    :class:`~sknano.structure_io.atoms.Atom`
+    target_atom :class:`~sknano.structure_io.atoms.Atom`
         An :class:`~sknano.structure_io.atoms.Atom` instance.
 
     """
     atom_tree = atoms.atom_tree
-    target_index = None
-
-    if nearest_target:
-        target_index = \
-            atom_tree.query(target_coords,
-                            distance_upper_bound=search_radius)[-1]
-    else:
-        target_index = \
-            np.random.choice(
-                atom_tree.query_ball_point(target_coords, search_radius))
-
-    return atoms[target_index]
+    print('Starting search radius: {}'.format(search_radius))
+    target_atom = None
+    while True:
+        try:
+            target_index = None
+            if nearest_target:
+                target_index = \
+                    atom_tree.query(target_coords,
+                                    distance_upper_bound=search_radius)[-1]
+            else:
+                target_index = \
+                    np.random.choice(
+                        atom_tree.query_ball_point(target_coords,
+                                                   search_radius))
+            target_atom = atoms[target_index]
+        except (IndexError, ValueError):
+            search_radius += 1
+            errmsg = 'No target atom found within search radius.\n' + \
+                'Increasing search radius by 1 to {}'.format(search_radius)
+            print(errmsg)
+        else:
+            print('Found target atom.')
+            return target_atom
