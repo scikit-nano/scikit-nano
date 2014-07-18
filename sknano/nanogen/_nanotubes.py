005edc2f9838a61316fdd9f71bf48fb827690319
Andrew Merrill <androomerrill@gmail.com>
2014-07-18 02:38:58 -0400

    updating header docstring

--- sknano/nanogen/_nanotubes.py
+++ sknano/nanogen/_nanotubes.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-=============================================================
-Nanotube structure tools (:mod:`sknano.nanogen._nanotubes`)
-=============================================================
+==============================================================================
+Class representation of Nanotube structures (:mod:`sknano.nanogen._nanotubes`)
+==============================================================================
 
 .. currentmodule:: sknano.nanogen._nanotubes
 
