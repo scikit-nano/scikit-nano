eee2d1768111432acf27951b326e5931a1024cb5
Andrew Merrill <androomerrill@gmail.com>
2014-07-18 02:02:49 -0400

    adding some code to NanotubeStoneWalesDefectGenerator class

--- sknano/nanogen/_nanotube_defect_generators.py
+++ sknano/nanogen/_nanotube_defect_generators.py
@@ -26,7 +26,160 @@ __all__ = ['NanotubeStoneWalesDefectGenerator',
 
 
 class NanotubeStoneWalesDefectGenerator(StoneWalesDefectGenerator):
-    pass
+    """Generate Stone-Wales defects in nanotube structure data.
+
+    .. versionadded:: 0.2.26
+
+    Parameters
+    ----------
+    n, m : int, optional
+        Chiral indices defining the nanotube chiral vector
+        :math:`\\mathbf{C}_{h} = n\\mathbf{a}_{1} + m\\mathbf{a}_{2} = (n, m)`.
+    nx, ny, nz : int, optional
+        Number of repeat unit cells in the :math:`x, y, z` dimensions.
+    fname : str, optional
+        Structure data filename. If you don't provide a structure data file,
+        the you **must** provide the structure data parameters to
+        generate the structure data file.
+    structure_format : {None, str}, optional
+        Chemical file format of saved structure data.
+        If `None`, then guess based on `fname` file extension.
+        Otherwise, must be one of:
+
+            - xyz
+            - data
+
+    element1, element2 : {str, int}, optional
+        Element symbol or atomic number of basis atoms 1 and 2
+    bond : float, optional
+        :math:`\\mathrm{a}_{\\mathrm{CC}} =` distance between
+        nearest neighbor atoms. Must be in units of **Angstroms**.
+    vdw_spacing : float, optional
+        van der Waals distance between nearest neighbor tubes
+    bundle_packing : {None, 'hexagonal', 'cubic'}, optional
+        close packing arrangement of bundles
+    bundle_geometry : {None, 'triangle', 'hexagon', 'square', 'rectangle',
+                       'rhombus', 'rhomboid'}, optional
+    Lx, Ly, Lz : float, optional
+        length of bundle in :math:`x, y, z` dimensions in **nanometers**.
+        Overrides the :math:`n_x, n_y, n_z` cell values.
+    verbose : bool, optional
+        Verbose output
+
+    Raises
+    ------
+    `TypeError`
+        If `fname` is `None` and `n` and `m` are not integers.
+
+    Examples
+    --------
+
+    You can provide any existing structure data file in any supported
+    format or else provide structure parameters to generate a new structure
+    in real-time. The `NanotubeStoneWalesDefectGenerator` uses
+    the `NanotubeBundleGenerator` class to generate
+    nanotube structure data. See the structure generator classes provided
+    by the :mod:`~sknano.nanogen` module for more detailed docs.
+
+    In the next example, we'll generate a nanotube bundle and add
+    some SW defects to it.
+
+    >>> from sknano.nanogen import NanotubeStoneWalesDefectGenerator
+    >>> swntbundle = NanotubeStoneWalesDefectGenerator(
+    ...     n=10, m=5, Lz=10, fix_Lz=True, bundle_geometry='hexagon')
+
+    Notice that I used the `Lz` keyword argument to specify the
+    **length** of the nanotube bundle in **nanometers**, and I also
+    set `fix_Lz=True`. If you don't set `fix_Lz=True`, then the
+    length of the nanotube will get truncated such that it's an integer
+    multiple of the unit cell length, which, depending on the chirality, may
+    end up being very different than the desired length set by `Lz`.
+
+    Next, I add 20 SW defects distributed uniformly along the
+    :math:`z`-axis. For the `NanotubeStoneWalesDefectGenerator`,
+    if `uniform` is `True`, the code is written such it will divide the total
+    number of defects evenly among all nanotubes and distribute those
+    defects uniformly along the specified `bin_axis`. The circumferential
+    location of defects is selected randomly.  Future revisions will allow
+    more precise control of the positioning.
+
+    >>> swntbundle.generate_structure_defects(Nvac_sites=20, uniform=True,
+    ...                                       bin_axis='z', defect_type='575')
+
+    Running that command left me with the saved defect structure data which
+    I loaded into VMD. I used the VMD selection command that was printed to the
+    terminal window to highlight the atoms and bonds surrounding the defects.
+
+    The rendered structure looks like:
+
+    After relaxing the defected structure data in LAMMPS, the rendered
+    structure looks like:
+
+    """
+    def __init__(self, n=None, m=None, nx=1, ny=1, nz=1,
+                 element1='C', element2='C', bond=CCbond, vdw_spacing=3.4,
+                 bundle_packing=None, bundle_geometry=None,
+                 Lx=None, Ly=None, Lz=None, fix_Lz=False,
+                 rotate_structure=False, rotation_angle=None,
+                 rotation_axis=None,
+                 fname=None, outpath=None, structure_format=None,
+                 verbose=False, **kwargs):
+
+        self._Ntubes = None
+        self._Natoms_per_tube = None
+
+        if fname is None and isinstance(n, int) and isinstance(m, int):
+            generator = NanotubeBundleGenerator(
+                n=n, m=m, nx=nx, ny=ny, nz=nz, element1=element1,
+                element2=element2, bond=bond, vdw_spacing=vdw_spacing,
+                bundle_packing=bundle_packing, bundle_geometry=bundle_geometry,
+                Lx=Lx, Ly=Ly, Lz=Lz, fix_Lz=fix_Lz, verbose=verbose)
+            generator.save_data(structure_format='data', **kwargs)
+            fname = generator.fname
+            structure_format = generator.structure_format
+            self._Ntubes = generator.Ntubes
+            self._Natoms_per_tube = generator.Natoms_per_tube
+        else:
+            raise TypeError('Either `fname` but be provided or '
+                            '`n` and `m` must be specified as integers')
+
+        super(NanotubeStoneWalesDefectGenerator, self).__init__(
+            fname=fname, outpath=outpath, structure_format=structure_format,
+            verbose=verbose)
+
+    def generate_structure_defects(self, N_defect_sites=None, uniform=False,
+                                   bin_axis='z', defect_bounds=None,
+                                   Ntubes=None,
+                                   vmd_selection_radius=np.sqrt(10.5),
+                                   show_vmd_selection_cmd=True):
+        """Generate structure defects.
+
+        Parameters
+        ----------
+        uniform : bool, optional
+            Generate uniform defects in structure data.
+        bin_axis : {'x', 'y', 'z'}, optional
+            axis along which to generate uniform defects
+        Ntubes : {None, int}, optional
+        show_vmd_selection_cmd : bool, optional
+            Generate a VMD selection string that can be used to
+            select the atoms surrounding the defects.
+        vmd_selection_radius : float, optional
+            Cutoff radius for VMD selection command in units of **Angstroms**.
+
+        """
+
+        self._vmd_selection_radius = vmd_selection_radius
+        self._show_vmd_selection_cmd = show_vmd_selection_cmd
+
+        if uniform:
+            pass
+        else:
+            super(NanotubeStoneWalesDefectGenerator,
+                  self)._generate_random_defects()
+
+        super(NanotubeStoneWalesDefectGenerator,
+              self)._generate_structure_defects()
 
 
 class CrossLinkedNanotubeBundleGenerator(CrossLinkedDefectGenerator):
