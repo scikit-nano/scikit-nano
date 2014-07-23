a6e13a76bf2a9b89329a942d119b3cf9c000bc28
Andrew Merrill <androomerrill@gmail.com>
2014-07-23 01:00:17 -0400

    adding new _units module to core package for future dev

---
+++ sknano/core/_units.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+==================================================================
+Abstract data structures for math (:mod:`sknano.core._coremath`)
+==================================================================
+
+.. currentmodule:: sknano.core._coremath
+
+"""
+from __future__ import absolute_import, division, print_function
+__docformat__ = 'restructuredtext en'
