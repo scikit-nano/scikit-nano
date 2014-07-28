fa92182ce03e7740e49c19c366c25bd6e2861c05
Andrew Merrill <androomerrill@gmail.com>
2014-07-28 15:59:31 -0400

    updating __init__ imports

--- sknano/generators/__init__.py
+++ sknano/generators/__init__.py
@@ -10,4 +10,7 @@ Classes for structure generator objects (:mod:`sknano.generators`)
 from __future__ import absolute_import, division, print_function
 __docformat__ = 'restructuredtext en'
 
+from ._base import *
+from ._nanotube_generators import *
+
 __all__ = [s for s in dir() if not s.startswith('_')]
