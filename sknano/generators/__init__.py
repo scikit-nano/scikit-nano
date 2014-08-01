e5ff5b27a4c3f47d7f58e0e7452886297a0c076d
Andrew Merrill <androomerrill@gmail.com>
2014-08-01 04:02:41 -0400

    adding _nanotube_bundle_generator import

--- sknano/generators/__init__.py
+++ sknano/generators/__init__.py
@@ -12,5 +12,6 @@ __docformat__ = 'restructuredtext en'
 
 from ._base import *
 from ._nanotube_generators import *
+from ._nanotube_bundle_generators import *
 
 __all__ = [s for s in dir() if not s.startswith('_')]
