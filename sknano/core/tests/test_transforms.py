638940bd3494a4e7399e374bffb1dfb3e2517b02
Andrew Merrill <androomerrill@gmail.com>
2014-08-20 10:34:43 -0400

    adding new test module

---
+++ sknano/core/tests/test_transforms.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+from __future__ import absolute_import, division, print_function
+import unittest
+
+from sknano.core import rotate_point, rotation_matrix, transformation_matrix
+
+
+class TestTransforms(unittest.TestCase):
+
+    def test_rotate_point(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
