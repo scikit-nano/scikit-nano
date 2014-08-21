5c55aadf08e83d6af2f4c8f90107082ba4f37297
Andrew Merrill <androomerrill@gmail.com>
2014-08-21 12:46:20 -0400

    breaking up test class into separate classes for each structure class

--- sknano/structures/tests/test_structures.py
+++ sknano/structures/tests/test_structures.py
@@ -7,18 +7,40 @@ import unittest
 from sknano.structures import SWNT, MWNT, SWNTBundle, MWNTBundle
 
 
-class TestNanotube(unittest.TestCase):
+class TestSWNT(unittest.TestCase):
 
     def setUp(self):
-        self.n = 10
-        self.m = 10
-        self.Ch = (self.n, self.m)
+        self.swnt = SWNT(10, 10)
 
     def test_swnt(self):
-        swnt = SWNT(n=self.n, m=self.m)
-        self.assertEqual(self.swnt.n, self.n)
+        self.assertEqual(self.swnt.n, 10)
 
-        mwnt = MWNT(n=self.n, m=self.m)
+
+class TestSWNTBundle(unittest.TestCase):
+
+    def setUp(self):
+        self.swntbundle = SWNTBundle(10, 10)
+
+    def test_swnt(self):
+        self.assertEqual(self.swntbundle.n, 10)
+
+
+class TestMWNT(unittest.TestCase):
+
+    def setUp(self):
+        self.mwnt = MWNT(10, 10)
+
+    def test_mwnt(self):
+        self.assertEqual(self.mwnt.n, 10)
+
+
+class TestMWNTBundle(unittest.TestCase):
+
+    def setUp(self):
+        self.mwntbundle = MWNTBundle(10, 10)
+
+    def test_mwnt(self):
+        self.assertEqual(self.mwntbundle.n, 10)
 
 if __name__ == '__main__':
     unittest.main()
