14158cf16d066aa5cf0ac382a19edbb8372000b9
Andrew Merrill <androomerrill@gmail.com>
2014-08-23 04:55:05 -0400

    adding new setup modules at the individual subpackage level

---
+++ sknano/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from __future__ import division, print_function, absolute_import
+
+
+def configuration(parent_package='', top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration('sknano', parent_package, top_path)
+    config.add_subpackage('apps')
+    config.add_subpackage('core')
+    config.add_subpackage('generators')
+    config.add_subpackage('io')
+    config.add_subpackage('scripts')
+    config.add_subpackage('structures')
+    config.add_subpackage('utils')
+    config.add_data_dir('data')
+    config.make_config_py()
+    return config
+
+if __name__ == '__main__':
+    from numpy.distutils.core import setup
+    setup(**configuration(top_path='').todict())
