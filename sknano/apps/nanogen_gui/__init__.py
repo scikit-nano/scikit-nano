bc9ee9aa4a9c4e0d9f7bd0aaf12192dc419e52f3
Andrew Merrill <androomerrill@gmail.com>
2014-08-22 21:32:01 -0400

    updating module imports

--- sknano/apps/nanogen_gui/__init__.py
+++ sknano/apps/nanogen_gui/__init__.py
@@ -9,7 +9,7 @@ NanoGen GUI front-ends (:mod:`sknano.apps.nanogen_gui`)
 .. versionadded:: 0.2.24
 
 """
-from __future__ import division, print_function, absolute_import
+from __future__ import absolute_import, division, print_function
 __docformat__ = 'restructuredtext en'
 
 from ._ng_model import *
--- sknano/apps/nanogen_gui/_ng_model.py
+++ sknano/apps/nanogen_gui/_ng_model.py
@@ -10,7 +10,8 @@ NanoGen model (:mod:`sknano.apps.nanogen_gui._ng_model`)
 from __future__ import absolute_import, division, print_function
 __docformat__ = 'restructuredtext en'
 
-from ..nanogen import NanotubeBundle, Graphene
+from sknano.core.refdata import CCbond, dVDW
+from sknano.structures import SWNT, SWNTBundle, Graphene
 
 __all__ = ['NGModel']
 
@@ -19,160 +20,161 @@ class NGModel(object):
     """:mod:`~sknano.apps.nanogen_gui` MVC model class."""
     def __init__(self):
         self._observers = []
-        self._swntbundle = NanotubeBundle(n=10, m=10, bond=1.42)
-        self._graphene = Graphene(length=10, width=1, bond=1.42, edge='AC')
+        self.swnt = SWNT(n=10, m=10, bond=CCbond)
+        self.swntbundle = SWNTBundle(n=10, m=10, bond=CCbond)
+        self.graphene = Graphene(length=10, width=1, bond=CCbond, edge='AC')
 
     @property
     def n(self):
         """Chiral index :math:`n`"""
-        return self._swntbundle.n
+        return self.swntbundle.n
 
     @n.setter
     def n(self, value):
-        self._swntbundle.n = value
+        self.swntbundle.n = value
         self.notify_observers()
 
     @property
     def m(self):
         """Chiral index :math:`m`"""
-        return self._swntbundle.m
+        return self.swntbundle.m
 
     @m.setter
     def m(self, value):
-        self._swntbundle.m = value
+        self.swntbundle.m = value
         self.notify_observers()
 
     @property
     def nanotube_bond(self):
-        return self._swntbundle.bond
+        return self.swntbundle.bond
 
     @nanotube_bond.setter
     def nanotube_bond(self, value):
-        self._swntbundle.bond = value
+        self.swntbundle.bond = value
         self.notify_observers()
 
     @property
     def nanotube_element1(self):
-        return self._swntbundle.element1
+        return self.swntbundle.element1
 
     @nanotube_element1.setter
     def nanotube_element1(self, value):
-        self._swntbundle.element1 = value
+        self.swntbundle.element1 = value
         self.notify_observers()
 
     @property
     def nanotube_element2(self):
-        return self._swntbundle.element2
+        return self.swntbundle.element2
 
     @nanotube_element2.setter
     def nanotube_element2(self, value):
-        self._swntbundle.element2 = value
+        self.swntbundle.element2 = value
         self.notify_observers()
 
     @property
     def Lx(self):
-        return self._swntbundle.Lx
+        return self.swntbundle.Lx
 
     @property
     def Ly(self):
-        return self._swntbundle.Ly
+        return self.swntbundle.Ly
 
     @property
     def Lz(self):
-        return self._swntbundle.Lz
+        return self.swntbundle.Lz
 
     @Lz.setter
     def Lz(self, value):
-        self._swntbundle.Lz = value
+        self.swntbundle.Lz = value
         self.notify_observers()
 
     @property
     def nx(self):
-        return self._swntbundle.nx
+        return self.swntbundle.nx
 
     @nx.setter
     def nx(self, value):
-        self._swntbundle.nx = value
+        self.swntbundle.nx = value
         self.notify_observers()
 
     @property
     def ny(self):
-        return self._swntbundle.ny
+        return self.swntbundle.ny
 
     @ny.setter
     def ny(self, value):
-        self._swntbundle.ny = value
+        self.swntbundle.ny = value
         self.notify_observers()
 
     @property
     def nz(self):
-        return self._swntbundle.nz
+        return self.swntbundle.nz
 
     @nz.setter
     def nz(self, value):
-        self._swntbundle.nz = value
+        self.swntbundle.nz = value
         self.notify_observers()
 
     @property
     def length(self):
-        return self._graphene.length
+        return self.graphene.length
 
     @length.setter
     def length(self, value):
-        self._graphene.length = value
+        self.graphene.length = value
         self.notify_observers()
 
     @property
     def width(self):
-        return self._graphene.width
+        return self.graphene.width
 
     @width.setter
     def width(self, value):
-        self._graphene.width = value
+        self.graphene.width = value
         self.notify_observers()
 
     @property
     def edge(self):
-        return self._graphene.edge
+        return self.graphene.edge
 
     @edge.setter
     def edge(self, value):
-        self._graphene.edge = value
+        self.graphene.edge = value
 
     @property
     def nlayers(self):
-        return self._graphene.nlayers
+        return self.graphene.nlayers
 
     @nlayers.setter
     def nlayers(self, value):
-        self._graphene.nlayers = value
+        self.graphene.nlayers = value
         self.notify_observers()
 
     @property
     def graphene_bond(self):
-        return self._graphene.bond
+        return self.graphene.bond
 
     @graphene_bond.setter
     def graphene_bond(self, value):
-        self._graphene.bond = value
+        self.graphene.bond = value
         self.notify_observers()
 
     @property
     def graphene_element1(self):
-        return self._graphene.element1
+        return self.graphene.element1
 
     @graphene_element1.setter
     def graphene_element1(self, value):
-        self._graphene.element1 = value
+        self.graphene.element1 = value
         self.notify_observers()
 
     @property
     def graphene_element2(self):
-        return self._graphene.element2
+        return self.graphene.element2
 
     @graphene_element2.setter
     def graphene_element2(self, value):
-        self._graphene.element2 = value
+        self.graphene.element2 = value
         self.notify_observers()
 
     def register_observer(self, observer):
@@ -182,7 +184,7 @@ class NGModel(object):
         self._observers.remove(observer)
 
     def notify_observers(self):
-        self._swntbundle.compute_bundle_params()
-        self._graphene.compute_layer_params()
+        self.swntbundle.compute_bundle_params()
+        self.graphene.compute_layer_params()
         for observer in self._observers[:]:
             observer.update_app_view()
