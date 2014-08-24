9cf4fa0542ebf0e2c5f309d352e38f1d2e543408
Andrew Merrill <androomerrill@gmail.com>
2014-08-24 03:24:13 -0400

    consolidating and cleaning up some test_methods

--- sknano/core/tests/test_math.py
+++ sknano/core/tests/test_math.py
@@ -67,12 +67,6 @@ class TestPoint(unittest.TestCase):
 class TestVector(unittest.TestCase):
     def test_vector_with_no_args(self):
         v = Vector()
-        print('vector: {}\n'.format(v))
-        print('vector.x: {}'.format(v.x))
-        print('vector.y: {}'.format(v.y))
-        print('vector.z: {}'.format(v.z))
-        print('vector.p0: {}'.format(v.p0))
-        print('vector.p: {}\n'.format(v.p))
         self.assertTrue(np.allclose(v, np.zeros(3)))
         self.assertEqual(v.x, 0.0)
         self.assertEqual(v.y, 0.0)
@@ -82,42 +76,23 @@ class TestVector(unittest.TestCase):
 
     def test_vector_with_list_of_ones(self):
         v = Vector([1, 1, 1])
-        print('vector: {}\n'.format(v))
-        print('vector.x: {}'.format(v.x))
-        print('vector.y: {}'.format(v.y))
-        print('vector.z: {}'.format(v.z))
-        print('vector.p0: {}'.format(v.p0))
-        print('vector.p: {}\n'.format(v.p))
         self.assertTrue(np.allclose(v, np.ones(3)))
         self.assertEqual(v.x, 1.0)
         self.assertEqual(v.y, 1.0)
         self.assertEqual(v.z, 1.0)
         self.assertTrue(np.allclose(v.p0, np.zeros(3)))
-        self.assertTrue(np.allclose(v.p, v))
+        self.assertTrue(np.allclose(v.p, np.ones(3)))
 
-    def test_vector_with_p_set(self):
+    def test_vector_with_kwargs(self):
         v = Vector(p=[1, 1, 1])
-        print('vector: {}\n'.format(v))
-        print('vector.x: {}'.format(v.x))
-        print('vector.y: {}'.format(v.y))
-        print('vector.z: {}'.format(v.z))
-        print('vector.p0: {}'.format(v.p0))
-        print('vector.p: {}\n'.format(v.p))
         self.assertTrue(np.allclose(v, np.ones(3)))
         self.assertEqual(v.x, 1.0)
         self.assertEqual(v.y, 1.0)
         self.assertEqual(v.z, 1.0)
         self.assertTrue(np.allclose(v.p0, np.zeros(3)))
-        self.assertTrue(np.allclose(v.p, v))
+        self.assertTrue(np.allclose(v.p, np.ones(3)))
 
-    def test_vector_with_p0_set(self):
         v = Vector(p0=[1, 1, 1])
-        print('vector: {}\n'.format(v))
-        print('vector.x: {}'.format(v.x))
-        print('vector.y: {}'.format(v.y))
-        print('vector.z: {}'.format(v.z))
-        print('vector.p0: {}'.format(v.p0))
-        print('vector.p: {}\n'.format(v.p))
         self.assertTrue(np.allclose(v, -np.ones(3)))
         self.assertEqual(v.x, -1.0)
         self.assertEqual(v.y, -1.0)
@@ -125,102 +100,52 @@ class TestVector(unittest.TestCase):
         self.assertTrue(np.allclose(v.p0, np.ones(3)))
         self.assertTrue(np.allclose(v.p, np.zeros(3)))
 
-    def test_vector_with_p_and_p0_set(self):
         v = Vector(p=[1, 1, 1], p0=[1, 1, 1])
-        print('vector: {}\n'.format(v))
-        print('vector.x: {}'.format(v.x))
-        print('vector.y: {}'.format(v.y))
-        print('vector.z: {}'.format(v.z))
-        print('vector.p0: {}'.format(v.p0))
-        print('vector.p: {}\n'.format(v.p))
         self.assertTrue(np.allclose(v, np.zeros(3)))
         self.assertEqual(v.x, 0.0)
         self.assertEqual(v.y, 0.0)
         self.assertEqual(v.z, 0.0)
         self.assertTrue(np.allclose(v.p0, np.ones(3)))
         self.assertTrue(np.allclose(v.p, np.ones(3)))
-
-    def test_changing_p0(self):
-        v = Vector()
-        print('vector: {}\n'.format(v))
-        v.p0 = np.ones(3)
-        print('vector: {}'.format(v))
-        print('vector.x: {}'.format(v.x))
-        print('vector.y: {}'.format(v.y))
-        print('vector.z: {}'.format(v.z))
-        print('vector.p0: {}'.format(v.p0))
-        print('vector.p: {}\n'.format(v.p))
-        self.assertTrue(np.allclose(v, -np.ones(3)))
-        self.assertEqual(v.x, -1.0)
-        self.assertEqual(v.y, -1.0)
-        self.assertEqual(v.z, -1.0)
-        self.assertTrue(np.allclose(v.p0, np.ones(3)))
-        self.assertTrue(np.allclose(v.p, np.zeros(3)))
 
     def test_property_changes(self):
+        v = Vector()
+        v.p0 = np.ones(3)
+        self.assertTrue(np.allclose(v, -np.ones(3)))
+        self.assertEqual(v.x, -1.0)
+        self.assertEqual(v.y, -1.0)
+        self.assertEqual(v.z, -1.0)
+        self.assertTrue(np.allclose(v.p0, np.ones(3)))
+        self.assertTrue(np.allclose(v.p, np.zeros(3)))
+
         v = Vector(p=[1., 1., 1.], p0=[1., 1., 1.])
-        print('vector: {}\n'.format(v))
-        print('vector.x: {}'.format(v.x))
-        print('vector.y: {}'.format(v.y))
-        print('vector.z: {}'.format(v.z))
-        print('vector.p0: {}'.format(v.p0))
-        print('vector.p: {}\n'.format(v.p))
         self.assertTrue(np.allclose(v, np.zeros(3)))
         self.assertEqual(v.x, 0.0)
         self.assertEqual(v.y, 0.0)
         self.assertEqual(v.z, 0.0)
         self.assertTrue(np.allclose(v.p0, np.ones(3)))
         self.assertTrue(np.allclose(v.p, np.ones(3)))
-        print('changing v.p0')
         v.p0 = np.zeros(3)
-        print('vector: {}'.format(v))
-        print('vector.x: {}'.format(v.x))
-        print('vector.y: {}'.format(v.y))
-        print('vector.z: {}'.format(v.z))
-        print('vector.p0: {}'.format(v.p0))
-        print('vector.p: {}'.format(v.p))
         self.assertTrue(np.allclose(v, np.ones(3)))
         self.assertEqual(v.x, 1.0)
         self.assertEqual(v.y, 1.0)
         self.assertEqual(v.z, 1.0)
         self.assertTrue(np.allclose(v.p0, np.zeros(3)))
         self.assertTrue(np.allclose(v.p, np.ones(3)))
-        print('changing v.x')
         v.x = 5.0
-        print('changing v.y')
-        v.y = -5.0
-        print('changing v.z')
-        v.z = 0.0
         self.assertEqual(v.x, 5.0)
-        self.assertEqual(v.y, -5.0)
-        self.assertEqual(v.z, 0.0)
-        print('vector: {}'.format(v))
-        print('vector.x: {}'.format(v.x))
-        print('vector.y: {}'.format(v.y))
-        print('vector.z: {}'.format(v.z))
-        print('vector.p0: {}'.format(v.p0))
-        print('vector.p: {}'.format(v.p))
-        print('vector.p.x: {}'.format(v.p.x))
-        print('vector.p.y: {}'.format(v.p.y))
-        print('vector.p.z: {}\n'.format(v.p.z))
         self.assertEqual(v.x, v.p.x)
+        v.y = -5.0
+        self.assertEqual(v.y, -5.0)
         self.assertEqual(v.y, v.p.y)
+        v.z = 0.0
+        self.assertEqual(v.z, 0.0)
         self.assertEqual(v.z, v.p.z)
-        print('changing v.p0')
-        v.p0 = np.array([0.5,-10,2.5])
-        print('vector.p: {}'.format(v.p))
-        print('vector.p0: {}'.format(v.p0))
-        print('vector v=p-p0: {}'.format(v))
-        print('vector.p.x: {}'.format(v.p.x))
-        print('vector.p.y: {}'.format(v.p.y))
-        print('vector.p.z: {}'.format(v.p.z))
-        print('vector.p0.x: {}'.format(v.p0.x))
-        print('vector.p0.y: {}'.format(v.p0.y))
-        print('vector.p0.z: {}'.format(v.p0.z))
-        print('vector.x: {}'.format(v.x))
-        print('vector.y: {}'.format(v.y))
-        print('vector.z: {}'.format(v.z))
-        print()
+
+        v.p0 = np.array([0.5, -10.0, 2.5])
+        self.assertEqual(v.p0.x, 0.5)
+        self.assertEqual(v.p0.y, -10.0)
+        self.assertEqual(v.p0.z, 2.5)
 
     def test_add(self):
         v1 = Vector([1.0, 0.0], p0=[5., 5.])
@@ -253,7 +178,25 @@ class TestVector(unittest.TestCase):
         print('v3.p0: {}\n'.format(v3.p0))
         print('v3.p: {}\n'.format(v3.p))
 
-    def test_cross(self):
+    def test_2D_dot(self):
+        v1 = Vector([1.0, 0.0])
+        v2 = Vector([1.0, 1.0])
+
+        # this way works, but defeats the purpose of the subclasses
+        print('\ncomputing np.dot(np.asarray(v1), np.asarray(v2))')
+        c = np.dot(np.asarray(v1), np.asarray(v2))
+        print('c = np.dot(v1, v2): {}\n'.format(c))
+        self.assertEqual(c, 1.0)
+
+        # now try the sknano.core.math.vector.dot function
+        print('v1: {}'.format(v1))
+        print('v2: {}'.format(v2))
+        print('\ncomputing vec.dot(v1, v2)')
+        c = vec.dot(v1, v2)
+        print('c = vec.dot(v1, v2): {}\n'.format(c))
+        self.assertEqual(c, 1.0)
+
+    def test_2D_cross(self):
         v1 = Vector([1.0, 0.0])
         v2 = Vector([1.0, 1.0])
 
@@ -264,6 +207,64 @@ class TestVector(unittest.TestCase):
         v3 = vec.cross(v1, v2)
         print('v3 = vec.cross(v1, v2) = {}\n'.format(v3))
         self.assertEqual(v3, np.cross(np.asarray(v1), np.asarray(v2)))
+
+        v1 = Vector([1, 0], p0=[1, 1])
+        v2 = Vector([0, 1], p0=[1, 1])
+
+        print('\ncomputing vec.cross(v1, v2)')
+        print('v1: {}'.format(v1))
+        print('v2: {}'.format(v2))
+        v3 = vec.cross(v1, v2)
+        print('v3 = vec.cross(v1, v2) = {}\n'.format(v3))
+        self.assertTrue(np.allclose(v3, np.cross(np.asarray(v1),
+                                                 np.asarray(v2))))
+
+        v1 = Vector([1, 0], p0=[1, 0])
+        v2 = Vector([0, 1], p0=[0, 1])
+
+        print('\ncomputing vec.cross(v1, v2)')
+        print('v1: {}'.format(v1))
+        print('v2: {}'.format(v2))
+        v3 = vec.cross(v1, v2)
+        print('v3 = vec.cross(v1, v2) = {}\n'.format(v3))
+        self.assertTrue(np.allclose(v3, np.cross(np.asarray(v1),
+                                                 np.asarray(v2))))
+
+    def test_3D_dot(self):
+        v1 = Vector([1.0, 0.0, 0.0])
+        v2 = Vector([1.0, 1.0, 0.0])
+
+        # this way works, but defeats the purpose of the subclasses
+        print('\ncomputing np.dot(np.asarray(v1), np.asarray(v2))')
+        c = np.dot(np.asarray(v1), np.asarray(v2))
+        print('c = np.dot(v1, v2): {}\n'.format(c))
+        self.assertEqual(c, 1.0)
+
+        # now try the sknano.core.math.vector.dot function
+        print('v1: {}'.format(v1))
+        print('v2: {}'.format(v2))
+        print('\ncomputing vec.dot(v1, v2)')
+        c = vec.dot(v1, v2)
+        print('c = vec.dot(v1, v2): {}\n'.format(c))
+        self.assertEqual(c, 1.0)
+
+    def test_3D_cross(self):
+        v1 = Vector([1, 0, 0])
+        v2 = Vector([0, 1, 0])
+
+        print('\ncomputing vec.cross(v1, v2)')
+        print('v1: {}'.format(v1))
+        print('v2: {}'.format(v2))
+        v3 = vec.cross(v1, v2)
+        print('v3 = vec.cross(v1, v2) = {}\n'.format(v3))
+        print('v3.nd: {}\n'.format(v3.nd))
+        print('v3.p0: {}\n'.format(v3.p0))
+        print('v3.p: {}\n'.format(v3.p))
+        self.assertIsInstance(v3, Vector)
+        self.assertTrue(np.allclose(v3, np.cross(np.asarray(v1),
+                                                 np.asarray(v2))))
+        self.assertTrue(np.allclose(v3.p0, v1.p0))
+        self.assertTrue(np.allclose(v3.p0, v2.p0))
 
         v1 = Vector([1, 0, 0], p0=[1, 1, 1])
         v2 = Vector([0, 1, 0], p0=[1, 1, 1])
@@ -316,24 +317,6 @@ class TestVector(unittest.TestCase):
         self.assertTrue(np.allclose(v3.p0, v1.p0))
         self.assertFalse(np.allclose(v3.p0, v2.p0))
 
-    def test_dot(self):
-        v1 = Vector([1.0, 0.0])
-        v2 = Vector([1.0, 1.0])
-
-        # this way works, but defeats the purpose of the subclasses
-        print('\ncomputing np.dot(np.asarray(v1), np.asarray(v2))')
-        c = np.dot(np.asarray(v1), np.asarray(v2))
-        print('c = np.dot(v1, v2): {}\n'.format(c))
-        self.assertEqual(c, 1.0)
-
-        # now try the sknano.core.math.vector.dot function
-        print('v1: {}'.format(v1))
-        print('v2: {}'.format(v2))
-        print('\ncomputing vec.dot(v1, v2)')
-        c = vec.dot(v1, v2)
-        print('c = vec.dot(v1, v2): {}\n'.format(c))
-        self.assertEqual(c, 1.0)
-
     def test_str(self):
         v1 = Vector()
         print('\n{!s}\n'.format(v1))
@@ -341,6 +324,7 @@ class TestVector(unittest.TestCase):
     def test_repr(self):
         v1 = Vector()
         print('\n{!r}\n'.format(v1))
+
 
 if __name__ == '__main__':
     unittest.main()
