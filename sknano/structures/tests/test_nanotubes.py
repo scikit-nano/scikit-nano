ddc7649154cbbbbdc44ee458c6e315a3d5c1f3e9
Andrew Merrill <androomerrill@gmail.com>
2014-08-25 17:03:52 -0400

    breaking apart the structure module tests into separate test_modules

---
+++ sknano/structures/tests/test_nanotubes.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#
+from __future__ import absolute_import, division, print_function
+
+import unittest
+
+from sknano.structures import SWNT, MWNT, SWNTBundle, MWNTBundle
+
+
+class TestSWNT(unittest.TestCase):
+
+    def setUp(self):
+        self.swnt = SWNT(10, 10)
+
+    def test_swnt(self):
+        self.assertEqual(self.swnt.n, 10)
+
+
+class TestSWNTBundle(unittest.TestCase):
+
+    def setUp(self):
+        self.swntbundle = SWNTBundle(10, 10)
+
+    def test_swnt(self):
+        self.assertEqual(self.swntbundle.n, 10)
+
+
+class TestMWNT(unittest.TestCase):
+
+    def setUp(self):
+        self.mwnt = MWNT(10, 10)
+
+    def test_mwnt(self):
+        self.assertEqual(self.mwnt.n, 10)
+
+
+class TestMWNTBundle(unittest.TestCase):
+
+    def setUp(self):
+        self.mwntbundle = MWNTBundle(10, 10)
+
+    def test_mwnt(self):
+        self.assertEqual(self.mwntbundle.n, 10)
+
+if __name__ == '__main__':
+    unittest.main()
