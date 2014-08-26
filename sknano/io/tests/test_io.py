f3a82ec9434ff97cbc55c7af2ec84400b8017d56
Andrew Merrill <androomerrill@gmail.com>
2014-08-25 20:24:34 -0400

    replacing unittest framework with nose test tools

--- sknano/io/tests/test_io.py
+++ sknano/io/tests/test_io.py
@@ -1,37 +1,28 @@
 # -*- coding: utf-8 -*-
 #
 from __future__ import absolute_import, division, print_function
-from pprint import pprint
 
-import unittest
-
+import nose
+from nose.tools import *
 from sknano.io import DATAData, DATAReader, DATAWriter, DATA2XYZConverter, \
     XYZData, XYZReader, XYZWriter, XYZ2DATAConverter
 
 
-class TestDATAIO(unittest.TestCase):
-
-    def setUp(self):
-        self.infile = '1010r_1cell.data'
-
-    def test_reader(self):
-        reader = DATAReader(self.infile)
-        atoms = reader.atoms
-        print('Natoms: {}'.format(atoms.Natoms))
-        print('atoms: {}'.format(atoms))
+def test_reader():
+    infile = '1010r_1cell.data'
+    reader = DATAReader(infile)
+    atoms = reader.atoms
+    print('Natoms: {}'.format(atoms.Natoms))
+    print('atoms: {}'.format(atoms))
 
 
-class TestXYZIO(unittest.TestCase):
-
-    def setUp(self):
-        self.infile = '1010r_1cell.xyz'
-
-    def test_reader(self):
-        reader = XYZReader(self.infile)
-        atoms = reader.atoms
-        print('Natoms: {}'.format(atoms.Natoms))
-        print('atoms: {}'.format(atoms))
+def test_reader():
+    infile = '1010r_1cell.xyz'
+    reader = XYZReader(infile)
+    atoms = reader.atoms
+    print('Natoms: {}'.format(atoms.Natoms))
+    print('atoms: {}'.format(atoms))
 
 
 if __name__ == '__main__':
-    unittest.main()
+    nose.runmodule()
