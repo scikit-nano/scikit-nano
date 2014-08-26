f55ac6bd411d5ec25bceb22b02e6acc888f799f2
Andrew Merrill <androomerrill@gmail.com>
2014-08-26 14:19:17 -0400

    updating test modules

--- sknano/structures/tests/test_extras.py
+++ sknano/structures/tests/test_extras.py
@@ -1,44 +1,21 @@
 # -*- coding: utf-8 -*-
-#
 from __future__ import absolute_import, division, print_function
 
-from sknano.structures import SWNT, MWNT, SWNTBundle, MWNTBundle
+import nose
+from nose.tools import *
+from sknano.structures import filter_Ch_list, generate_Ch_list
 
 
-class TestSWNT(unittest.TestCase):
-
-    def setUp(self):
-        self.swnt = SWNT(10, 10)
-
-    def test_swnt(self):
-        self.assertEqual(self.swnt.n, 10)
+def test_generate_Ch_list():
+    Ch_list = generate_Ch_list(ni=5, nf=20, mi=0, mf=20, handedness='right')
+    assert_equal(len(Ch_list), 216)
 
 
-class TestSWNTBundle(unittest.TestCase):
+def test_filter_Ch_list():
+    Ch_list = generate_Ch_list(ni=5, nf=20, mi=0, mf=20, handedness='right')
+    Ch_list = filter_Ch_list(Ch_list)
+    assert_equal(len(Ch_list), 216)
 
-    def setUp(self):
-        self.swntbundle = SWNTBundle(10, 10)
-
-    def test_swnt(self):
-        self.assertEqual(self.swntbundle.n, 10)
-
-
-class TestMWNT(unittest.TestCase):
-
-    def setUp(self):
-        self.mwnt = MWNT(10, 10)
-
-    def test_mwnt(self):
-        self.assertEqual(self.mwnt.n, 10)
-
-
-class TestMWNTBundle(unittest.TestCase):
-
-    def setUp(self):
-        self.mwntbundle = MWNTBundle(10, 10)
-
-    def test_mwnt(self):
-        self.assertEqual(self.mwntbundle.n, 10)
 
 if __name__ == '__main__':
-    unittest.main()
+    nose.runmodule()
--- sknano/structures/tests/test_swnt_bundle.py
+++ sknano/structures/tests/test_swnt_bundle.py
@@ -3,27 +3,13 @@ from __future__ import absolute_import, division, print_function
 
 import nose
 from nose.tools import *
-from sknano.structures import SWNT, MWNT, SWNTBundle, MWNTBundle
-
-
-def test_swnt():
-    swnt = SWNT(10, 10)
-    assertEqual(swnt.n, 10)
+from sknano.structures import SWNTBundle
 
 
 def test_swnt_bundle():
     swntbundle = SWNTBundle(10, 10)
-    assertEqual(swntbundle.n, 10)
+    assert_equal(swntbundle.n, 10)
 
-
-def test_mwnt():
-    mwnt = MWNT(10, 10)
-    assertEqual(mwnt.n, 10)
-
-
-def test_mwnt_bundle():
-    mwntbundle = MWNTBundle(10, 10)
-    assertEqual(mwntbundle.n, 10)
 
 if __name__ == '__main__':
     nose.runmodule()
