8e72fcf0d4cae7e85bdf9856276626ba88df4549
Andrew Merrill <androomerrill@gmail.com>
2014-08-25 20:27:56 -0400

    cleaning up old code

--- sknano/io/tests/test_xyz_format.py
+++ sknano/io/tests/test_xyz_format.py
@@ -4,24 +4,12 @@ from __future__ import absolute_import, division, print_function
 
 import nose
 from nose.tools import *
-from sknano.io import DATAData, DATAReader, DATAWriter, DATA2XYZConverter, \
-    XYZData, XYZReader, XYZWriter, XYZ2DATAConverter
-
-
-def test_reader():
-    infile = '1010r_1cell.data'
-    reader = DATAReader(infile)
-    atoms = reader.atoms
-    print('Natoms: {}'.format(atoms.Natoms))
-    print('atoms: {}'.format(atoms))
+from sknano.io import XYZData, XYZReader, XYZWriter, XYZ2DATAConverter
 
 
 def test_reader():
     infile = '1010r_1cell.xyz'
     reader = XYZReader(infile)
-    atoms = reader.atoms
-    print('Natoms: {}'.format(atoms.Natoms))
-    print('atoms: {}'.format(atoms))
 
 
 if __name__ == '__main__':
