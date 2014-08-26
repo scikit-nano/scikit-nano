00a652aaef73f7f40479554d6e89df50649ce1e0
Andrew Merrill <androomerrill@gmail.com>
2014-08-25 20:20:57 -0400

    replacing unittest framework with nose test tools

--- sknano/core/math/tests/test_point.py
+++ sknano/core/math/tests/test_point.py
@@ -2,77 +2,52 @@
 
 from __future__ import absolute_import, division, print_function
 
-#import nose
-import unittest
+import nose
+from nose.tools import *
 
 import numpy as np
 
 from sknano.core.math import Point
 
 
-class TestPoint(unittest.TestCase):
+def test_instantiation():
+    p = Point()
+    assert_true(np.allclose(p, np.zeros(3)))
 
-    def test_point_with_no_args(self):
-        pt = Point()
-        print('point: {}'.format(pt))
-        print('point.x: {}'.format(pt.x))
-        print('point.y: {}'.format(pt.y))
-        print('point.z: {}\n'.format(pt.z))
+    p = Point([0,0,0], dtype=float)
+    assert_true(np.allclose(p, np.zeros(3)))
 
-    def test_point_with_list_of_zeros(self):
-        pt = Point([0,0,0], dtype=float)
-        print('point: {}'.format(pt))
-        print('point.x: {}'.format(pt.x))
-        print('point.y: {}'.format(pt.y))
-        print('point.z: {}\n'.format(pt.z))
-        self.assertTrue(np.allclose(pt, np.zeros(3)))
-        self.assertEqual(pt.x, 0.0)
-        self.assertEqual(pt.y, 0.0)
-        self.assertEqual(pt.z, 0.0)
+    p = Point(np.ones(3))
+    assert_true(np.allclose(p, np.ones(3)))
+    assert_equal(pt.x, 1.0)
+    assert_equal(pt.y, 1.0)
+    assert_equal(pt.z, 1.0)
 
-    def test_point_with_ones(self):
-        pt = Point(np.ones(3))
-        print('point: {}'.format(pt))
-        print('point.x: {}'.format(pt.x))
-        print('point.y: {}'.format(pt.y))
-        print('point.z: {}\n'.format(pt.z))
-        self.assertTrue(np.allclose(pt, np.ones(3)))
-        self.assertEqual(pt.x, 1.0)
-        self.assertEqual(pt.y, 1.0)
-        self.assertEqual(pt.z, 1.0)
+    p = Point([None, None, None])
+    assert_true(np.allclose(p, np.zeros(3)))
 
-    def test_point_with_list_of_None(self):
-        pt = Point([None, None, None])
-        print('point: {}'.format(pt))
-        pt.x = 2.5
-        print('point: {}'.format(pt))
+    p = Point(np.arange(10))
+    assert_true(np.allclose(p, np.arange(10)))
 
-    def test_rezero_coords(self):
-        pt = Point([1e-9,1e-11,-1e-16])
-        print('point: {}'.format(pt))
-        print('point.x: {}'.format(pt.x))
-        print('point.y: {}'.format(pt.y))
-        print('point.z: {}'.format(pt.z))
-        pt.rezero_coords()
-        print('point: {}\n'.format(pt))
 
-    def test_point_with_arange10(self):
-        pt = Point(np.arange(10))
-        print('point: {}\n'.format(pt))
+def test_rezero_coords():
+    p = Point([1e-9,1e-11,-1e-16])
+    p.rezero()
 
-    def test_transforms(self):
-        p = Point([1.0, 0.0, 0.0])
-        p.rotate(np.pi/2, rot_axis='z')
-        self.assertTrue(np.allclose(p, np.array([0, 1, 0])))
-        p.rotate(np.pi/2, rot_axis='z')
-        self.assertTrue(np.allclose(p, np.array([-1, 0, 0])))
-        p.rotate(np.pi/2, rot_axis='z')
-        self.assertTrue(np.allclose(p, np.array([0, -1, 0])))
-        p.rotate(np.pi/2, rot_axis='z')
-        self.assertTrue(np.allclose(p, np.array([1, 0, 0])))
-        p.rotate(np.pi/2, rot_axis=[0, 0, 1], anchor_point=[1, 1, 0])
-        self.assertTrue(np.allclose(p, np.array([2, 1, 0])))
+
+def test_transforms():
+    p = Point([1.0, 0.0, 0.0])
+    p.rotate(np.pi/2, rot_axis='z')
+    assert_true(np.allclose(p, np.array([0, 1, 0])))
+    p.rotate(np.pi/2, rot_axis='z')
+    assert_true(np.allclose(p, np.array([-1, 0, 0])))
+    p.rotate(np.pi/2, rot_axis='z')
+    assert_true(np.allclose(p, np.array([0, -1, 0])))
+    p.rotate(np.pi/2, rot_axis='z')
+    assert_true(np.allclose(p, np.array([1, 0, 0])))
+    p.rotate(np.pi/2, rot_axis=[0, 0, 1], anchor_point=[1, 1, 0])
+    assert_true(np.allclose(p, np.array([2, 1, 0])))
 
 
 if __name__ == '__main__':
-    unittest.main()
+    nose.runmodule()
--- sknano/core/math/tests/test_transforms.py
+++ sknano/core/math/tests/test_transforms.py
@@ -1,61 +1,62 @@
 #! /usr/bin/env python
 
 from __future__ import absolute_import, division, print_function
+import nose
+from nose.tools import *
 import numpy as np
-import unittest
 
 from sknano.core.math import rotation_matrix, transformation_matrix, \
     rotation_transform, Rx, Ry, Rz, Point, Vector
 
 
-class TestTransforms(unittest.TestCase):
+def test_rotation_matrix():
+    assert_true(np.allclose(rotation_matrix(np.pi/2),
+                            np.array([[0.0, -1.0], [1.0, 0.0]])))
 
-    def test_rotation_matrix(self):
-        self.assertTrue(np.allclose(rotation_matrix(np.pi/2),
-                                    np.array([[0.0, -1.0], [1.0, 0.0]])))
+    sqrt2 = np.sqrt(2)
+    assert_true(np.allclose(rotation_matrix(np.pi/4),
+                            np.array([[1 / sqrt2, -1 / sqrt2],
+                                      [1 / sqrt2, 1 / sqrt2]])))
 
-        sqrt2 = np.sqrt(2)
-        self.assertTrue(np.allclose(rotation_matrix(np.pi/4),
-                                    np.array([[1 / sqrt2, -1 / sqrt2],
-                                              [1 / sqrt2, 1 / sqrt2]])))
+    sqrt3 = np.sqrt(3)
+    assert_true(np.allclose(
+        rotation_matrix(np.pi/2, rot_axis=Vector(np.ones(3))),
+        np.array([[1 / 3, 1 / 3 - 1 / sqrt3, 1 / 3 + 1 / sqrt3],
+                  [1 / 3 + 1 / sqrt3, 1 / 3, 1 / 3 - 1 / sqrt3],
+                  [1 / 3 - 1 / sqrt3, 1 / 3 + 1 / sqrt3, 1 / 3]])))
 
-        sqrt3 = np.sqrt(3)
-        self.assertTrue(np.allclose(
-            rotation_matrix(np.pi/2, rot_axis=Vector(np.ones(3))),
-            np.array([[1 / 3, 1 / 3 - 1 / sqrt3, 1 / 3 + 1 / sqrt3],
-                      [1 / 3 + 1 / sqrt3, 1 / 3, 1 / 3 - 1 / sqrt3],
-                      [1 / 3 - 1 / sqrt3, 1 / 3 + 1 / sqrt3, 1 / 3]])))
+    [[assert_true(np.allclose(
+        Rmat(angle), rotation_matrix(angle, rot_axis=axis)))
+        for angle in np.linspace(0.0, 2 * np.pi, np.pi / 4)]
+        for Rmat, axis in zip((Rx, Ry, Rz), ('x', 'y', 'z'))]
 
-        [[self.assertTrue(np.allclose(
-            Rmat(angle), rotation_matrix(angle, rot_axis=axis)))
-            for angle in np.linspace(0.0, 2 * np.pi, np.pi / 4)]
-            for Rmat, axis in zip((Rx, Ry, Rz), ('x', 'y', 'z'))]
 
-    def test_transformation_matrix(self):
-        self.assertTrue(np.allclose(
-            transformation_matrix(np.pi/2, rot_axis=Point([1.0, 1.0])),
-            np.array([[0.0, -1.0, 2.0], [1.0, 0.0, 0.0], [0.0, 0.0, 1.0]])))
+def test_transformation_matrix():
+    assert_true(np.allclose(
+        transformation_matrix(np.pi/2, rot_axis=Point([1.0, 1.0])),
+        np.array([[0.0, -1.0, 2.0], [1.0, 0.0, 0.0], [0.0, 0.0, 1.0]])))
 
-    def test_point_rotation(self):
-        self.assertTrue(np.allclose(
-            rotation_transform(Point([1.0, 0.0]), np.pi / 2),
-            np.array([0.0, 1.0])))
 
-        with self.assertRaises(TypeError):
-            np.allclose(rotation_transform(
-                Point([1.0, 0.0, 0.0]), angle=np.pi/2),
-                np.array([0.0, 1.0, 0.0]))
+def test_point_rotation():
+    assert_true(np.allclose(
+        rotation_transform(Point([1.0, 0.0]), np.pi / 2),
+        np.array([0.0, 1.0])))
 
-        self.assertTrue(np.allclose(
-            rotation_transform(Point([1.0, 0.0, 0.0]), angle=np.pi / 2,
-                               rot_axis=Vector([1.0, 0.0, 0.0])),
-            np.array([1.0, 0.0, 0.0])))
+    with assert_raises(TypeError):
+        np.allclose(rotation_transform(
+            Point([1.0, 0.0, 0.0]), angle=np.pi/2),
+            np.array([0.0, 1.0, 0.0]))
 
-        self.assertTrue(np.allclose(
-            rotation_transform(Point([0.0, 1.0, 0.0]), angle=np.pi / 2,
-                               rot_axis=Vector([1.0, 0.0, 0.0])),
-            np.array([0.0, 0.0, 1.0])))
+    assert_true(np.allclose(
+        rotation_transform(Point([1.0, 0.0, 0.0]), angle=np.pi / 2,
+                           rot_axis=Vector([1.0, 0.0, 0.0])),
+        np.array([1.0, 0.0, 0.0])))
+
+    assert_true(np.allclose(
+        rotation_transform(Point([0.0, 1.0, 0.0]), angle=np.pi / 2,
+                           rot_axis=Vector([1.0, 0.0, 0.0])),
+        np.array([0.0, 0.0, 1.0])))
 
 
 if __name__ == '__main__':
-    unittest.main()
+    nose.runmodule()
