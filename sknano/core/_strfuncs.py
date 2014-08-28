f459b7a459dbb686b6b148abf938a958f42a4317
Andrew Merrill <androomerrill@gmail.com>
2014-08-28 01:25:22 -0400

    replacing plural_word_check with pluralize

--- sknano/core/_strfuncs.py
+++ sknano/core/_strfuncs.py
@@ -10,10 +10,15 @@ Helper functions for string manipulation (:mod:`sknano.core._strfuncs`)
 from __future__ import absolute_import, division, print_function
 __docformat__ = 'restructuredtext en'
 
-__all__ = ['plural_word_check']
+__all__ = ['plural_word_check', 'pluralize']
 
 
 def plural_word_check(word, count):
+    """Alias for :func:`pluralize`"""
+    return pluralize(word, count)
+
+
+def pluralize(word, count):
     """Make a word plural by adding an *s* if `count` != 1.
 
     Parameters
