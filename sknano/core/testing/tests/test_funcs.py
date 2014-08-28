76154787aa30021298748a2008c97de3f2f4fe4d
Andrew Merrill <androomerrill@gmail.com>
2014-08-27 20:57:33 -0400

    updating test code

--- sknano/core/testing/tests/test_funcs.py
+++ sknano/core/testing/tests/test_funcs.py
@@ -14,9 +14,9 @@ from sknano.core.testing import generate_atoms
 def test_generate_atoms():
     atoms = generate_atoms(elements=element_symbols)
     assert_equals(atoms.Natoms, len(element_symbols))
-
-    atoms = generate_atoms(structure='SWNT')
-
+    atoms = \
+        generate_atoms(generator_class='SWNTGenerator', n=10, m=10, nz=10)
+    assert_equals(atoms.Natoms, 400)
 
 if __name__ == '__main__':
     nose.runmodule()
