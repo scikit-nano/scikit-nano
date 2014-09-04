040109a45750c0d1eb84595c0c9e4b4938c19234
Andrew Merrill <androomerrill@gmail.com>
2014-09-04 11:13:05 -0400

    adding new property methods for mean_length, mean_angle, and atoms

--- sknano/core/atoms/_bonds.py
+++ sknano/core/atoms/_bonds.py
@@ -16,6 +16,7 @@ from operator import attrgetter
 import numpy as np
 
 from sknano.core.math import vector as vec
+from ._extended_atoms import XAtoms
 from ._base import BondList
 #from ._bond import Bond
 
@@ -67,6 +68,20 @@ class Bonds(BondList):
         return np.asarray([bond.length for bond in self])
 
     @property
+    def mean_length(self):
+        return np.mean(self.lengths)
+
+    @property
     def angles(self):
         return np.asarray([vec.angle(b1.vector, b2.vector) for (b1, b2) in
                            combinations(self, 2)])
+
+    @property
+    def mean_angle(self):
+        return np.mean(self.angles)
+
+    @property
+    def atoms(self):
+        atoms = XAtoms()
+        [atoms.extend(bond.atoms) for bond in self]
+        return XAtoms(atoms=list(set(atoms)))
