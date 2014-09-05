98d1d7e0f3cf3b17d3f957f8f6d5ef86a2c07f9f
Andrew Merrill <androomerrill@gmail.com>
2014-09-04 22:56:22 -0400

    Refactoring UnrolledSWNT class.
    Moving calculation of `Lx` and `Ly` attributes to separate
    property methods and removing the `compute_params` function.

--- sknano/structures/_unrolled_swnt.py
+++ sknano/structures/_unrolled_swnt.py
@@ -13,31 +13,25 @@ __docformat__ = 'restructuredtext en'
 from sknano.core.refdata import dVDW
 
 from ._mixins import UnrolledSWNTMixin
-from ._swnt import SWNT
+from ._base import StructureBase
 
 __all__ = ['UnrolledSWNT']
 
 
-class UnrolledSWNT(UnrolledSWNTMixin, SWNT):
+class UnrolledSWNT(UnrolledSWNTMixin, StructureBase):
 
     def __init__(self, nx=1, ny=1, Lx=None, Ly=None, nlayers=1,
                  layer_spacing=dVDW, stacking_order='AB', **kwargs):
 
         super(UnrolledSWNT, self).__init__(**kwargs)
 
-        self._nx = int(nx)
-        self._ny = int(ny)
-        self._Lx = Lx
-        self._Ly = Ly
+        self.nx = nx
+        self.ny = ny
 
-        self.compute_tube_params()
+    @property
+    def Lx(self):
+        return self.nx * self.Ch / 10
 
-    def compute_tube_params(self):
-
-        super(UnrolledSWNT, self).compute_tube_params()
-
-        self._Lx = \
-            self.compute_Lx(n=self._n, m=self._m, nx=self._nx, bond=self._bond)
-
-        self._Ly = \
-            self.compute_Ly(n=self._n, m=self._m, ny=self._ny, bond=self._bond)
+    @property
+    def Ly(self):
+        return self.ny * dVDW / 10
