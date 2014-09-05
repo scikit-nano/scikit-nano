5210ec2dfcb142baa5af49959d2db891bb4a6809
Andrew Merrill <androomerrill@gmail.com>
2014-09-04 23:07:31 -0400

    adding new test functions for the GrapheneGenerator class

--- sknano/generators/tests/test_graphene_generator.py
+++ sknano/generators/tests/test_graphene_generator.py
@@ -4,7 +4,22 @@ from __future__ import absolute_import, division, print_function
 
 import nose
 from nose.tools import *
-#from sknano.generators import GrapheneGenerator
+from sknano.generators import GrapheneGenerator
+
+
+def test1():
+    generator = GrapheneGenerator(width=5, length=5)
+    generator.save_data()
+
+
+def test2():
+    generator = GrapheneGenerator(length=20, width=1, edge='ZZ')
+    generator.save_data()
+
+
+def test3():
+    generator = GrapheneGenerator(length=20, width=1, edge='AC')
+    generator.save_data()
 
 
 if __name__ == '__main__':
