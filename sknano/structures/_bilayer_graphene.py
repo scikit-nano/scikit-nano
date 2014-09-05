efac93c00d215cd04fb08543d887841409f62754
Andrew Merrill <androomerrill@gmail.com>
2014-09-04 22:33:08 -0400

    Graphene and BilayerGraphene structure class updates.
    Major refactoring to remove the useless property
    setter/getter methods and other small updates.

--- sknano/structures/_bilayer_graphene.py
+++ sknano/structures/_bilayer_graphene.py
@@ -10,32 +10,15 @@ Graphene structure classes (:mod:`sknano.structures._bilayer_graphene`)
 from __future__ import absolute_import, division, print_function
 __docformat__ = 'restructuredtext en'
 
-#import itertools
-
-import numpy as np
-
 from ._graphene import Graphene
-
-edge_types = {'armchair': 'AC', 'zigzag': 'ZZ'}
 
 __all__ = ['BilayerGraphene']
 
 
 class BilayerGraphene(Graphene):
-    def __init__(self, layer_rotation_angle=None, deg2rad=True, **kwargs):
+    def __init__(self, layer_rotation_angle=None, **kwargs):
 
-        if layer_rotation_angle is not None and deg2rad:
-            layer_rotation_angle = np.radians(layer_rotation_angle)
-        kwargs['layer_rotation_angles'] = layer_rotation_angle
-        kwargs['nlayers'] = 2
+        super(BilayerGraphene, self).__init__(
+            nlayers=2, layer_rotation_angles=layer_rotation_angle, **kwargs)
 
-        super(BilayerGraphene, self).__init__(**kwargs)
-        self._layer_rotation_angle = self._layer_rotation_angles
-
-    @property
-    def layer_rotation_angle(self):
-        return self._layer_rotation_angle
-
-    @layer_rotation_angle.setter
-    def layer_rotation_angle(self, value):
-        self._layer_rotation_angle = value
+        self.layer_rotation_angle = self.layer_rotation_angles
--- sknano/structures/_graphene.py
+++ sknano/structures/_graphene.py
@@ -11,19 +11,20 @@ from __future__ import absolute_import, division, print_function
 __docformat__ = 'restructuredtext en'
 
 #import itertools
+import numbers
 
 import numpy as np
 
-from ..core.atoms import Atom
-from ..core.math import Vector
-from ..core.refdata import CCbond, dVDW, grams_per_Da
-
-edge_types = {'armchair': 'AC', 'zigzag': 'ZZ'}
+#from sknano.core.atoms import Atom
+from sknano.core.math import Vector
+from sknano.core.refdata import dVDW  # , grams_per_Da
+from ._base import StructureBase
+from ._extras import edge_types
 
 __all__ = ['GraphenePrimitiveCell', 'Graphene']
 
 
-class GraphenePrimitiveCell(object):
+class GraphenePrimitiveCell(StructureBase):
     """Class for generating interactive Graphene primitive unit cell.
 
     Parameters
@@ -32,51 +33,26 @@ class GraphenePrimitiveCell(object):
         bond length between nearest-neighbor atoms in **Angstroms**.
 
     """
-    def __init__(self, bond=CCbond, **kwargs):
-        self._bond = bond
-        self._a = np.sqrt(3) * self._bond
+    def __init__(self, **kwargs):
+        super(GraphenePrimitiveCell, self).__init__(**kwargs)
+        self.a = np.sqrt(3) * self.bond
 
-        self._a1 = Vector(nd=2)
-        self._a2 = Vector(nd=2)
+        self.a1 = Vector(nd=2)
+        self.a2 = Vector(nd=2)
 
-        self._a1.x = self._a2.x = np.sqrt(3) / 2 * self._a
-        self._a1.y = 1 / 2 * self._a
-        self._a2.y = -self._a1.y
+        self.a1.x = self.a2.x = np.sqrt(3) / 2 * self.a
+        self.a1.y = 1 / 2 * self.a
+        self.a2.y = -self.a1.y
 
-        self._b1 = Vector(nd=2)
-        self._b2 = Vector(nd=2)
+        self.b1 = Vector(nd=2)
+        self.b2 = Vector(nd=2)
 
-        self._b1.x = self._b2.x = 1 / np.sqrt(3) * 2 * np.pi / self._a
-        self._b1.y = 2 * np.pi / self._a
-        self._b2.y = -self._b1.y
-
-    @property
-    def a(self):
-        """Length of graphene unit cell vector."""
-        return self._a
-
-    @property
-    def a1(self):
-        """:math:`a_1` unit vector."""
-        return self._a1
-
-    @property
-    def a2(self):
-        """:math:`a_2` unit vector."""
-        return self._a2
-
-    @property
-    def b1(self):
-        """:math:`b_1` reciprocal lattice vector."""
-        return self._b1
-
-    @property
-    def b2(self):
-        """:math:`b_2` reciprocal lattice vector."""
-        return self._b2
+        self.b1.x = self.b2.x = 1 / np.sqrt(3) * 2 * np.pi / self.a
+        self.b1.y = 2 * np.pi / self.a
+        self.b2.y = -self.b1.y
 
 
-class Graphene(object):
+class Graphene(StructureBase):
     """Class for generating interactive Graphene objects.
 
     Parameters
@@ -113,13 +89,14 @@ class Graphene(object):
 
     """
 
-    def __init__(self, length=None, width=None, edge=None, element1='C',
-                 element2='C', bond=CCbond, nlayers=1, layer_spacing=dVDW,
-                 layer_rotation_angles=None, stacking_order='AB',
-                 verbose=False):
+    def __init__(self, length=None, width=None, edge=None, nlayers=1,
+                 layer_spacing=dVDW, layer_rotation_angles=None,
+                 stacking_order='AB', deg2rad=True, **kwargs):
 
-        self._length = length
-        self._width = width
+        super(Graphene, self).__init__(**kwargs)
+
+        self.length = length
+        self.width = width
         if edge in ('armchair', 'zigzag'):
             edge = edge_types[edge]
         elif edge not in ('AC', 'ZZ'):
@@ -127,52 +104,50 @@ class Graphene(object):
                   'choosing one at random...\n')
             edge = np.random.choice(['AC', 'ZZ'])
             print('the randomly chosen edge type is: {}'.format(edge))
-        self._edge = edge
-
-        self._element1 = element1
-        self._element2 = element2
-
-        if bond is None:
-            bond = CCbond
-
-        self._bond = bond
-
-        self._verbose = verbose
+        self.edge = edge
 
         self._Nx = 0
         self._Ny = 0
 
-        self._layer_mass = None
-        self._Natoms = 0
-        self._Natoms_per_layer = None
+        self.layer_mass = None
+        self.Natoms = 0
+        self.Natoms_per_layer = 0
 
-        self._nlayers = nlayers
-        self._layer_spacing = layer_spacing
-        self._layer_rotation_angles = layer_rotation_angles
-        self._stacking_order = stacking_order
+        self.nlayers = nlayers
+        self.layer_spacing = layer_spacing
 
-        self._layer_shift = Vector()
+        if layer_rotation_angles is not None and deg2rad:
+            if isinstance(layer_rotation_angles, numbers.Number):
+                layer_rotation_angles = np.radians(layer_rotation_angles)
+            elif isinstance(layer_rotation_angles, (list, np.ndarray)):
+                layer_rotation_angles = \
+                    np.radians(np.asarray(layer_rotation_angles)).tolist()
+
+        self.layer_rotation_angles = layer_rotation_angles
+        self.stacking_order = stacking_order
+
+        self.layer_shift = Vector()
 
         if nlayers > 1 and stacking_order == 'AB':
             if edge == 'AC':
-                self._layer_shift.y = self._bond
+                self.layer_shift.y = self.bond
             else:
-                self._layer_shift.x = self._bond
+                self.layer_shift.x = self.bond
 
-        self._cell = Vector()
+        self.cell = Vector()
         if edge == 'AC':
             # Set up the unit cell with the armchair edge aligned
             # along the `y`-axis.
-            self._cell.x = np.sqrt(3) * bond
-            self._cell.y = 3 * bond
+            self.cell.x = np.sqrt(3) * self.bond
+            self.cell.y = 3 * self.bond
         else:
             # Set up the unit cell with the zigzag edge aligned
             # along the `y`-axis.
-            self._cell.x = 3 * bond
-            self._cell.y = np.sqrt(3) * bond
+            self.cell.x = 3 * self.bond
+            self.cell.y = np.sqrt(3) * self.bond
 
-        self._Nx = int(np.ceil(10 * self._width / self._cell.x))
-        self._Ny = int(np.ceil(10 * self._length / self._cell.y))
+        self.Nx = int(np.ceil(10 * self.width / self.cell.x))
+        self.Ny = int(np.ceil(10 * self.length / self.cell.y))
 
     def __repr__(self):
         retstr = 'Graphene(length={!r}, width={!r}, edge={!r}, ' + \
@@ -183,150 +158,3 @@ class Graphene(object):
                              self.element2, self.bond, self.nlayers,
                              self.layer_spacing, self.layer_rotation_angles,
                              self.stacking_order)
-
-    def compute_layer_params(self):
-        pass
-
-    @property
-    def Natoms(self):
-        """Number of :class:`~sknano.core.atoms.Atom`\ s in
-        **unit cell**."""
-        return self._Natoms
-
-    @property
-    def Natoms_per_layer(self):
-        """Number of :class:`~sknano.core.atoms.Atom`\ s in
-        **layer**."""
-        return self._Natoms_per_layer
-
-    @classmethod
-    def compute_Natoms_per_layer(cls, n=None, m=None, element1=None,
-                                 element2=None, **kwargs):
-        return 0
-
-    @property
-    def length(self):
-        """Graphene layer length in **nanometers**."""
-        return self._length
-
-    @length.setter
-    def length(self, value):
-        """Set graphene layer length in **nanometers**."""
-        self._length = value
-        self.compute_layer_params()
-
-    @property
-    def width(self):
-        """Graphene layer width in **nanometers**."""
-        return self._width
-
-    @width.setter
-    def width(self, value):
-        """Set graphene layer width in **nanometers**."""
-        self._width = value
-        self.compute_layer_params()
-
-    @property
-    def bond(self):
-        """Bond length in **Angstroms**."""
-        return self._bond
-
-    @bond.setter
-    def bond(self, value):
-        """Set bond length in **Angstroms**."""
-        self._bond = float(value)
-        self.compute_layer_params()
-
-    @property
-    def edge(self):
-        """Graphene edge type along length."""
-        return self._edge
-
-    @edge.setter
-    def edge(self, value):
-        """Set Graphene edge type along length."""
-        self._edge = value
-        self.compute_layer_params()
-
-    @property
-    def element1(self):
-        """Element symbol of :class:`~sknano.core.atoms.Atom` 1."""
-        return self._element1
-
-    @element1.setter
-    def element1(self, value):
-        """Set element symbol of :class:`~sknano.core.atoms.Atom` 1."""
-        self._element1 = value
-        self.compute_layer_params()
-
-    @property
-    def element2(self):
-        """Element symbol of :class:`~sknano.core.atoms.Atom` 2."""
-        return self._element2
-
-    @element2.setter
-    def element2(self, value):
-        """Set element symbol of :class:`~sknano.core.atoms.Atom` 2."""
-        self._element2 = value
-        self.compute_layer_params()
-
-    @property
-    def layer_mass(self):
-        """Graphene layer mass in grams."""
-        return self._layer_mass
-
-    @property
-    def nlayers(self):
-        return self._nlayers
-
-    @nlayers.setter
-    def nlayers(self, value):
-        self._nlayers = value
-
-    @property
-    def layer_spacing(self):
-        return self._layer_spacing
-
-    @layer_spacing.setter
-    def layer_spacing(self, value):
-        self._layer_spacing = value
-
-    @property
-    def layer_rotation_angles(self):
-        return self._layer_rotation_angles
-
-    @layer_rotation_angles.setter
-    def layer_rotation_angles(self, value):
-        self._layer_rotation_angles = value
-
-    @classmethod
-    def compute_layer_mass(cls, n=None, m=None, width=None, length=None,
-                           edge=None, element1=None, element2=None):
-        """Compute graphene layer mass in **grams**.
-
-        Parameters
-        ----------
-        n, m : int, optional
-        width, length : float, optional
-        edge : {'AC', 'ZZ'}
-        element1, element2 : str, optional
-
-        Returns
-        -------
-        mass : float
-
-        """
-        Natoms_per_layer = Graphene.compute_Natoms_per_layer(
-            n=n, m=m, element1=element1, element2=element2)
-
-        if element1 is None:
-            element1 = 'C'
-        if element2 is None:
-            element2 = 'C'
-
-        atom1 = Atom(element1)
-        atom2 = Atom(element2)
-
-        mass = Natoms_per_layer * (atom1.m + atom2.m) / 2
-
-        return mass
