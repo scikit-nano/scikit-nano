4d5ca520657f12250d6abe276660d403d37c97ae
Andrew Merrill <androomerrill@gmail.com>
2014-09-04 22:20:02 -0400

    Updating __init__ module imports and docstrings.

--- sknano/structures/__init__.py
+++ sknano/structures/__init__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 """
 ===============================================================================
-Classes for abstract nano-structure objects (:mod:`sknano.structures`)
+Classes for abstract nanostructure objects (:mod:`sknano.structures`)
 ===============================================================================
 
 .. currentmodule:: sknano.structures
 
-This package defines the base classes for creating
-abstract object representations of nano-structures
+This package defines classes for creating
+abstract object representations of nanostructures
 including fullerenes, graphene, and nanotubes.
 
 See the specific class doc pages below for detailed documentation on its use.
@@ -15,26 +15,89 @@ See the specific class doc pages below for detailed documentation on its use.
 Contents
 ========
 
-Classes for creating abstract object representations of nano-structures
+Classes for creating abstract object representations of nanostructures
 --------------------------------------------------------------------------
 .. autosummary::
    :toctree: generated/
 
+   StructureBase
+
+   MWNTMixin
+   NanotubeBundleMixin
+   UnrolledSWNTMixin
+
    GraphenePrimitiveCell
    Graphene
    BilayerGraphene
-   Nanotube
-   NanotubeBundle
+
+   SWNT
+   SWNTBundle
+   MWNT
+   MWNTBundle
+
+Functions for computing nanostructure properties
+--------------------------------------------------------------------------
+.. autosummary::
+   :toctree: generated/
+
+   compute_d
+   compute_dR
+   compute_N
+   compute_t1
+   compute_t2
+   compute_Ch
+   compute_chiral_angle
+   compute_T
+   compute_dt
+   compute_rt
+   compute_M
+   compute_R
+   compute_R_chiral_angle
+   compute_so
+   compute_psi
+   compute_tau
+   compute_Lz
+   compute_electronic_type
+   compute_Natoms_per_unit_cell
+   compute_Natoms_per_tube
+   compute_Natoms
+   compute_unit_cell_mass
+   compute_linear_mass_density
+   compute_tube_mass
+   compute_bundle_density
+
+Helper functions and data structures related to :math:`(n, m)` chirality data
+-------------------------------------------------------------------------------
+.. autosummary::
+   :toctree: generated/
+
+   cmp_Ch
+   filter_Ch
+   filter_Ch_list
+   generate_Ch_list
+   generate_Ch_property_grid
+   get_Ch_indices
+   get_Ch_type
+   map_Ch
+   chiral_type_name_mappings
+   Ch_types
+   filter_key_type_mappings
 
 """
 from __future__ import absolute_import, division, print_function
 __docformat__ = 'restructuredtext en'
 
+from ._compute_funcs import *
+
+from ._base import *
 from ._extras import *
-#from ._fullerenes import *
-from ._graphene import *
-from ._bilayer_graphene import *
 from ._mixins import *
+
+#from ._fullerenes import *
+
+from ._bilayer_graphene import *
+from ._graphene import *
+
 from ._swnt import *
 from ._swnt_bundle import *
 from ._mwnt import *
