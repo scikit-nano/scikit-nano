1f9d9431c776fd692e8ccf3008a9fc23cfda4263
Andrew Merrill <androomerrill@gmail.com>
2014-09-08 17:06:58 -0400

    adding new utility sub-package for molecular dynamics

---
+++ sknano/utils/mdtools/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+===================================================================
+Utilities for molecular dynamics (:mod:`sknano.utils.mdtools`)
+===================================================================
+
+.. currentmodule:: sknano.utils.mdtools
+
+Contents
+========
+
+Classes for performing molcular dynamics runs
+------------------------------------------------
+.. autosummary::
+   :toctree: generated/
+
+
+Helper functions
+------------------
+.. autosummary::
+   :toctree: generated/
+
+"""
+from __future__ import absolute_import, division, print_function
+
+__docformat__ = 'restructuredtext en'
+
+__all__ = [s for s in dir() if not s.startswith('_')]
