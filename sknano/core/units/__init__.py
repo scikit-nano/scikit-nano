1b50568997ec821a704a9b8b4579a0db8ad95d53
Andrew Merrill <androomerrill@gmail.com>
2014-09-09 19:21:48 -0400

    Updating sknano.core.units __init__ module docstring

--- sknano/core/units/__init__.py
+++ sknano/core/units/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 """
 =====================================================================
-Units package (:mod:`sknano.core.units._units`)
+Units package (:mod:`sknano.core.units`)
 =====================================================================
 
-.. currentmodule:: sknano.core.units._units
+.. currentmodule:: sknano.core.units
 
 """
 from __future__ import absolute_import, division, print_function
