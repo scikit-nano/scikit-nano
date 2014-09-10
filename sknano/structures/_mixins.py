714005a5713ba0243aec2639f14712134d961f35
Andrew Merrill <androomerrill@gmail.com>
2014-09-10 13:16:49 -0400

    Moving non-SWNT specific properties to NanotubeMixin class.
    Created a new NanotubeMixin class in the _mixins module.
    Moved all property decorator methods that were not SWNT
    specific to the NanotubeMixin class.

--- sknano/structures/_mixins.py
+++ sknano/structures/_mixins.py
@@ -15,8 +15,299 @@ import numbers
 import numpy as np
 
 from sknano.core.math import Vector
+from ._compute_funcs import *
+from ._extras import get_Ch_type
 
-__all__ = ['MWNTMixin', 'NanotubeBundleMixin', 'UnrolledSWNTMixin']
+__all__ = ['MWNTMixin', 'NanotubeMixin', 'NanotubeBundleMixin',
+           'UnrolledSWNTMixin']
+
+
+class NanotubeMixin(object):
+
+    @property
+    def n(self):
+        """Chiral index :math:`n`.
+
+        The component of the chiral vector :math:`\\mathbf{C}_h`
+        along :math:`\\mathbf{a}_1`:
+
+        .. math::
+
+           \\mathbf{C}_h = n\\mathbf{a}_1 + m\\mathbf{a}_2 = (n, m)
+
+        """
+        return self._n
+
+    @n.setter
+    def n(self, value):
+        """Set chiral index :math:`n`"""
+        if not (isinstance(value, numbers.Real) or value >= 0):
+            raise TypeError('Expected an integer.')
+        self._n = int(value)
+
+    @n.deleter
+    def n(self):
+        del self._n
+
+    @property
+    def m(self):
+        """Chiral index :math:`m`.
+
+        The component of the chiral vector :math:`\\mathbf{C}_h`
+        along :math:`\\mathbf{a}_2`:
+
+        .. math::
+
+           \\mathbf{C}_h = n\\mathbf{a}_1 + m\\mathbf{a}_2 = (n, m)
+
+        """
+        return self._m
+
+    @m.setter
+    def m(self, value):
+        """Set chiral index :math:`m`"""
+        if not (isinstance(value, numbers.Real) or value >= 0):
+            raise TypeError('Expected an integer.')
+        self._m = int(value)
+
+    @m.deleter
+    def m(self):
+        del self._m
+
+    @property
+    def d(self):
+        """:math:`d=\\gcd{(n, m)}`
+
+        :math:`d` is the **G**\ reatest **C**\ ommon **D**\ ivisor of
+        :math:`n` and :math:`m`.
+
+        """
+        return compute_d(self.n, self.m)
+
+    @property
+    def dR(self):
+        """:math:`d_R=\\gcd{(2n + m, 2m + n)}`
+
+        :math:`d_R` is the **G**\ reatest **C**\ ommon **D**\ ivisor of
+        :math:`2n + m` and :math:`2m + n`.
+
+        """
+        return compute_dR(self.n, self.m)
+
+    @property
+    def N(self):
+        """Number of graphene hexagons in nanotube *unit cell*.
+
+        .. math::
+
+           N = \\frac{4(n^2 + m^2 + nm)}{d_R}
+
+        """
+        return compute_N(self.n, self.m)
+
+    @property
+    def t1(self):
+        """:math:`t_{1} = \\frac{2m + n}{d_{R}}`
+
+        where :math:`d_R = \\gcd{(2n + m, 2m + n)}`.
+
+        The component of the translation vector :math:`\\mathbf{T}`
+        along :math:`\\mathbf{a}_1`:
+
+        .. math::
+
+           \\mathbf{T} = t_1\\mathbf{a}_{1} + t_2\\mathbf{a}_2
+
+        """
+        return compute_t1(self.n, self.m)
+
+    @property
+    def t2(self):
+        """:math:`t_2 = -\\frac{2n + m}{d_R}`
+
+        where :math:`d_R = \\gcd{(2n + m, 2m + n)}`.
+
+        The component of the translation vector :math:`\\mathbf{T}`
+        along :math:`\\mathbf{a}_2`:
+
+        .. math::
+
+           \\mathbf{T} = t_1\\mathbf{a}_1 + t_2\\mathbf{a}_2
+
+        """
+        return compute_t2(self.n, self.m)
+
+    @property
+    def Ch(self):
+        u"""SWNT circumference :math:`|\\mathbf{C}_h|` in **\u212b**"""
+        return compute_Ch(self.n, self.m, bond=self.bond)
+
+    @property
+    def chiral_angle(self):
+        """Chiral angle :math:`\\theta_c` in **degrees**.
+
+        .. math::
+
+           \\theta_c = \\tan^{-1}\\left(\\frac{\\sqrt{3} m}{2n + m}\\right)
+
+        """
+        return compute_chiral_angle(self.n, self.m)
+
+    @property
+    def chiral_type(self):
+        return get_Ch_type((self.n, self.m))
+
+    @property
+    def T(self):
+        u"""Length of nanotube unit cell :math:`|\\mathbf{T}|` in \u212b.
+
+        .. math::
+
+           |\\mathbf{T}| = \\frac{\\sqrt{3} |\\mathbf{C}_{h}|}{d_{R}}
+
+        """
+        return compute_T(self.n, self.m, bond=self.bond, length=True)
+
+    @property
+    def M(self):
+        """:math:`M = np - nq`
+
+        :math:`M` is the number of multiples of the translation vector
+        :math:`\\mathbf{T}` in the vector :math:`N\\mathbf{R}`.
+
+        """
+        return compute_M(self.n, self.m)
+
+    @property
+    def R(self):
+        """Symmetry vector :math:`\\mathbf{R} = (p, q)`.
+
+        .. math::
+
+           \\mathbf{R} = p\\mathbf{a}_1 + q\\mathbf{a}_2
+
+        """
+        return compute_R(self.n, self.m, bond=self.bond, length=False)
+
+    @property
+    def nz(self):
+        """Number of nanotube unit cells along the :math:`z`-axis."""
+        return self._nz
+
+    @nz.setter
+    def nz(self, value):
+        """Set number of nanotube unit cells along the :math:`z`-axis."""
+        if not (isinstance(value, numbers.Real) or value > 0):
+            raise TypeError('Expected a real, positive number.')
+        self._nz = value
+        if self._assume_integer_unit_cells:
+            self._nz = int(np.ceil(value))
+
+    @property
+    def electronic_type(self):
+        """SWNT electronic type.
+
+        .. versionadded:: 0.2.7
+
+        The electronic type is determined as follows:
+
+        if :math:`(2n + m)\\,\\mathrm{mod}\\,3=0`, the nanotube is
+        **metallic**.
+
+        if :math:`(2n + m)\\,\\mathrm{mod}\\,3=1`, the nanotube is
+        **semiconducting, type 1**.
+
+        if :math:`(2n + m)\\,\\mathrm{mod}\\,3=2`, the nanotube is
+        **semiconducting, type 2**.
+
+        The :math:`x\\,\\mathrm{mod}\\,y` notation is mathematical
+        shorthand for the *modulo* operation, which computes the
+        **remainder** of the division of :math:`x` by :math:`y`.
+        So, for example, all *armchair* nanotubes must be metallic
+        since the chiral indices satisfy: :math:`2n + m = 2n + n = 3n` and
+        therefore :math:`3n\\,\\mathrm{mod}\\,3` i.e. the remainder of the
+        division of :math:`3n/3=n` is always zero.
+
+        .. note::
+           Mathematically, :math:`(2n + m)\\,\\mathrm{mod}\\,3` is equivalent
+           to :math:`(n - m)\\,\\mathrm{mod}\\,3` when distinguishing
+           between metallic and semiconducting. However, when
+           distinguishing between semiconducting types,
+           one must be careful to observe the following convention:
+
+           * Semiconducting, **type 1** means:
+
+             * :math:`(2n + m)\\,\\mathrm{mod}\\,3=1`
+             * :math:`(n - m)\\,\\mathrm{mod}\\,3=2`
+
+           * Semiconducting, **type 2** means:
+
+             * :math:`(2n + m)\\,\\mathrm{mod}\\,3=2`
+             * :math:`(n - m)\\,\\mathrm{mod}\\,3=1`
+
+        """
+        return compute_electronic_type(self.n, self.m)
+
+    @property
+    def Natoms(self):
+        """Number of atoms in nanotube.
+
+        .. versionchanged:: 0.3.0
+
+           **Returns total number of atoms per nanotube.**
+           Use :attr:`~SWNT.Natoms_per_unit_cell` to get the number of
+           atoms per unit cell.
+
+        .. math::
+
+           N_{\\mathrm{atoms}} = 2N\\times n_z =
+           \\frac{4(n^2 + m^2 + nm)}{d_R}\\times n_z
+
+        where :math:`N` is the number of graphene hexagons mapped to the
+        nanotube unit cell and :math:`n_z` is the number of unit cells.
+
+        """
+        return compute_Natoms(self.n, self.m, self.nz)
+
+    @property
+    def Natoms_per_unit_cell(self):
+        """Number of atoms in nanotube unit cell.
+
+        .. math::
+
+           N_{\\mathrm{atoms}} = 2N = \\frac{4(n^2 + m^2 + nm)}{d_R}
+
+        where :math:`N` is the number of graphene hexagons mapped to the
+        nanotube unit cell.
+
+        """
+        return compute_Natoms_per_unit_cell(self.n, self.m)
+
+    @property
+    def unit_cell_mass(self):
+        """Unit cell mass in atomic mass units."""
+        return compute_unit_cell_mass(self.n, self.m,
+                                      element1=self.element1,
+                                      element2=self.element2)
+
+    @property
+    def Lz(self):
+        """SWNT length :math:`L_z = L_{\\mathrm{tube}}` in
+        **nanometers**."""
+        return self.nz * self.T
+
+    @property
+    def fix_Lz(self):
+        return self._fix_Lz
+
+    @fix_Lz.setter
+    def fix_Lz(self, value):
+        if not isinstance(value, bool):
+            raise TypeError('Expected `True` or `False`')
+        self._fix_Lz = value
+        self._assume_integer_unit_cells = True
+        if self.fix_Lz:
+            self._assume_integer_unit_cells = False
 
 
 class MWNTMixin(object):
@@ -157,6 +448,15 @@ class NanotubeBundleMixin(object):
 
 
 class UnrolledSWNTMixin(object):
+
+    @property
+    def Lx(self):
+        return self.nx * self.Ch / 10
+
+    @property
+    def Ly(self):
+        return self.ny * self.layer_spacing / 10
+
     @property
     def nx(self):
         """Number of nanotubes along the :math:`x`-axis."""
