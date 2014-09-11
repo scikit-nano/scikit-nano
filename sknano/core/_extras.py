7e1cb7e5be1355c127fd83e07133db8afc3c6bd1
Andrew Merrill <androomerrill@gmail.com>
2014-09-11 02:04:02 -0400

    adding an AttrDict class

--- sknano/core/_extras.py
+++ sknano/core/_extras.py
@@ -10,6 +10,12 @@ misc core functions, preset data structures, etc. (:mod:`sknano.core._extras`)
 from __future__ import absolute_import, division, print_function
 __docformat__ = 'restructuredtext en'
 
-__all__ = ['components', 'dimensions', 'xyz', 'xyz_axes']
+__all__ = ['components', 'dimensions', 'xyz', 'xyz_axes', 'AttrDict']
 
 components = dimensions = xyz = xyz_axes = ('x', 'y', 'z')
+
+
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
