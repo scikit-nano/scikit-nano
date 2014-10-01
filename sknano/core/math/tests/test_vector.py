2563869a372ca651333364a7f3e1cc981317c467
Andrew Merrill <androomerrill@gmail.com>
2014-10-01 09:12:12 -0400

    adding unit vector tests

--- sknano/core/math/tests/test_vector.py
+++ sknano/core/math/tests/test_vector.py
@@ -185,6 +185,21 @@ def test_3D_cross():
     assert_false(np.allclose(v3.p0, v2.p0))
 
 
+def test_unit_vector_cross():
+    e1 = Vector([1, 0, 0])
+    e2 = Vector([0, 1, 0])
+    e3 = Vector([0, 0, 1])
+
+    assert_true(np.allclose(vec.cross(e1, e2), e3))
+    assert_equal(vec.cross(e1, e2), e3)
+
+    assert_true(np.allclose(vec.cross(e1, e3), -e2))
+    assert_equal(vec.cross(e1, e3), -e2)
+
+    assert_true(np.allclose(vec.cross(e2, e3), e1))
+    assert_equal(vec.cross(e2, e3), e1)
+
+
 def test_transforms():
     v = Vector([1.0, 0.0, 0.0])
     v.rotate(np.pi/2, rot_axis='z')
