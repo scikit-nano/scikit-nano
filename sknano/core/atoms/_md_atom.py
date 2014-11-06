4891098ae9ea9c68fbabcc184b776a8ab5916b5f
Andrew Merrill <androomerrill@gmail.com>
2014-11-06 14:57:24 -0500

    Adding new modules for developing new MDAtom/MDAtoms classes.
    MDAtom/MDAtoms will be derived from the KDTAtom/KDTAtoms
    classes and will be developed for analyzing the trajectories
    of structure data atoms from molecular dynamics simulations.

---
+++ sknano/core/atoms/_md_atom.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+===============================================================================
+Atom class for MD analysis (:mod:`sknano.core.atoms._md_atom`)
+===============================================================================
+
+An `Atom` class for molecular dynamics structure analysis.
+
+.. currentmodule:: sknano.core.atoms._md_atom
+
+"""
+from __future__ import absolute_import, division, print_function
+__docformat__ = 'restructuredtext en'
+
+#import numbers
+
+#from ._bonds import Bonds
+from ._kdtree_atom import KDTAtom
+#from ._neighbor_atoms import NeighborAtoms
+
+__all__ = ['MDAtom']
+
+
+class MDAtom(KDTAtom):
+    """An `Atom` class for molecular dynamics structure analysis.
+
+    Parameters
+    ----------
+
+    """
+    def __init__(self, **kwargs):
+        super(MDAtom, self).__init__(**kwargs)
+
+    def __str__(self):
+        """Return a nice string representation of `MDAtom`."""
+        return super(MDAtom, self).__str__()
+
+    def __repr__(self):
+        """Return canonical string representation of `MDAtom`."""
+        #strrep = "Atom(element={element!r}, atomID={atomID!r}, " + \
+        #    "moleculeID={moleculeID!r}, atomtype={atomtype!r}, " + \
+        #    "q={q!r}, m={m!r}, x={x:.6f}, y={y:.6f}, z={z:.6f}, " + \
+        #    "CN={CN!r}, NN={NN!r})"
+        #parameters = dict(element=self.element, atomID=self.atomID,
+        #                  moleculeID=self.moleculeID, atomtype=self.atomtype,
+        #                  q=self.q, m=self.m, x=self.x, y=self.y, z=self.z,
+        #                  CN=self.CN, NN=self.NN)
+        #return strrep.format(**parameters)
+        return super(MDAtom, self).__repr__()
---
+++ sknano/core/atoms/_md_atoms.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+===============================================================================
+Atoms class for MD analysis (:mod:`sknano.core.atoms._md_atoms`)
+===============================================================================
+
+An `Atoms` class for molecular dynamics structure analysis.
+
+.. currentmodule:: sknano.core.atoms._md_atoms
+
+"""
+from __future__ import absolute_import, division, print_function
+__docformat__ = 'restructuredtext en'
+
+#import numbers
+#import numpy as np
+
+#from sknano.core.math import Vector, vector as vec
+#from ._atom_bonds import AtomBonds
+#from ._bond import Bond
+#from ._bonds import Bonds
+#from ._extended_atoms import XAtoms
+#from ._neighbor_atoms import NeighborAtoms
+from ._kdtree_atoms import KDTAtoms
+
+__all__ = ['MDAtoms']
+
+
+class MDAtoms(KDTAtoms):
+    """An `Atoms` class for molecular dynamics structure analysis."""
+    def __init__(self, **kwargs):
+        super(MDAtoms, self).__init__(**kwargs)
