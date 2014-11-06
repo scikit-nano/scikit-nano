26a2f3ae80eeaec01e9267172adf03f00cbcea4a
Andrew Merrill <androomerrill@gmail.com>
2014-11-06 14:33:13 -0500

    adding test modules for sknano.core.molecules package

---
+++ sknano/core/molecules/tests/test_molecule.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+
+from __future__ import absolute_import, division, print_function
+
+import nose
+from nose.tools import *
+#from sknano.core.atoms import Atom
+from sknano.core.molecules import Molecule
+
+
+def test_instantiation():
+    m = Molecule()
+    assert_is_instance(m, Molecule)
+
+
+if __name__ == '__main__':
+    nose.runmodule()
---
+++ sknano/core/molecules/tests/test_molecules.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+from __future__ import absolute_import, division, print_function
+
+import nose
+from nose.tools import *
+#from sknano.core.atoms import Atom, Atoms
+from sknano.core.molecules import Molecules
+#from sknano.testing import generate_atoms
+
+
+def test_instantiation():
+    molecules = Molecules()
+    assert_is_instance(molecules, Molecules)
+
+
+if __name__ == '__main__':
+    nose.runmodule()
