6ee9f659c33bdf811141c470ebb5f75c5d2660ab
Andrew Merrill <androomerrill@gmail.com>
2014-11-06 20:32:09 -0500

    Updating sknano.testing package docstrings and module imports'

--- sknano/testing/__init__.py
+++ sknano/testing/__init__.py
@@ -8,15 +8,26 @@ Helper functions for testing and development (:mod:`sknano.testing`)
 
 Contents
 ========
+
+Helper functions for generating test data
+------------------------------------------
 .. autosummary::
    :toctree: generated/
 
    generate_atoms
+
+Test fixtures
+--------------
+.. autosummary::
+   :toctree: generated/
+
+   GeneratorTestFixtures
 
 """
 from __future__ import absolute_import, division, print_function
 __docformat__ = 'restructuredtext en'
 
 from ._funcs import *
+from ._tools import *
 
 __all__ = [s for s in dir() if not s.startswith('_')]
