cc0a51d2949c45809ddc653de52da4002f9a33a9
Andrew Merrill <androomerrill@gmail.com>
2014-11-11 12:04:09 -0500

    updating lammps_dump_format test module

--- sknano/io/tests/test_lammps_dump_format.py
+++ sknano/io/tests/test_lammps_dump_format.py
@@ -11,14 +11,12 @@ from sknano.io import DUMPData, DUMPReader, DUMPWriter
 #def test_reader():
 #    dumpfile = resource_filename('sknano', 'data/lammpstrj/dump.peptide')
 #    dumpdata = DUMPReader(dumpfile)
-#    print('dumpdata.nsnaps: {}'.format(dumpdata.nsnaps))
 
 
 def test_dump():
     #dumpfile = resource_filename('sknano', 'data/lammpstrj/dump.peptide')
     dumpfile = resource_filename('sknano', 'data/lammpstrj/1010+ion.dump')
-    dumpdata = DUMPData(dumpfile)
-    #print('dumpdata.nsnaps: {}'.format(dumpdata.nsnaps))
+    traj = DUMPData(dumpfile).trajectory
 
 
 if __name__ == '__main__':
