07664e34b3724479d45faf8cf6a404f6dbd9743d
Andrew Merrill <androomerrill@gmail.com>
2014-11-11 03:17:34 -0500

    Adding imports from _poav_atom and _poav_atoms modules.

--- sknano/core/atoms/__init__.py
+++ sknano/core/atoms/__init__.py
@@ -14,10 +14,24 @@ Contents
 
    Atom
    Atoms
+
    XAtom
    XAtoms
+
    KDTAtom
    KDTAtoms
+
+   POAVAtom
+   POAVAtoms
+
+   StructureAtom
+   StructureAtoms
+
+   Bond
+   Bonds
+
+   Trajectory
+   Snapshot
 
 """
 from __future__ import absolute_import, division, print_function
@@ -33,7 +47,9 @@ from ._extended_atoms import *
 from ._kdtree_atom import *
 from ._kdtree_atoms import *
 from ._neighbor_atoms import *
-#from ._mixins import *
+from ._poav_atom import *
+from ._poav_atoms import *
+from ._structure_atoms import *
 from ._trajectory import *
 
 __all__ = [s for s in dir() if not s.startswith('_')]
