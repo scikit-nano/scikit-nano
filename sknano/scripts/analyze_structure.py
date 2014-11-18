52e9f9889136d6e0f7cd0162eb7398f72ae08e3a
Andrew Merrill <androomerrill@gmail.com>
2014-11-18 09:29:40 -0500

    Suspending further development on analyze_structure script.
    The analysis code was becoming too customized and tailored to
    a specific application, which is inconsistent with the
    intent of the scikit-nano scripts. The scripts should provide
    a command-line interface for executing the functions and classes
    that are utilized in an interactive session, or to run any
    GUI apps, which is what the `nanogen` and `nanogenui` scripts
    do, respectively.
    
    If I further develop the `StructureAnalyzer` class with general
    structure analysis methods, then this script should be developed
    further as a CLI to that class.

--- sknano/scripts/analyze_structure.py
+++ sknano/scripts/analyze_structure.py
@@ -9,13 +9,15 @@ Command line script (:mod:`sknano.scripts.analyze_structure`)
 
 """
 from __future__ import absolute_import, division, print_function
+
+from collections import OrderedDict
 import argparse
 import os
 import sys
 
 import numpy as np
 
-from sknano.io import DATAReader
+from sknano.io import StructureReader
 from sknano.structures import get_Ch_indices
 
 __all__ = ['analyze_structure']
@@ -24,18 +26,19 @@ __all__ = ['analyze_structure']
 def argparser():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--cfg', '--config', metavar='CONFIG_FILE',
-                        default=None, help='config file with analysis '
+    parser.add_argument('--cfg', '--config', dest='analysis_config',
+                        metavar='CONFIG_FILE', default=None,
+                        help='config file with analysis '
                         'settings. (default: %(default)s)')
     parser.add_argument(
         '--structure-format', default=None, choices=('data', 'dump', 'xyz'),
         help='input structure data file format. If `None`, then guess '
         'format from file name. (default: %(default)s)')
-    parser.add_argument('structure_file', help='input structure data file.')
     subparsers = parser.add_subparsers(title='sub-commands')
 
     poav_parser = subparsers.add_parser('POAV')
-    poav_parser.add_argument('--atom-ids', metavar='ATOM_ID', nargs='+',
+    poav_parser.add_argument('--atom-ids', metavar='ATOM_ID', type=int,
+                             nargs='+',
                              help='One or more atom IDs to analyze. '
                              '(default: %(default)s)')
     poav_parser.add_argument('--include-NN', action='store_true',
@@ -44,11 +47,19 @@ def argparser():
                              '(default: %(default)s)')
     poav_parser.set_defaults(analyze='POAV')
 
+    parser.add_argument('structure_file',
+                        help='input structure data files.')
+
     return parser
 
 
 def analyze_structure(analyze=None, analysis_config=None,
-                      structure_format=None, **kwargs):
+                      structure_format=None, structure_file=None,
+                      **kwargs):
+    pass
+
+
+def analyze_POAVs(atoms, atom_ids=None, include_NN=False):
     pass
 
 
