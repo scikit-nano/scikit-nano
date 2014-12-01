fed02dd021a40909de15c54716379ff2b726adbb
Andrew Merrill <androomerrill@gmail.com>
2014-12-01 10:42:28 -0500

    Large rewrite of sknano.core.math._transforms module.
    Rewrote transformation_matrix function to implement
    Vector and Point objects in calculations.

--- sknano/core/math/_transforms.py
+++ sknano/core/math/_transforms.py
@@ -7,8 +7,14 @@ Functions for linear algebra transforms (:mod:`sknano.core.math._transforms`)
 .. currentmodule:: sknano.core.math._transforms
 
 """
-from __future__ import absolute_import, division, print_function
+from __future__ import absolute_import, division, print_function, \
+    unicode_literals
+
+from itertools import combinations
+
 import six
+from six.moves import range
+
 __docformat__ = 'restructuredtext en'
 
 import numpy as np
@@ -93,94 +99,127 @@ def transformation_matrix(angle=None, rot_axis=None, anchor_point=None,
     -----
 
     """
-    if deg2rad:
-        angle = np.radians(angle)
+    if angle is None and from_vector is None and to_vector is None:
+        raise TypeError('Expected `angle` or `from_vector` and `to_vector` '
+                        'set to values with valid types.')
 
-    cosa = np.cos(angle)
-    sina = np.sin(angle)
+    from . import vector, Vector, Point
 
-    # Handle 2D rotation about origin
-    if rot_axis is None and anchor_point is None:
-        return Rz(angle)
+    if angle is None and not (from_vector is None and to_vector is None):
+        from_vector = Vector(from_vector)
+        to_vector = Vector(to_vector)
+        if from_vector.nd != to_vector.nd:
+            raise ValueError('`from_vector` and `to_vector` must have same '
+                             'dimensions.')
+        angle = vector.angle(from_vector, to_vector)
+        if from_vector.nd == 2:
+            if not np.allclose(Vector(np.dot(Rz(angle)[:2,:2], from_vector)),
+                               to_vector):
+                angle = -angle
 
-    # Handle 3D rotation about origin
-    # Handle 3D rotation around the rotation vector
-    # Handle N-D rotation about origin
-    # Handle N-D rotation around the N-D rotation vector anchored at
-    # an arbitrary N-D point.
-    if rot_axis is not None and isinstance(rot_axis, (str, six.text_type)):
-        try:
-            rot_axis = _str2array[rot_axis]
-        except KeyError:
-            raise ValueError(
-                'Invalid `rot_axis` string: {}'.format(rot_axis))
-    elif not isinstance(rot_axis, (tuple, list, np.ndarray)):
-        raise ValueError('`rot_axis` must be a sequence')
+            return transformation_matrix(angle=angle, rot_point=rot_point)
 
-    if anchor_point is None:
-        anchor_point = np.zeros(len(rot_axis))
+        elif from_vector.nd == 3:
+            rot_axis = vector.cross(from_vector, to_vector)
 
-    nd = len(rot_axis)
-    if nd == 2:
-        Tmat = Rz(angle)
-        # Handle 2D rotation about arbitrary 2D point
-        if not np.allclose(rot_axis, np.zeros(2)):
-            a, b = rot_axis
-            m13 = a - a * cosa + b * sina
-            m23 = b - b * cosa - a * sina
-            Tmat[:2, 2] = np.array([m13, m23])
-    elif nd == 3:
-        Tmat = np.zeros((4, 4))
-
-        if np.allclose(rot_axis, I[:3, 0]):
-            Tmat[:3,:3] = Rx(angle)
-        elif np.allclose(rot_axis, I[:3, 1]):
-            Tmat[:3,:3] = Ry(angle)
-        elif np.allclose(rot_axis, I[:3, 2]):
-            Tmat[:3,:3] = Rz(angle)
+            return transformation_matrix(angle=angle, rot_axis=rot_axis,
+                                         anchor_point=anchor_point,
+                                         rot_point=rot_point)
         else:
-            u, v, w = rot_axis
-            uu, vv, ww = u**2, v**2, w**2
-            uv, uw, vw = u * v, u * w, v * w
-            l = np.sqrt(uu + vv + ww)
-            ll = l**2
+            raise ValueError('currently, transformation_matrices can only '
+                             'be computed for rotation transformations in '
+                             '2D and 3D.')
+    else:
+        if deg2rad:
+            angle = np.radians(angle)
 
-            m11 = (uu + (vv + ww) * cosa) / ll
-            m22 = (vv + (uu + ww) * cosa) / ll
-            m33 = (ww + (uu + vv) * cosa) / ll
-            m12 = (uv * (1 - cosa) - w * l * sina) / ll
-            m21 = (uv * (1 - cosa) + w * l * sina) / ll
-            m13 = (uw * (1 - cosa) + v * l * sina) / ll
-            m31 = (uw * (1 - cosa) - v * l * sina) / ll
-            m23 = (vw * (1 - cosa) - u * l * sina) / ll
-            m32 = (vw * (1 - cosa) + u * l * sina) / ll
+        cosa = np.cos(angle)
+        sina = np.sin(angle)
 
-            Tmat[:3,:3] = np.array([[m11, m12, m13],
-                                    [m21, m22, m23],
-                                    [m31, m32, m33]])
+        if rot_axis is None:
 
-        if not np.allclose(anchor_point, np.zeros(3)):
-            a, b, c = anchor_point
-            u, v, w = rot_axis
-            uu, vv, ww = u**2, v**2, w**2
-            au, bu, cu = a * u, b * u, c * u
-            av, bv, cv = a * v, b * v, c * v
-            aw, bw, cw = a * w, b * w, c * w
-            l = np.sqrt(uu + vv + ww)
-            ll = l**2
+            if rot_point is not None and \
+                    isinstance(rot_point, (list, np.ndarray)) and \
+                    len(rot_point) == 2:
+                p = Point(rot_point)
+            else:
+                p = Point(nd=2)
 
-            m14 = ((a * (vv + ww) - u * (bv + cw)) * (1 - cosa) +
-                   (bw - cv) * l * sina) / ll
-            m24 = ((b * (uu + ww) - v * (au + cw)) * (1 - cosa) +
-                   (cu - aw) * l * sina) / ll
-            m34 = ((c * (uu + vv) - w * (au + bv)) * (1 - cosa) +
-                   (av - bu) * l * sina) / ll
+            Tmat = Rz(angle)
 
-            Tmat[:, 3] = np.array([m14, m24, m34, 1.0])
+            if not np.allclose(p, np.zeros(2)):
+                for i in range(2):
+                    j, = list(set(range(2)) - {i})
+                    Tmat[i, 2] = p[i] - p[i] * cosa + (-1)**i * p[j] * sina
 
-    Tmat[np.where(np.abs(Tmat) <= np.finfo(float).eps)] = 0.0
+        else:
+            # Handle 3D rotation about origin
+            # Handle 3D rotation around the rotation vector
+            # Handle N-D rotation about origin
+            # Handle N-D rotation around the N-D rotation vector anchored at
+            # an arbitrary N-D point.
 
-    return Tmat
+            if isinstance(rot_axis, (str, six.text_type)):
+                try:
+                    rot_axis = _str2array[rot_axis]
+                except KeyError:
+                    raise ValueError(
+                        'Invalid `rot_axis` string: {}'.format(rot_axis))
+            elif not isinstance(rot_axis, (tuple, list, np.ndarray)):
+                raise ValueError('`rot_axis` must be a sequence')
+
+            if anchor_point is None and rot_point is not None:
+                anchor_point = rot_point
+
+            if anchor_point is not None and \
+                    isinstance(anchor_point, (list, np.ndarray)) and \
+                    len(anchor_point) == 3:
+                anchor_point = Point(anchor_point)
+            else:
+                anchor_point = Point()
+
+            v = Vector(np.asarray(rot_axis), p0=anchor_point)
+
+            Tmat = np.zeros((4, 4))
+
+            if np.allclose(v, I[:3, 0]):
+                Tmat[:3,:3] = Rx(angle)
+            elif np.allclose(v, I[:3, 1]):
+                Tmat[:3,:3] = Ry(angle)
+            elif np.allclose(v, I[:3, 2]):
+                Tmat[:3,:3] = Rz(angle)
+            else:
+                for i in range(3):
+                    Tmat[i, i] = (v[i]**2 +
+                                  (v[(i + 1) % 3]**2 +
+                                   v[(i + 2) % 3]**2) * cosa) / v.norm**2
+
+                for i, j in combinations(list(range(3)), 2):
+                    k = list(set(range(3)) - {i, j})[0]
+                    Tmat[i, j] = \
+                        (v[i] * v[j] * (1 - cosa) +
+                         (-1)**(i + j) * v[k] * v.norm * sina) / v.norm**2
+                    Tmat[j, i] = \
+                        (v[i] * v[j] * (1 - cosa) -
+                         (-1)**(i + j) * v[k] * v.norm * sina) / v.norm**2
+
+            if not np.allclose(v.p0, np.zeros(3)):
+
+                p = v.p0
+
+                for i in range(3):
+                    j, k = list(set(range(3)) - {i})
+                    Tmat[i, 3] = ((p[i] * (v[j]**2 + v[k]**2) -
+                                   v[i] * (p[j] * v[j] + p[k] * v[k])) *
+                                  (1 - cosa) + (-1)**i *
+                                  (p[j] * v[k] - p[k] * v[j]) *
+                                  v.norm * sina) / v.norm**2
+
+            Tmat[3, 3] = 1.0
+
+        Tmat[np.where(np.abs(Tmat) <= np.finfo(float).eps)] = 0.0
+
+        return Tmat
 
 
 def affine_transform(angle=None, rot_axis=None, anchor_point=None,
@@ -350,8 +389,31 @@ def Rz(angle, deg2rad=False):
     return Rmat
 
 
-def reflection_matrix():
-    pass
+def reflection_matrix(v):
+    """Generate reflection matrix that represents reflection of points \
+        in a mirror normal to the vector `v`.
+
+    Parameters
+    ----------
+    v : :class:`~sknano.core.math.Vector`
+
+    Returns
+    -------
+    :class:`~numpy:numpy.ndarray`
+
+    """
+    from ._vector import Vector
+    v = Vector(v)
+
+    Rmat = np.zeros((v.nd, v.nd))
+    for i in range(v.nd):
+        Rmat[i, i] = 1 - 2 * v[i]**2 / v.norm**2
+
+    for i, j in combinations(list(range(v.nd)), 2):
+        Rmat[i, j] = Rmat[j, i] = -2 * v[i] * v[j] / v.norm**2
+
+    Rmat[np.where(np.abs(Rmat) <= np.finfo(float).eps)] = 0.0
+    return Rmat
 
 
 def rotation_matrix(angle=None, rot_axis=None, anchor_point=None,
@@ -426,26 +488,11 @@ def scaling_matrix(s=None, v=None):
         v = Vector(v)
 
         Smat = np.zeros((v.nd, v.nd))
+        for i in range(v.nd):
+            Smat[i, i] = 1 + (s - 1) * v[i]**2 / v.norm**2
 
-        if v.nd == 2:
-            s11 = s * v.x**2 + v.y**2
-            s22 = v.x**2 + s * v.y**2
-            s12 = s21 = (s - 1) * v.x * v.y
-            Smat[:, :] = 1 / v.norm**2 * np.array([[s11, s12],
-                                                   [s21, s22]])
-        else:
-
-            s11 = 1 + (s - 1) * v.x**2 / v.norm**2
-            s22 = 1 + (s - 1) * v.y**2 / v.norm**2
-            s33 = 1 + (s - 1) * v.z**2 / v.norm**2
-
-            s12 = s21 = (s - 1) * v.x * v.y / v.norm**2
-            s13 = s31 = (s - 1) * v.x * v.z / v.norm**2
-            s23 = s32 = (s - 1) * v.y * v.z / v.norm**2
-
-            Smat[:,:] = np.array([[s11, s12, s13],
-                                  [s21, s22, s23],
-                                  [s31, s32, s33]])
+        for i, j in combinations(list(range(v.nd)), 2):
+            Smat[i, j] = Smat[j, i] = (s - 1) * v[i]**2 * v[j]**2 / v.norm**2
 
         Smat[np.where(np.abs(Smat) <= np.finfo(float).eps)] = 0.0
         return Smat
