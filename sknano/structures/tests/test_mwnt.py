31ac6524011118ea0a5af6bbc3618d1bc6da83e5
Andrew Merrill <androomerrill@gmail.com>
2014-12-22 12:02:10 -0500

    updating `MWNT` test module

--- sknano/structures/tests/test_mwnt.py
+++ sknano/structures/tests/test_mwnt.py
@@ -28,8 +28,7 @@ def test2():
 
 
 def test3():
-    mwnt = MWNT(Ch=[(5, 5), (10, 10), (15, 15), (20, 20)],
-                Lz=2.5, fix_Lz=True)
+    mwnt = MWNT(Ch=[(5, 5), (10, 10), (15, 15), (20, 20)], Lz=2.5)
     print(mwnt)
     print(mwnt.todict())
     print(mwnt.chiral_types)
@@ -41,7 +40,7 @@ def test3():
 
 def test4():
     mwnt = MWNT(Ch=[(5, 0), (5, 5), (10, 5), (10, 0), (10, 10), (20, 10)],
-                Lz=1.0, fix_Lz=True)
+                Lz=1.0)
     print(mwnt)
     print(mwnt.todict())
     print(mwnt.chiral_types)
