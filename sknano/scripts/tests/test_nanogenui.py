bb2ed817ed3f931ad5db3081fe351b1222eadfec
Andrew Merrill <androomerrill@gmail.com>
2015-01-13 18:21:28 -0500

    adding test module for nanogenui script

---
+++ sknano/scripts/tests/test_nanogenui.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+
+from __future__ import absolute_import, division, print_function
+
+import subprocess
+import nose
+from nose.tools import *
+#from sknano.scripts.nanogenui import NanoGen
+
+
+def test_gui_launch():
+    gui_launch_successful = False
+    try:
+        subprocess.call("nanogenui", timeout=1)
+    except subprocess.TimeoutExpired:
+        gui_launch_successful = True
+    except OSError:
+        pass
+    assert_true(gui_launch_successful)
+
+
+if __name__ == '__main__':
+    nose.runmodule()
