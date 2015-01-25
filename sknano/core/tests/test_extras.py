9ccd2cd3be51fb1b6c5578f3ed372c2c1485a6a4
Andrew Merrill <androomerrill@gmail.com>
2015-01-24 23:23:12 -0500

    adding new test module

---
+++ sknano/core/tests/test_extras.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+
+import nose
+from nose.tools import *
+
+from sknano.core import cyclic_pairs, dedupe, rezero_array, ListBasedSet
+
+
+def test1():
+    lst = [1, 2, 2, 3, 4, 50, 50, 4, 5]
+    assert_equal(list(dedupe(lst)), [1, 2, 3, 4, 50, 5])
+
+
+def test2():
+    lst = ['a', 'b', 'c']
+    assert_equal(cyclic_pairs(lst), [('a', 'b'), ('b', 'c'), ('c', 'a')])
+
+
+def test3():
+    l1 = ListBasedSet('abcdef')
+    l2 = ListBasedSet('defghi')
+    assert_equal(list(l1 & l2), ['d', 'e', 'f'])
+
+
+if __name__ == '__main__':
+    nose.runmodule()
