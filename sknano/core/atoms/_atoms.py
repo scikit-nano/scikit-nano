65fef18444753972709f4b48c0af2adad97b5762
Andrew Merrill <androomerrill@gmail.com>
2015-01-27 18:41:37 -0500

    Adding docstring example code to `Atoms.filter` method.

--- sknano/core/atoms/_atoms.py
+++ sknano/core/atoms/_atoms.py
@@ -93,6 +93,29 @@ class Atoms(UserList):
             If `invert` is `True`, return the elements where `~condition`
             (i.e., numpy.invert(condition)) is `True`.
 
+        Examples
+        --------
+        An example using the structure data of a 10 nm `(10, 0)`
+        `SWCNT`:
+
+        >>> from sknano.generators import SWNTGenerator
+        >>> swnt = SWNTGenerator(10, 0, Lz=10, fix_Lz=True).atoms
+        >>> # select 'left', 'middle', 'right' atoms
+        >>> latoms = swnt.filter(swnt.z <= 25)
+        >>> matoms = swnt.filter((swnt.z < 75) & (swnt.z > 25))
+        >>> ratoms = swnt.filter(swnt.z >= 75)
+        >>> from pprint import pprint
+        >>> pprint([getattr(atoms, 'bounds') for atoms in
+        ...         (latoms, matoms, ratoms)])
+        [Cuboid(pmin=Point([-3.914435, -3.914435, 0.0]), \
+                pmax=Point([3.914435, 3.914435, 24.85])),
+         Cuboid(pmin=Point([-3.914435, -3.914435, 25.56]), \
+                pmax=Point([3.914435, 3.914435, 74.55])),
+         Cuboid(pmin=Point([-3.914435, -3.914435, 75.97]), \
+                pmax=Point([3.914435, 3.914435, 100.11]))]
+        >>> latoms.Natoms + matoms.Natoms + ratoms.Natoms == swnt.Natoms
+        True
+
         """
         if invert:
             condition = ~condition
