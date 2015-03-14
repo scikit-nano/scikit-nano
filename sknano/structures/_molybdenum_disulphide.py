ea391fa93598611a4a173f5338366eebc466bd19
Andrew Merrill <androomerrill@gmail.com>
2015-03-14 13:09:29 -0400

    Adding new module for MoS2.

---
+++ sknano/structures/_molybdenum_disulphide.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+===============================================================================
+MoS2 structure class (:mod:`sknano.structures._molybdenum_disulphide`)
+===============================================================================
+
+.. currentmodule:: sknano.structures._molybdenum_disulphide
+
+"""
+from __future__ import absolute_import, division, print_function
+__docformat__ = 'restructuredtext en'
+
+#import itertools
+import numbers
+
+import numpy as np
+
+#from sknano.core.atoms import Atom
+from sknano.core.math import Vector
+from ._base import StructureBase
+
+__all__ = ['MoS2']
+
+
+class MoS2(StructureBase):
+    """Molybdenum disulphide structure class.
+
+    Parameters
+    ----------
+
+    """
+
+    def __init__(self, nlayers=1, layer_spacing=3.14,
+                 sulphur_layer_spacing=3.01, **kwargs):
+
+        super().__init__(**kwargs)
+
+        self.layer_mass = None
+        self.Natoms = 0
+        self.Natoms_per_layer = 0
+
+        self.nlayers = nlayers
+        self.layer_spacing = layer_spacing
+
+        self.layer_shift = Vector()
+
+        self.cell = Vector()
