73bbb8a3017a27792ed612f25f7eb1ca932d63bc
Andrew Merrill <androomerrill@gmail.com>
2015-05-19 14:45:49 -0400

    Adding classmethod `from_pymatgen` to use pymatgen module.
    CrystalStructure.from_pymatgen is a wrapper to generate the
    CrystalStructure data from an instance of a
    pymatgen.Structure class.

--- sknano/core/crystallography/_structures.py
+++ sknano/core/crystallography/_structures.py
@@ -22,6 +22,7 @@ __docformat__ = 'restructuredtext en'
 
 # from sknano.core.math import Point, Vector
 
+from sknano.core.atoms import StructureAtom as Atom, StructureAtoms as Atoms
 from ._lattices import CrystalLattice
 
 __all__ = ['CrystalStructure', 'DiamondStructure',
@@ -32,16 +33,24 @@ __all__ = ['CrystalStructure', 'DiamondStructure',
 class CrystalStructure:
     """Abstract base class for crystal structures."""
 
-    def __init__(self, lattice, basis, coords=None, cartesian=False):
+    def __init__(self, lattice=None, basis=None, coords=None, cartesian=False):
+
+        if basis is None:
+            self._atoms = Atoms()
+        elif basis is not None:
+            self._atoms = Atoms(atoms=basis)
 
         self.lattice = lattice
         self.basis = basis
-
-        self.fmtstr = "{lattice!r}, {basis!r}"
+        self.fmtstr = "lattice={lattice!r}, basis={basis!r}"
 
     def __repr__(self):
         return "{}({})".format(self.__class__.__name__,
                                self.fmtstr.format(**self.todict()))
+
+    @property
+    def atoms(self):
+        return self._atoms
 
     @property
     def basis(self):
@@ -62,9 +71,43 @@ class CrystalStructure:
             value = CrystalLattice(cell_matrix=value)
         self._lattice = value
 
+    def __getattr__(self, name):
+        if name != '_atoms':
+            return getattr(self._atoms, name)
+
+    def __setattr__(self, name, value):
+        if name.startswith('_'):
+            super().__setattr__(name, value)
+        else:
+            setattr(self._atoms, name, value)
+
+    def __delattr__(self, name):
+        if name.startswith('_'):
+            super().__delattr__(name)
+        else:
+            delattr(self._atoms, name)
+
     @property
     def unit_cell(self):
         pass
+
+    @classmethod
+    def from_pymatgen(cls, lattice, species, coords, **kwargs):
+        try:
+            from pymatgen import Lattice, Structure
+        except ImportError as e:
+            print(e)
+        else:
+            atoms = Atoms()
+            if isinstance(lattice, CrystalLattice):
+                lattice = Lattice(lattice.cell_matrix)
+            structure = Structure(lattice, species, coords)
+            for site in structure.sites:
+                atoms.append(Atom(element=site.specie.symbol,
+                                  x=site.x, y=site.y, z=site.z))
+            return cls(lattice=CrystalLattice(
+                       cell_matrix=structure.lattice.matrix),
+                       basis=atoms)
 
     def todict(self):
         """Return `dict` of `CrystalStructure` parameters."""
