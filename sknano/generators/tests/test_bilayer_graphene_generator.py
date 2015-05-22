d343d3e65b387fdeb4cb3bbdb4b992117efc2e1c
Andrew Merrill <androomerrill@gmail.com>
2015-05-22 16:04:13 -0400

    Updating unittest for BilayerGrapheneGenerator.
    Implementing use of the `GeneratorTestFixtures` class to
    allow saving the generated structure data.

--- sknano/generators/tests/test_bilayer_graphene_generator.py
+++ sknano/generators/tests/test_bilayer_graphene_generator.py
@@ -6,10 +6,16 @@ from __future__ import unicode_literals
 import nose
 from nose.tools import *
 from sknano.generators import BilayerGrapheneGenerator
+from sknano.testing import GeneratorTestFixtures
 
-def test1():
-    blg = BilayerGrapheneGenerator(length=1, width=1, layer_rotation_angle=45)
 
+class TestCase(GeneratorTestFixtures):
+
+    def test1(self):
+        generator = BilayerGrapheneGenerator(length=1, width=1,
+                                             layer_rotation_angle=45)
+        generator.save_data()
+        self.tmpdata.append(generator.fname)
 
 
 if __name__ == '__main__':
