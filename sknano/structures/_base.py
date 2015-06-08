d37d82cd6a54fa952f20da2763f867d5a09dc89f
Andrew Merrill <androomerrill@gmail.com>
2015-06-08 14:25:43 -0400

    Adding `abstractmethod` `todict` as well as a `fmtstr` property.

--- sknano/structures/_base.py
+++ sknano/structures/_base.py
@@ -13,13 +13,16 @@ __docformat__ = 'restructuredtext en'
 
 # import numbers
 
-from sknano.core.crystallography import StructureBase as XtalBase
+from abc import ABCMeta, abstractmethod
+
+# from sknano.core.crystallography import StructureBase as XtalBase
+from sknano.core.atoms import BasisAtom
 from sknano.core.refdata import aCC
 
 __all__ = ['StructureBase']
 
 
-class StructureBase:
+class StructureBase(metaclass=ABCMeta):
     """Base class for creating abstract representation of nano structure.
 
     Parameters
@@ -29,21 +32,18 @@ class StructureBase:
         :class:`~sknano.core.atoms.Atom` 1 and 2
 
     """
-    def __init__(self, *args, bond=None, verbose=False, debug=False,
-                 **kwargs):
+    def __init__(self, *args, bond=None, verbose=False, debug=False, **kwargs):
         if bond is None:
             bond = aCC
         self.bond = bond
         self.verbose = verbose
         self.debug = debug
 
-        # self.fmtstr = "{lattice!r}, {basis!r}, {coords!r}, cartesian=True"
-        super().__init__(*args, **kwargs)
+        self.fmtstr = ""
 
-
-    # def __repr__(self):
-    #     return "{}({})".format(self.__class__.__name__,
-    #                            self.fmtstr.format(**self.todict()))
+    def __repr__(self):
+        return "{}({})".format(self.__class__.__name__,
+                               self.fmtstr.format(**self.todict()))
 
     def __getattr__(self, name):
         try:
@@ -51,12 +51,30 @@ class StructureBase:
         except AttributeError:
             return super().__getattr__(name)
 
+    @property
+    def fmtstr(self):
+        return self._fmtstr
+
+    @fmtstr.setter
+    def fmtstr(self, value):
+        self._fmtstr = value
 
     @property
     def element1(self):
         return self.basis[0].symbol
 
+    @element1.setter
+    def element1(self, value):
+        self.basis[0] = value
+
     @property
     def element2(self):
         return self.basis[1].symbol
 
+    @element2.setter
+    def element2(self, value):
+        self.basis[1] = value
+
+    @abstractmethod
+    def todict(self):
+        return NotImplementedError
