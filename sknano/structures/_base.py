ecced4d825259283ac3520f9a633cc4e7d5be5b9
Andrew Merrill <androomerrill@gmail.com>
2015-06-08 07:33:39 -0400

    Tweaking StructureBase class with the following hacks...
    * Removing subclassing of crystallography `StructureBase` class.
    * Adding __getattr__ method to get the `self.unit_cell` proxy
      attribute, otherwise call super class __getattr__ method.
    * Adding lazy computed property methods for
      `element1` and `element2`.

--- sknano/structures/_base.py
+++ sknano/structures/_base.py
@@ -19,7 +19,7 @@ from sknano.core.refdata import aCC
 __all__ = ['StructureBase']
 
 
-class StructureBase(XtalBase):
+class StructureBase:
     """Base class for creating abstract representation of nano structure.
 
     Parameters
@@ -37,4 +37,26 @@ class StructureBase(XtalBase):
         self.verbose = verbose
         self.debug = debug
 
+        # self.fmtstr = "{lattice!r}, {basis!r}, {coords!r}, cartesian=True"
         super().__init__(*args, **kwargs)
+
+
+    # def __repr__(self):
+    #     return "{}({})".format(self.__class__.__name__,
+    #                            self.fmtstr.format(**self.todict()))
+
+    def __getattr__(self, name):
+        try:
+            return getattr(self.unit_cell, name)
+        except AttributeError:
+            return super().__getattr__(name)
+
+
+    @property
+    def element1(self):
+        return self.basis[0].symbol
+
+    @property
+    def element2(self):
+        return self.basis[1].symbol
+
