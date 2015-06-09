b5f72d3eb46aeaf2c39f5c0e36c87cf31d09aa53
Andrew Merrill <androomerrill@gmail.com>
2015-06-09 15:50:59 -0400

    Refactoring the overridden `NN` property methods...
    Adding the more conventional methods for defining
    the `NN` property through the `property` decorator
    methods instead of only overriding the parent `NN.setter`
    method.

--- sknano/core/atoms/_md_atom.py
+++ sknano/core/atoms/_md_atom.py
@@ -28,16 +28,23 @@ class MDAtom(Atom):
     """
     def __init__(self, *args, reference_atom=None, t0_atom=None, **kwargs):
         super().__init__(*args, **kwargs)
+
         self.reference_atom = reference_atom
+        self.t0_atom = t0_atom
         try:
             self.r0 = t0_atom.r
         except AttributeError:
             pass
 
-    @Atom.NN.setter
+    @property
+    def NN(self):
+        return super().NN
+
+    @NN.setter
     def NN(self, value):
         """Set nearest-neighbor `Atoms`."""
-        if self.reference_atom is not None:
+        # if self.reference_atom is not None:
+        try:
             value = sknano.core.atoms.MDAtoms(
                 atoms=[value[value.ids.tolist().index(atom.id)]
                        if atom.id in value.ids else
@@ -45,7 +52,19 @@ class MDAtom(Atom):
                                       element=atom.element, id=atom.id,
                                       mol=atom.mol, type=atom.type,
                                       mass=atom.mass, q=atom.q,
-                                      x=np.inf, y=np.inf, z=np.inf)
+                                      x=np.inf, y=np.inf, z=np.inf,
+                                      CN=atom.CN, NN=atom.NN)
                        for atom in self.reference_atom.NN])
-
+        except AttributeError:
+            pass
         super(MDAtom, MDAtom).NN.__set__(self, value)
+
+    @NN.deleter
+    def NN(self):
+        super(MDAtom, MDAtom).NN.__delete__(self)
+
+    def todict(self):
+        super_dict = super().todict()
+        super_dict.update(dict(reference_atom=self.reference_atom,
+                               NN=self.NN))
+        return super_dict
