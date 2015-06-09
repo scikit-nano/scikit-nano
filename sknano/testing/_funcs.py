db9ea5c0196ef89e0d9d06e20b1002e8e78ecbd6
Andrew Merrill <androomerrill@gmail.com>
2015-06-09 15:16:52 -0400

    refactoring `generate_atoms` helper function

--- sknano/testing/_funcs.py
+++ sknano/testing/_funcs.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 ========================================================================
-Helper functions (:mod:`sknano.testing._funcs`)
+Helper functions for testing (:mod:`sknano.testing._funcs`)
 ========================================================================
 
 .. currentmodule:: sknano.testing._funcs
@@ -10,10 +10,8 @@ Helper functions (:mod:`sknano.testing._funcs`)
 from __future__ import absolute_import, division, print_function
 from __future__ import unicode_literals
 
-#from inspect import getargspec
 import importlib
 
-#import numpy as np
 from sknano.core.atoms import StructureAtom as Atom, StructureAtoms as Atoms
 from sknano.core.refdata import element_symbols as periodic_table_of_elements
 from sknano.generators import STRUCTURE_GENERATORS
@@ -21,24 +19,24 @@ from sknano.generators import STRUCTURE_GENERATORS
 __all__ = ['generate_atoms']
 
 
-def generate_atoms(elements=None, generator_class=None, **kwargs):
+def generate_atoms(*args, elements=None, generator_class=None, **kwargs):
+    atoms = None
     if elements is not None:
+        if isinstance(elements, str) and elements == 'periodic_table':
+            elements = periodic_table_of_elements
         if isinstance(elements, list):
             atoms = Atoms()
             for e in elements:
-                atoms.append(Atom(element=e))
-            return atoms
-        elif isinstance(elements, str) and elements == 'periodic_table':
-            atoms = Atoms()
-            for e in periodic_table_of_elements:
-                atoms.append(Atom(element=e))
-            return atoms
+                atoms.append(Atom(e))
+
     elif generator_class is not None and \
             generator_class in STRUCTURE_GENERATORS:
         try:
             generator = getattr(importlib.import_module('sknano.generators'),
                                 generator_class)
-            structure = generator(**kwargs)
-            return structure.atoms
+            structure = generator(*args, **kwargs)
+            atoms = structure.atoms
         except ImportError as e:
             print(e)
+
+    return atoms
