d3363c110565dfc4df6b8b0528372c16732a5355
Andrew Merrill <androomerrill@gmail.com>
2015-06-29 23:26:55 -0400

    updating `structures.extras` unit tests

--- sknano/structures/tests/test_extras.py
+++ sknano/structures/tests/test_extras.py
@@ -5,10 +5,10 @@ from __future__ import unicode_literals
 import nose
 from nose.tools import *
 from sknano.structures import filter_Ch, filter_Ch_list, generate_Ch_list, \
-    generate_Ch_property_grid
+    generate_Ch_property_grid, get_chiral_type
 
 
-def test_filter_Ch_list():
+def test1():
     Ch_list = generate_Ch_list(ni=5, nf=20, mi=0, mf=20, handedness='right')
     Ch_list = filter_Ch_list(Ch_list)
     assert_equal(len(Ch_list), 216)
@@ -17,17 +17,25 @@ def test_filter_Ch_list():
                                            handedness='right'))
 
 
-def test_generate_Ch_list():
-    Ch_list = generate_Ch_list(ni=5, nf=20, mi=0, mf=20, handedness='right')
-    assert_equal(len(Ch_list), 216)
+def test2():
+    list1 = generate_Ch_list(ni=5, nf=20, mi=0, mf=20, handedness='right')
+    list2 = generate_Ch_list(ni=5, imax=20, handedness='right')
+    assert_equal(len(list1), 216)
+    assert_equal(list1, list2)
 
-    Ch_list = generate_Ch_list(ni=5, imax=20, handedness='right')
-    assert_equal(len(Ch_list), 216)
 
-
-def test_generate_Ch_property_grid():
+def test3():
     Ch_grid = generate_Ch_property_grid(compute='compute_dt', imax=20)
-    assert_equal(Ch_grid.shape, (21,21))
+    assert_equal(Ch_grid.shape, (21, 21))
+
+
+def test4():
+    Ch_list = generate_Ch_list(ni=5, nf=20, mi=0, mf=20,
+                               chiral_types='achiral')
+    print(Ch_list)
+    chiral_types = [get_chiral_type(Ch) for Ch in Ch_list]
+    print(set(chiral_types))
+    assert_equal(set(chiral_types), {'armchair', 'zigzag'})
 
 
 if __name__ == '__main__':
