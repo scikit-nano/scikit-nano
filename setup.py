f0a52e83a0c728aa9ca3edf37ae28466a44ed52f
Andrew Merrill <androomerrill@gmail.com>
2015-07-06 11:27:52 -0400

    bumping setup version number for dev

--- setup.py
+++ setup.py
@@ -86,8 +86,8 @@ Topic :: Software Development :: Libraries :: Python Modules
 
 MAJOR = 0
 MINOR = 3
-MICRO = 10
-ISRELEASED = True
+MICRO = 11
+ISRELEASED = False
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 STABLEVERSION = None
