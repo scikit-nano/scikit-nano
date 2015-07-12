ea516e09d96eba6b447871d23db9a53da2d9ace0
Andrew Merrill <androomerrill@gmail.com>
2015-07-12 12:14:45 -0400

    removing old commented out code

--- sknano/core/crystallography/_unit_cell.py
+++ sknano/core/crystallography/_unit_cell.py
@@ -1,30 +1,23 @@
 # -*- coding: utf-8 -*-
 """
 ===========================================================================
-Crystal unit cell classes (:mod:`sknano.core.crystallography._unit_cells`)
+Crystal unit cell class (:mod:`sknano.core.crystallography._unit_cell`)
 ===========================================================================
 
-.. currentmodule:: sknano.core.crystallography._unit_cells
+.. currentmodule:: sknano.core.crystallography._unit_cell
 
 """
 from __future__ import absolute_import, division, print_function, \
     unicode_literals
 __docformat__ = 'restructuredtext en'
 
-# import copy
-# import numbers
-# from abc import ABCMeta, abstractproperty
-
-# import numpy as np
-
-# from sknano.core.math import Vector
 from sknano.core.atoms import BasisAtoms as Atoms
 
 __all__ = ['UnitCell']
 
 
 class UnitCell:
-    """Abstract base class for crystal unit cell."""
+    """Crystal unit cell class."""
 
     def __init__(self, lattice=None, basis=None, coords=None, cartesian=False):
 
@@ -49,6 +42,15 @@ class UnitCell:
         return "{}({})".format(self.__class__.__name__,
                                self.fmtstr.format(**self.todict()))
 
+    def __getattr__(self, name):
+        try:
+            return getattr(self.lattice, name)
+        except AttributeError:
+            return super().__getattr__(name)
+
+    def __iter__(self):
+        return iter(self.basis)
+
     # @property
     # def basis(self):
     #     return self._basis
@@ -69,37 +71,11 @@ class UnitCell:
     #                 else:
     #                     atom.rs = lattice.cartesian_to_fractional(pos)
 
-    # @property
-    # def lattice(self):
-    #     return self._lattice
-
-    # @lattice.setter
-    # def lattice(self, value):
-    #     self._lattice = value
-
-    def __getattr__(self, name):
-        try:
-            return getattr(self.lattice, name)
-        except AttributeError:
-            return super().__getattr__(name)
-
-        # if name != '_lattice':
-        #     return getattr(self._lattice, name)
-
-    # def __setattr__(self, name, value):
-    #     if name.startswith('_'):
-    #         super().__setattr__(name, value)
-    #     else:
-    #         setattr(self._lattice, name, value)
-
-    def __iter__(self):
-        return iter(self.basis)
-
     def rotate(self, **kwargs):
         self.lattice.rotate(**kwargs)
         self.basis.rotate(**kwargs)
 
     def todict(self):
-        """Return `dict` of `CrystalStructure` parameters."""
+        """Return `dict` of `UnitCell` parameters."""
         return dict(lattice=self.lattice, basis=self.basis.symbols.tolist(),
                     coords=self.basis.rs.tolist())
