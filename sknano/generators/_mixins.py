c97ed6f4f2d2e50ca172d5b85712579117d5d2cb
Andrew Merrill <androomerrill@gmail.com>
2015-07-13 14:45:39 -0400

    updating `generate_bundle` method to update `bundle_list`

--- sknano/generators/_mixins.py
+++ sknano/generators/_mixins.py
@@ -9,7 +9,6 @@ Generator mixin classes (:mod:`sknano.generators._mixins`)
 """
 from __future__ import absolute_import, division, print_function
 from __future__ import unicode_literals
-from builtins import object
 __docformat__ = 'restructuredtext en'
 
 import copy
@@ -18,17 +17,18 @@ __all__ = ['CappedNanotubeGeneratorMixin',
            'NanotubeBundleGeneratorMixin']
 
 
-class CappedNanotubeGeneratorMixin(object):
+class CappedNanotubeGeneratorMixin:
     """Mixin class for generating capped nanotubes."""
 
     def generate_endcaps(self):
         pass
 
 
-class NanotubeBundleGeneratorMixin(object):
+class NanotubeBundleGeneratorMixin:
     """Mixin class for generating nanotube bundles."""
 
     def generate_bundle(self):
+        self.bundle_list = []
         atomsobj0 = copy.deepcopy(self.atoms)
         atomsobj0.center_CM()
         self.structure_data.clear()
@@ -36,3 +36,4 @@ class NanotubeBundleGeneratorMixin(object):
             atomsobj = copy.deepcopy(atomsobj0)
             atomsobj.translate(dr)
             self.atoms.extend(atomsobj)
+            self.bundle_list.append(atomsobj)
