65bb3e6f52b193f64cff761b6ebf57ba2e27b613
Andrew Merrill <androomerrill@gmail.com>
2015-07-13 14:47:41 -0400

    updating graphene generator tests

--- sknano/generators/tests/test_graphene_generator.py
+++ sknano/generators/tests/test_graphene_generator.py
@@ -5,7 +5,8 @@ from __future__ import unicode_literals
 
 import nose
 from nose.tools import *
-from sknano.generators import GrapheneGenerator
+from sknano.generators import GrapheneGenerator, \
+    PrimitiveCellGrapheneGenerator, ConventionalCellGrapheneGenerator
 from sknano.testing import GeneratorTestFixtures
 
 
@@ -18,14 +19,27 @@ class TestCase(GeneratorTestFixtures):
         self.tmpdata.append(generator.fname)
 
     def test2(self):
-        generator = GrapheneGenerator(armchair_edge_length=20,
-                                      zigzag_edge_length=1)
+        generator = ConventionalCellGrapheneGenerator(armchair_edge_length=5,
+                                                      zigzag_edge_length=5)
         generator.save()
         self.tmpdata.append(generator.fname)
 
     def test3(self):
-        generator = GrapheneGenerator(armchair_edge_length=1,
-                                      zigzag_edge_length=20)
+        generator = \
+            GrapheneGenerator.from_conventional_cell(armchair_edge_length=5,
+                                                     zigzag_edge_length=5)
+        generator.save()
+        self.tmpdata.append(generator.fname)
+
+    def test4(self):
+        generator = PrimitiveCellGrapheneGenerator(edge_length=5)
+        generator.save()
+        self.tmpdata.append(generator.fname)
+        print(generator.Natoms)
+
+    def test5(self):
+        generator = \
+            GrapheneGenerator.from_primitive_cell(edge_length=5)
         generator.save()
         self.tmpdata.append(generator.fname)
 
