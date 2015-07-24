26c78f36646014cb66b1305769785143c41c4a96
Andrew Merrill <androomerrill@gmail.com>
2015-07-24 12:39:42 -0400

    updating the periodic table unit tests

--- sknano/core/refdata/tests/test_periodic_table.py
+++ sknano/core/refdata/tests/test_periodic_table.py
@@ -5,9 +5,34 @@ from __future__ import unicode_literals
 import nose
 from nose.tools import *
 
+from sknano.core.refdata import atomic_masses, atomic_mass_symbol_map, \
+    atomic_numbers, atomic_number_symbol_map, element_names, element_symbols
 
-def test_data_structures():
-    pass
+
+def test1():
+    for data in (atomic_numbers, atomic_number_symbol_map,
+                 element_names, element_symbols):
+        assert_equal(len(data), len(element_symbols))
+
+
+def test2():
+    print(atomic_masses)
+    print(atomic_mass_symbol_map)
+    print(list(atomic_masses.keys()))
+    assert_equal(element_symbols, list(atomic_masses.keys()))
+
+
+def test3():
+    print(atomic_numbers)
+    print(atomic_number_symbol_map)
+    print(list(atomic_numbers.keys()))
+    assert_equal(element_symbols, list(atomic_numbers.keys()))
+
+
+def test4():
+    assert_equal(len(element_names), len(element_symbols))
+    assert_equal(list(atomic_masses.keys()), list(atomic_numbers.keys()))
+
 
 if __name__ == '__main__':
     nose.runmodule()
