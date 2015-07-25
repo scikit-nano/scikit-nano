6c049189aa1bc4d0ef9b6c934ebeaf3c0642c4c8
Andrew Merrill <androomerrill@gmail.com>
2015-07-25 15:48:54 -0400

    updating refdata unit tests

--- sknano/core/refdata/tests/test_element_data.py
+++ sknano/core/refdata/tests/test_element_data.py
@@ -5,11 +5,12 @@ from __future__ import unicode_literals
 import nose
 from nose.tools import *
 
-from sknano.core.refdata import element_data
+from sknano.core.refdata import element_data, element_symbols
 
 
 def test1():
     print(element_data)
+    assert_equal(len(element_data.keys()), len(element_symbols))
 
 
 if __name__ == '__main__':
