7af99cac51de3d84746df4bb1ffc34d381238efc
Andrew Merrill <androomerrill@gmail.com>
2015-07-30 14:22:14 -0400

    Updating ui2py script to generate both PyQt4/5 ui files

--- sknano/apps/nanogen_gui/ui2py.sh
+++ sknano/apps/nanogen_gui/ui2py.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
-pyuic5 nanogen.ui > _ui_nanogen.py
-pyuic5 swnt_generator.ui > _ui_swnt_generator.py
-pyuic5 mwnt_generator.ui > _ui_mwnt_generator.py
-pyuic5 graphene_generator.ui > _ui_graphene_generator.py
-pyuic5 fullerene_generator.ui > _ui_fullerene_generator.py
-pyuic5 bulk_structure_generator.ui > _ui_bulk_structure_generator.py
-pyuic5 mwnt_Ch_list_item_dialog.ui > _ui_mwnt_Ch_list_item_dialog.py
+pyuic4 nanogen.ui > _pyqt4_ui_nanogen.py
+pyuic4 swnt_generator.ui > _pyqt4_ui_swnt_generator.py
+pyuic4 mwnt_generator.ui > _pyqt4_ui_mwnt_generator.py
+pyuic4 graphene_generator.ui > _pyqt4_ui_graphene_generator.py
+pyuic4 fullerene_generator.ui > _pyqt4_ui_fullerene_generator.py
+pyuic4 bulk_structure_generator.ui > _pyqt4_ui_bulk_structure_generator.py
+pyuic4 mwnt_Ch_list_item_dialog.ui > _pyqt4_ui_mwnt_Ch_list_item_dialog.py
+
+pyuic5 nanogen.ui > _pyqt5_ui_nanogen.py
+pyuic5 swnt_generator.ui > _pyqt5_ui_swnt_generator.py
+pyuic5 mwnt_generator.ui > _pyqt5_ui_mwnt_generator.py
+pyuic5 graphene_generator.ui > _pyqt5_ui_graphene_generator.py
+pyuic5 fullerene_generator.ui > _pyqt5_ui_fullerene_generator.py
+pyuic5 bulk_structure_generator.ui > _pyqt5_ui_bulk_structure_generator.py
+pyuic5 mwnt_Ch_list_item_dialog.ui > _pyqt5_ui_mwnt_Ch_list_item_dialog.py
