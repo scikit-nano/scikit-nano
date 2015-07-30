578f57ed4327e4c0aa447ac0f206c7a6db9339a7
Andrew Merrill <androomerrill@gmail.com>
2015-07-30 13:29:46 -0400

    Updating nanogenui test script to check if it should run.

--- sknano/scripts/tests/test_nanogenui.py
+++ sknano/scripts/tests/test_nanogenui.py
@@ -3,21 +3,24 @@
 from __future__ import absolute_import, division, print_function
 from __future__ import unicode_literals
 
+import os
 import subprocess
 import nose
 from nose.tools import *
-#from sknano.scripts.nanogenui import NanoGen
 
 
 def test_gui_launch():
-    gui_launch_successful = False
-    try:
-        subprocess.call("nanogenui", timeout=1)
-    except subprocess.TimeoutExpired:
-        gui_launch_successful = True
-    except OSError:
-        pass
-    assert_true(gui_launch_successful)
+    if int(os.environ.get('SKNANO_SKIP_GUI_TESTS', 0)):
+        print('Skipping nanogenui gui launch.')
+    else:
+        gui_launch_successful = False
+        try:
+            subprocess.call("nanogenui", timeout=1)
+        except subprocess.TimeoutExpired:
+            gui_launch_successful = True
+        except OSError:
+            pass
+        assert_true(gui_launch_successful)
 
 
 if __name__ == '__main__':
