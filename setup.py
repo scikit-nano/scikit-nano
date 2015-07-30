09103aeb9e1caa0418fe4fde2e5d2a0bae6a3b19
Andrew Merrill <androomerrill@gmail.com>
2015-07-30 15:19:06 -0400

    bumping version to 0.3.14.dev for development

--- setup.py
+++ setup.py
@@ -86,8 +86,8 @@ Topic :: Software Development :: Libraries :: Python Modules
 
 MAJOR = 0
 MINOR = 3
-MICRO = 13
-ISRELEASED = True
+MICRO = 14
+ISRELEASED = False
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 STABLEVERSION = None
