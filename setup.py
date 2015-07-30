81bb7601842c27454bebd116e9b2e6d9c9aea41c
Andrew Merrill <androomerrill@gmail.com>
2015-07-30 14:45:09 -0400

    Updating setup configuration.
    Don't add the 'data' to the config instance as it's
    done in the subpackage setup.py file

--- setup.py
+++ setup.py
@@ -215,7 +215,6 @@ def configuration(parent_package='', top_path=None):
                        quiet=True)
 
     config.add_subpackage('sknano')
-    config.add_data_dir('sknano/data')
     config.get_version('sknano/version.py')
 
     return config
