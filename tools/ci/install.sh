9b85f6af5ead22b56a58d6f1f73375e9dc71b669
Andrew Merrill <androomerrill@gmail.com>
2015-08-06 01:51:02 -0400

    updating travis-ci config/tools

--- tools/ci/install.sh
+++ tools/ci/install.sh
@@ -6,4 +6,11 @@ echo ${PYTHON_VERSION}
 
 pip install --upgrade pip setuptools
 travis_wait pip install -r requirements.txt
+pip install nose
+
+export COVERAGE="true"
+
+if [[ "$COVERAGE" == "true" ]]; then
+    pip install coverage coveralls
+fi
 python setup.py install
