7c33026d90684b96f94f8f5ff53d4ed902fec76f
Andrew Merrill <androomerrill@gmail.com>
2015-08-06 05:51:20 -0400

    Replacing the `NeighborAtom`/`NeighborAtoms` classes...
    ...with `NeighborAtomMixin` and `NeighborAtomsMixin`,
    respectively.

--- sknano/core/atoms/_neighbor_atom.py
+++ sknano/core/atoms/_neighbor_atom.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 ===============================================================================
-Atom class for NN analysis (:mod:`sknano.core.atoms._neighbor_atom`)
+Mixin `Atom` class for NN analysis (:mod:`sknano.core.atoms._neighbor_atom`)
 ===============================================================================
 
 .. currentmodule:: sknano.core.atoms._neighbor_atom
@@ -11,15 +11,9 @@ from __future__ import absolute_import, division, print_function
 from __future__ import unicode_literals
 __docformat__ = 'restructuredtext en'
 
-# import sknano.core.atoms
-# from ._cn_atom import CNAtom
-# from ._id_atom import IDAtom
-# from ._xyz_atom import XYZAtom
-# from ._bond import Bond
-# from ._bonds import Bonds
-from ._kdtree_atom import KDTAtom
-
-__all__ = ['NeighborAtom']
+__all__ = ['NeighborAtomMixin']
 
 
-NeighborAtom = KDTAtom
+class NeighborAtomMixin:
+    """Mixin class for neighbor analysis."""
+    pass
--- sknano/core/atoms/_neighbor_atoms.py
+++ sknano/core/atoms/_neighbor_atoms.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 ===============================================================================
-NeighborAtom container class (:mod:`sknano.core.atoms._neighbor_atoms`)
+Mixin `Atoms` class for NN analysis (:mod:`sknano.core.atoms._neighbor_atoms`)
 ===============================================================================
 
 .. currentmodule:: sknano.core.atoms._neighbor_atoms
@@ -13,31 +13,8 @@ __docformat__ = 'restructuredtext en'
 
 # import numpy as np
 
-from ._cn_atoms import CNAtoms
-from ._id_atoms import IDAtoms
-from ._xyz_atoms import XYZAtoms
-# from ._bonds import Bonds
-# from ._neighbor_atom import NeighborAtom
-from ._structure_atom import StructureAtom
-
-__all__ = ['NeighborAtoms']
+__all__ = ['NeighborAtomsMixin']
 
 
-class NeighborAtoms(CNAtoms, IDAtoms, XYZAtoms):
-    """An `Atoms` sub-class for nearest-neighbor structure analysis.
-
-    Parameters
-    ----------
-    atoms : {None, sequence, `NeighborAtoms`}, optional
-        if not `None`, then a list of `NeighborAtom` instance objects or an
-        existing `NeighborAtoms` instance object.
-    kNN : :class:`~python:int`
-        Number of nearest neighbors to return when querying the kd-tree.
-    NNrc : :class:`~python:float`
-        Nearest neighbor radius cutoff.
-
-    """
-    @property
-    def __atom_class__(self):
-        # return NeighborAtom
-        return StructureAtom
+class NeighborAtomsMixin:
+    """Mixin :class:`~sknano.core.atoms.Atoms` class for NN analysis."""
