e38a03576154f7a65e62927364f652cb0a726fb4
Andrew Merrill <androomerrill@gmail.com>
2015-08-13 17:40:40 -0400

    updating unit tests

--- sknano/structures/tests/test_extras.py
+++ sknano/structures/tests/test_extras.py
@@ -3,8 +3,8 @@ from __future__ import absolute_import, division, print_function
 from __future__ import unicode_literals
 
 import nose
-from nose.tools import *
-from sknano.structures import filter_Ch, filter_Ch_list, generate_Ch_list, \
+from nose.tools import assert_equal
+from sknano.structures import filter_Ch_list, generate_Ch_list, \
     generate_Ch_property_grid, get_chiral_type
 
 
@@ -12,9 +12,6 @@ def test1():
     Ch_list = generate_Ch_list(ni=5, nf=20, mi=0, mf=20, handedness='right')
     Ch_list = filter_Ch_list(Ch_list)
     assert_equal(len(Ch_list), 216)
-    Ch_list = filter_Ch_list(Ch_list, property_filters=[('n', '<=', 10)])
-    assert_equal(Ch_list, generate_Ch_list(ni=5, nf=10, mi=0, mf=20,
-                                           handedness='right'))
 
 
 def test2():
@@ -37,6 +34,12 @@ def test4():
     print(set(chiral_types))
     assert_equal(set(chiral_types), {'armchair', 'zigzag'})
 
+
+def test5():
+    Ch_list = generate_Ch_list(ni=5, nf=20, mi=0, mf=20, handedness='right')
+    Ch_list = filter_Ch_list(Ch_list, property_filters=[('n', '<=', 10)])
+    assert_equal(Ch_list, generate_Ch_list(ni=5, nf=10, mi=0, mf=20,
+                                           handedness='right'))
 
 if __name__ == '__main__':
     nose.runmodule()
