3489478b2a434920783790355c950f2c39cabe8b
Andrew Merrill <androomerrill@gmail.com>
2015-08-17 11:43:06 -0400

    updating bulk generator unit tests

--- sknano/generators/tests/test_bulk_structure_generator.py
+++ sknano/generators/tests/test_bulk_structure_generator.py
@@ -6,7 +6,7 @@ from __future__ import unicode_literals
 from pkg_resources import resource_filename
 
 import nose
-from nose.tools import *
+from nose.tools import assert_equal
 
 import numpy as np
 
@@ -27,79 +27,79 @@ class TestCase(GeneratorTestFixtures):
         quartz = AlphaQuartzGenerator()
         quartz.save()
         self.tmpdata.append(quartz.fname)
-        assert_equals(quartz.atoms.Natoms, AlphaQuartz().basis.Natoms)
+        assert_equal(quartz.atoms.Natoms, AlphaQuartz().basis.Natoms)
 
     def test2(self):
-        quartz = AlphaQuartzGenerator(scaling_matrix=[3, 3, 3])
+        quartz = AlphaQuartzGenerator(scaling_matrix=[2, 2, 2])
         quartz.save()
         self.tmpdata.append(quartz.fname)
-        assert_equals(quartz.atoms.Natoms, AlphaQuartz().basis.Natoms * 3**3)
+        assert_equal(quartz.atoms.Natoms, AlphaQuartz().basis.Natoms * 2 ** 3)
 
     def test3(self):
         gold = GoldGenerator()
         gold.save()
         self.tmpdata.append(gold.fname)
-        assert_equals(gold.atoms.Natoms, Gold().basis.Natoms)
+        assert_equal(gold.atoms.Natoms, Gold().basis.Natoms)
 
     def test4(self):
-        gold = GoldGenerator(scaling_matrix=[5, 5, 5])
+        gold = GoldGenerator(scaling_matrix=[2, 2, 2])
         gold.save()
         self.tmpdata.append(gold.fname)
-        assert_equals(gold.atoms.Natoms, Gold().basis.Natoms * 5**3)
+        assert_equal(gold.atoms.Natoms, Gold().basis.Natoms * 2 ** 3)
 
     def test5(self):
         copper = CopperGenerator()
         copper.save()
         self.tmpdata.append(copper.fname)
-        assert_equals(copper.atoms.Natoms, Copper().basis.Natoms)
+        assert_equal(copper.atoms.Natoms, Copper().basis.Natoms)
 
     def test6(self):
-        copper = CopperGenerator(scaling_matrix=[5, 5, 5])
+        copper = CopperGenerator(scaling_matrix=[2, 2, 2])
         copper.save()
         self.tmpdata.append(copper.fname)
-        assert_equals(copper.atoms.Natoms, Copper().basis.Natoms * 5**3)
+        assert_equal(copper.atoms.Natoms, Copper().basis.Natoms * 2 ** 3)
 
     def test7(self):
         diamond = DiamondStructureGenerator()
         diamond.save()
         self.tmpdata.append(diamond.fname)
-        assert_equals(diamond.atoms.Natoms, DiamondStructure().basis.Natoms)
+        assert_equal(diamond.atoms.Natoms, DiamondStructure().basis.Natoms)
 
     def test8(self):
-        diamond = DiamondStructureGenerator(scaling_matrix=[3, 3, 3])
+        diamond = DiamondStructureGenerator(scaling_matrix=[2, 2, 2])
         diamond.save()
         self.tmpdata.append(diamond.fname)
-        assert_equals(diamond.atoms.Natoms,
-                      DiamondStructure().basis.Natoms * 3**3)
+        assert_equal(diamond.atoms.Natoms,
+                     DiamondStructure().basis.Natoms * 2 ** 3)
 
     def test9(self):
         caesium_chloride = CaesiumChlorideStructureGenerator()
         caesium_chloride.save()
         self.tmpdata.append(caesium_chloride.fname)
-        assert_equals(caesium_chloride.atoms.Natoms,
-                      CaesiumChlorideStructure().basis.Natoms)
+        assert_equal(caesium_chloride.atoms.Natoms,
+                     CaesiumChlorideStructure().basis.Natoms)
 
     def test10(self):
         caesium_chloride = \
-            CaesiumChlorideStructureGenerator(scaling_matrix=[3, 3, 3])
+            CaesiumChlorideStructureGenerator(scaling_matrix=[2, 2, 2])
         caesium_chloride.save()
         self.tmpdata.append(caesium_chloride.fname)
-        assert_equals(caesium_chloride.atoms.Natoms,
-                      CaesiumChlorideStructure().basis.Natoms * 3**3)
+        assert_equal(caesium_chloride.atoms.Natoms,
+                     CaesiumChlorideStructure().basis.Natoms * 2 ** 3)
 
     def test11(self):
         molybdenum_disulphide = MoS2Generator()
         molybdenum_disulphide.save()
         self.tmpdata.append(molybdenum_disulphide.fname)
-        assert_equals(molybdenum_disulphide.atoms.Natoms,
-                      MoS2().basis.Natoms)
+        assert_equal(molybdenum_disulphide.atoms.Natoms,
+                     MoS2().basis.Natoms)
 
     def test12(self):
-        molybdenum_disulphide = MoS2Generator(scaling_matrix=[3, 3, 3])
+        molybdenum_disulphide = MoS2Generator(scaling_matrix=[2, 2, 2])
         molybdenum_disulphide.save()
         self.tmpdata.append(molybdenum_disulphide.fname)
-        assert_equals(molybdenum_disulphide.atoms.Natoms,
-                      MoS2().basis.Natoms * 3**3)
+        assert_equal(molybdenum_disulphide.atoms.Natoms,
+                     MoS2().basis.Natoms * 2 ** 3)
 
     def test13(self):
         assert_equal(GoldGenerator().atoms, FCCStructureGenerator('Au').atoms)
