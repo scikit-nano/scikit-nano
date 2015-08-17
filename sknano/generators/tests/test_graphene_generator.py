ddb7fffd8e12f03b8c879d168ddde91d926fb14f
Andrew Merrill <androomerrill@gmail.com>
2015-08-17 18:50:16 -0400

    updating graphene generator tests

--- sknano/generators/tests/test_graphene_generator.py
+++ sknano/generators/tests/test_graphene_generator.py
@@ -13,35 +13,65 @@ from sknano.testing import GeneratorTestFixtures
 class TestCase(GeneratorTestFixtures):
 
     def test1(self):
-        generator = GrapheneGenerator(armchair_edge_length=5,
-                                      zigzag_edge_length=5)
+        generator = GrapheneGenerator(armchair_edge_length=2,
+                                      zigzag_edge_length=2)
         generator.save()
         self.tmpdata.append(generator.fname)
 
     def test2(self):
-        generator = ConventionalCellGrapheneGenerator(armchair_edge_length=5,
-                                                      zigzag_edge_length=5)
-        generator.save()
+        generator = ConventionalCellGrapheneGenerator(armchair_edge_length=2,
+                                                      zigzag_edge_length=2)
+        generator.save(fname='2nmx2nm_1layer_conventional_cell_graphene-1.xyz')
         self.tmpdata.append(generator.fname)
 
     def test3(self):
         generator = \
-            GrapheneGenerator.from_conventional_cell(armchair_edge_length=5,
-                                                     zigzag_edge_length=5)
+            GrapheneGenerator.from_conventional_cell(armchair_edge_length=2,
+                                                     zigzag_edge_length=2)
         generator.atoms.update_attrs()
-        generator.save(deepcopy=False)
+        # generator.save(deepcopy=False)
+        generator.save(fname='2nmx2nm_1layer_conventional_cell_graphene-2.xyz',
+                       deepcopy=False)
         self.tmpdata.append(generator.fname)
 
     def test4(self):
-        generator = PrimitiveCellGrapheneGenerator(edge_length=5)
-        generator.save()
+        generator = PrimitiveCellGrapheneGenerator(edge_length=2)
+        generator.save(fname='2nm_1layer_primitive_cell_graphene-1.xyz')
         self.tmpdata.append(generator.fname)
-        print(generator.Natoms)
+        # print(generator.Natoms)
 
     def test5(self):
         generator = \
-            GrapheneGenerator.from_primitive_cell(edge_length=5)
-        generator.save()
+            GrapheneGenerator.from_primitive_cell(edge_length=2)
+        generator.save(fname='2nm_1layer_primitive_cell_graphene-2.xyz')
+        self.tmpdata.append(generator.fname)
+
+    def test6(self):
+        generator = \
+            GrapheneGenerator.from_conventional_cell(armchair_edge_length=2,
+                                                     zigzag_edge_length=2,
+                                                     nlayers=3)
+        generator.save(fname='2nmx2nm_3layer_conventional_cell_graphene-3.xyz')
+        self.tmpdata.append(generator.fname)
+
+    def test7(self):
+        generator = \
+            GrapheneGenerator.from_primitive_cell(edge_length=2, nlayers=3)
+        generator.save(fname='2nm_3layer_primitive_cell_graphene-3.xyz')
+        self.tmpdata.append(generator.fname)
+
+    def test8(self):
+        generator = \
+            GrapheneGenerator.from_conventional_cell(armchair_edge_length=2,
+                                                     zigzag_edge_length=2,
+                                                     nlayers=3)
+        generator.save(fname='2nmx2nm_3layer_conventional_cell_graphene.data')
+        self.tmpdata.append(generator.fname)
+
+    def test9(self):
+        generator = \
+            GrapheneGenerator.from_primitive_cell(edge_length=2, nlayers=3)
+        generator.save(fname='2nm_3layer_primitive_cell_graphene.data')
         self.tmpdata.append(generator.fname)
 
 
