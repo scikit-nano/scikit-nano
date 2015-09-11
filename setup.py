5cf529762dfe754fe14a5771c04a5f493f9a52f0
Andrew Merrill <androomerrill@gmail.com>
2015-09-11 14:15:44 -0400

    bumping version to v0.3.18.dev for development

--- setup.py
+++ setup.py
@@ -84,8 +84,8 @@ Topic :: Software Development :: Libraries :: Python Modules
 
 MAJOR = 0
 MINOR = 3
-MICRO = 17
-ISRELEASED = True
+MICRO = 18
+ISRELEASED = False
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 STABLEVERSION = None
