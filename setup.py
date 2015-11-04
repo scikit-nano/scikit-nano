e2c40d453941d86d712e75bbdbd82ff80c553794
Andrew Merrill <androomerrill@gmail.com>
2015-11-04 08:55:40 -0500

    bumping setup version to v0.3.22.dev for development

--- setup.py
+++ setup.py
@@ -84,8 +84,8 @@ Topic :: Software Development :: Libraries :: Python Modules
 
 MAJOR = 0
 MINOR = 3
-MICRO = 21
-ISRELEASED = True
+MICRO = 22
+ISRELEASED = False
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 STABLEVERSION = None
