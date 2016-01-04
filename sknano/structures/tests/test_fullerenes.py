167f2d765204b3b43dab391c66bafa8483b80a4c
Andrew Merrill <androomerrill@gmail.com>
2016-01-04 02:46:46 -0500

    updating unit tests

--- sknano/structures/tests/test_fullerenes.py
+++ sknano/structures/tests/test_fullerenes.py
@@ -4,25 +4,14 @@ from __future__ import absolute_import, division, print_function
 from __future__ import unicode_literals
 
 import nose
-from nose.tools import *
+from nose.tools import assert_equal
 from sknano.structures import Fullerene, Fullerenes, load_fullerene_data
 
 
 def test1():
-    fullerene = Fullerene(20)
-    print(fullerene)
-    assert_equal(fullerene.N, 20)
-
-
-def test2():
-    fullerenes = Fullerenes()
-    print(fullerenes)
-
-
-def test3():
-    data = load_fullerene_data()
-    print(list(data.keys()))
-    print(list(data.values()))
+    for i, (fullerene, data) in enumerate(load_fullerene_data().items()):
+        N = Fullerenes.N[i]
+        assert_equal(Fullerene(N).N, N)
 
 
 if __name__ == '__main__':
