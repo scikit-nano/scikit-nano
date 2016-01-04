8eeb554168683731c448ac0ebdd1cd0688025692
Andrew Merrill <androomerrill@gmail.com>
2016-01-04 03:45:27 -0500

    Updating `MDAtom` class.
    * Removing the `t0_atom` parameter from call signature.
    * Removing the `NN` getter/setter methods.
    * Adding new attribute `reference_atom_neighbors` as
      replacement for the `NN` attribute.
    * Adding new attribute `reference_atom_bonds`.
    * Other minor updates/tweaks.

--- sknano/core/atoms/_md_atoms.py
+++ sknano/core/atoms/_md_atoms.py
@@ -20,6 +20,7 @@ import sknano.core.atoms
 from ._energy_atoms import EnergyAtom, EnergyAtoms
 from ._force_atoms import ForceAtom, ForceAtoms
 from ._structure_atoms import StructureAtom, StructureAtoms
+from ._bonds import Bond, Bonds
 
 __all__ = ['MDAtom', 'MDAtoms']
 
@@ -28,53 +29,58 @@ class MDAtom(StructureAtom, ForceAtom, EnergyAtom):
     """An `Atom` class for molecular dynamics trajectory analysis.
 
     """
-    def __init__(self, *args, reference_atom=None, t0_atom=None, **kwargs):
+    def __init__(self, *args, reference_atom=None, **kwargs):
         super().__init__(*args, **kwargs)
-
         self.reference_atom = reference_atom
-        self.t0_atom = t0_atom
-        try:
-            self.r0 = t0_atom.r
-        except AttributeError:
-            pass
 
     def __dir__(self):
         attrs = super().__dir__()
-        # attrs.append('reference_atom')
-        attrs.extend(['reference_atom', 't0_atom'])
+        attrs.append('reference_atom')
         return attrs
 
     @property
-    def NN(self):
-        return super().NN
+    def reference_atom(self):
+        return self._reference_atom
 
-    @NN.setter
-    def NN(self, value):
-        """Set nearest-neighbor `Atoms`."""
+    @reference_atom.setter
+    def reference_atom(self, value):
+        self._reference_atom = value
+        if value is not None:
+            try:
+                self.r0 = self.reference_atom.r
+            except AttributeError:
+                pass
+
+    @property
+    def reference_atom_neighbors(self):
+        atoms = []
+        neighbors = self.neighbors
         try:
-            atoms = []
-            for atom in self.reference_atom.NN:
-                if atom.id in value.ids:
-                    atoms.append(value[value.ids.tolist().index(atom.id)])
+            for atom in self.reference_atom.neighbors:
+                if atom.id in neighbors.ids:
+                    atoms.append(neighbors[
+                                 neighbors.ids.tolist().index(atom.id)])
                 else:
                     atomdict = atom.todict()
                     [atomdict.update({k: np.inf}) for k in ('x', 'y', 'z')]
-                    print(atomdict)
                     atoms.append(self.__class__(**atomdict))
-            value = sknano.core.atoms.MDAtoms(atoms)
         except AttributeError:
             pass
-        super(MDAtom, MDAtom).NN.__set__(self, value)
+        return sknano.core.atoms.MDAtoms(atoms)
 
-    @NN.deleter
-    def NN(self):
-        super(MDAtom, MDAtom).NN.__delete__(self)
+    @property
+    def reference_atom_bonds(self):
+        """Return atom `Bonds` instance."""
+        try:
+            return Bonds([Bond(self, nn) for nn in
+                          self.reference_atom_neighbors])
+        except (AttributeError, TypeError):
+            return Bonds()
 
     def todict(self):
         super_dict = super().todict()
         super_dict.update(dict(reference_atom=self.reference_atom,
-                               t0_atom=self.t0_atom,
-                               NN=self.NN))
+                               neighbors=self.neighbors))
         return super_dict
 
 
