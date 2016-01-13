a6f646bf52b168d9cc4b13e4a01041ad21e0a023
Andrew Merrill <androomerrill@gmail.com>
2016-01-13 11:01:17 -0500

    updating unit tests

--- sknano/core/tests/test_strings.py
+++ sknano/core/tests/test_strings.py
@@ -5,7 +5,8 @@ from __future__ import unicode_literals
 import nose
 from nose.tools import assert_equal
 
-from sknano.core import pluralize, ordinal_form
+from sknano.core import pluralize, ordinal_form, tuple_expr, list_expr, \
+    dict_expr, kwargs_expr, call_signature
 
 
 def test1():
@@ -35,6 +36,46 @@ def test4():
     for i in range(100):
         print(ordinal_form(i))
 
+
+def test5():
+    teststr = "(10, 5)"
+    assert_equal(tuple_expr.parseString(teststr)[0], (10.0, 5.0))
+
+
+def test6():
+    teststr = "[5, 5, 5]"
+    parsed = list_expr.parseString(teststr, parseAll=True)[0]
+    assert_equal(parsed, [5, 5, 5])
+
+
+def test7():
+    teststr = "{'a': 5, 'b': 1, 'c': [1, 1, 2]}"
+    assert_equal(dict_expr.parseString(teststr)[0],
+                 {'a': 5, 'b': 1, 'c': [1, 1, 2]})
+
+
+def test8():
+    teststr = "dict(a=5, b=1, c=[1, 1, 2])"
+    assert_equal(dict_expr.parseString(teststr)[0],
+                 {'a': 5, 'b': 1, 'c': [1, 1, 2]})
+
+
+def test9():
+    teststr = "a=5, b=2.5, c=[1, 2, 3]"
+    expr = kwargs_expr.parseString(teststr)[0]
+    print(expr)
+    assert_equal(expr, dict(a=5, b=2.5, c=[1, 2, 3]))
+
+
+def test10():
+    teststr = "(10, 5), 1, 2, 3, a=5, b=10, c=[1, 2, 3]"
+    expr = call_signature.parseString(teststr, parseAll=True)[0]
+    print(expr)
+    args, kwargs = expr
+    print('args: {}'.format(args))
+    print('kwargs: {}'.format(kwargs))
+    assert_equal(args, [(10, 5), 1, 2, 3])
+    assert_equal(kwargs, dict(a=5, b=10, c=[1, 2, 3]))
 
 if __name__ == '__main__':
     nose.runmodule()
