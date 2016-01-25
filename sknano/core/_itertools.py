f952b0cfa36c512be9e018a4cac0cc7415e73738
Andrew Merrill <androomerrill@gmail.com>
2016-01-25 11:40:35 -0500

    updating `itertools` docstrings

--- sknano/core/_itertools.py
+++ sknano/core/_itertools.py
@@ -22,23 +22,25 @@ from __future__ import absolute_import, division, print_function, \
 __docformat__ = 'restructuredtext en'
 
 from collections import Iterable
-import collections
+from operator import itemgetter
+
 import operator
+import collections
 import random
 from itertools import chain, combinations, count, cycle, filterfalse, \
-    islice, repeat, starmap, tee, zip_longest
+    groupby, islice, repeat, starmap, tee, zip_longest
 
 __all__ = ['cyclic_pairs', 'take', 'tabulate', 'consume', 'nth', 'quantify',
            'padnone', 'ncycles', 'dotproduct', 'flatten', 'repeatfunc',
            'pairwise', 'grouper', 'roundrobin', 'partition', 'powerset',
-           'unique_elements', 'iter_except', 'first_true',
-           'random_product', 'random_permutation', 'random_combination',
-           'random_combination_with_replacement']
+           'unique_elements', 'unique_everseen', 'unique_justseen',
+           'iter_except', 'first_true', 'random_product', 'random_permutation',
+           'random_combination', 'random_combination_with_replacement']
 
 
 def cyclic_pairs(iterable):
-    """
-    Generate a cyclic list of all subsequence pairs in `iterable`.
+    """Return iterator over all subsequence pairs in `iterable`, up to
+    last and first element pair.
 
     Returns
     -------
@@ -46,7 +48,6 @@ def cyclic_pairs(iterable):
 
     Examples
     --------
-
     >>> list(cyclic_pairs('ABC'))
     [('A', 'B'), ('B', 'C'), ('C', 'A')]
 
@@ -56,18 +57,73 @@ def cyclic_pairs(iterable):
 
 
 def take(n, iterable):
-    "Return first n items of the iterable as a list"
+    """Return first n items of the iterable as a list.
+
+    Parameters
+    ----------
+    n : :class:`~python:int`
+    iterable : :class:`collections.Iterable`
+
+    Returns
+    -------
+    :class:`~python:list`
+
+    Examples
+    --------
+    >>> from sknano.core import take, tabulate
+    >>> t = tabulate(lambda i: i)
+    >>> take(5, t)
+    [0, 1, 2, 3, 4]
+    >>> take(5, 5)
+    [5, 6, 7, 8, 9]
+
+    """
     return list(islice(iterable, n))
 
 
 def tabulate(function, start=0):
-    "Return function(0), function(1), ..."
+    """Return an iterator mapping `function` over linear input.
+
+    Returns function(0), function(1), ...
+
+    Parameters
+    ----------
+    function : callable
+    start : :class:`~python:int`
+        The start argument will be increased by 1 each time the iterator
+        is called and fed into `function`.
+
+    Returns
+    -------
+    :class:`~python:collections.Iterator`
+
+    Examples
+    --------
+    >>> from sknano.core import tabulate, ordinal_form, take
+    >>> t = tabulate(ordinal_form, 10)
+    >>> take(5, t)
+    ['10th', '11th', '12th', '13th', '14th']
+    >>> take(5, t)
+    ['15th', '16th', '17th', '18th', '19th']
+    >>> t = tabulate(ordinal_form)
+    >>> take(5, t)
+    ['0th', '1st', '2nd', '3rd', '4th']
+    >>> take(5, t)
+    ['5th', '6th', '7th', '8th', '9th']
+
+    """
     return map(function, count(start))
 
 
-def consume(iterator, n):
-    "Advance the iterator n-steps ahead. If n is none, consume entirely."
-    # Use functions that consume iterators at C speed.
+def consume(iterator, n=None):
+    """Advance the iterator n-steps ahead. If n is `None`, consume entirely.
+
+    Parameters
+    ----------
+    iterator : :class:`~python.collections.Iterator`
+    n : {:class:`~python:None`, :class:`~python:int`}, optional
+
+    """
     if n is None:
         # feed the entire iterator into a zero-length deque
         collections.deque(iterator, maxlen=0)
@@ -77,34 +133,98 @@ def consume(iterator, n):
 
 
 def nth(iterable, n, default=None):
-    "Returns the nth item or a default value"
+    """Returns the nth item or a default value.
+
+    Parameters
+    ----------
+    iterable : :class:`~python:collections.Iterable`
+    n : :class:`~python:int`
+    default : {:class:`~python:None`, :class:`~python:object`}, optional
+
+    Examples
+    --------
+    >>> from sknano.core import nth
+    >>> nth(range(10), 3)
+    3
+    >>> nth(range(10), 20, default='donkey')
+    'donkey'
+
+    """
     return next(islice(iterable, n, None), default)
 
 
 def quantify(iterable, pred=bool):
-    "Count how many times the predicate is true"
+    """Count how many times the predicate is true.
+
+    Examples
+    --------
+    >>> from sknano.core import quantify
+    >>> quantify([True, False, True])
+    2
+
+    """
     return sum(map(pred, iterable))
 
 
 def padnone(iterable):
     """Returns the sequence elements and then returns None indefinitely.
 
-    Useful for emulating the behavior of the built-in map() function.
+    Examples
+    --------
+    >>> from sknano.core import padnone
+    >>> take(5, padnone(range(3)))
+    [0, 1, 2, None, None]
+
     """
     return chain(iterable, repeat(None))
 
 
 def ncycles(iterable, n):
-    "Returns the sequence elements n times"
+    """Returns the sequence elements n times"""
     return chain.from_iterable(repeat(tuple(iterable), n))
 
 
 def dotproduct(vec1, vec2):
+    """Returns the dot product of two :class:`~python:collections.Iterable`\ s.
+
+    Parameters
+    ----------
+    vec1, vec2 : :class:`~python:collections.Iterable`
+
+    """
     return sum(map(operator.mul, vec1, vec2))
 
 
 def flatten(items, ignore_types=(str, bytes)):
-    "Flatten nested sequence into a single list of values."
+    """Yields single sequence of values with no nesting [PythonCookbook]_.
+
+    Parameters
+    ----------
+    items : sequence
+        nested sequence that you want to flatten into a single sequence with
+        no nesting
+    ignore_types : :class:`~python:tuple`, optional
+        :class:`~python:tuple` of :class:`~python:collections.Iterable`
+        :class:`~python:type`\ s to that should not be interpreted as
+        :class:`~python:collections.Iterable`\ s to be flattened.
+        Default is (:class:`~python:str`, :class:`~python:bytes`) to
+        prevent strings and bytes from being interpreted as iterables
+        and expanded as individual characters.
+
+    Yields
+    ------
+    sequence
+        single sequence with no nesting
+
+    Examples
+    --------
+    >>> items = [1, 2, [3, 4, [5, 6], 7], 8]
+    >>> from sknano.core import flatten
+    >>> items = list(flatten(items))
+    >>> print(items)
+    [1, 2, 3, 4, 5, 6, 7, 8]
+
+    """
     for x in items:
         if isinstance(x, Iterable) and not isinstance(x, ignore_types):
             yield from flatten(x)
@@ -115,7 +235,6 @@ def flatten(items, ignore_types=(str, bytes)):
 def repeatfunc(func, times=None, *args):
     """Repeat calls to func with specified arguments.
 
-    Example:  repeatfunc(random.random)
     """
     if times is None:
         return starmap(func, repeat(args))
@@ -123,22 +242,49 @@ def repeatfunc(func, times=None, *args):
 
 
 def pairwise(iterable):
-    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    """Returns an iterator of paired items, overlapping, from the original.
+
+    Examples
+    --------
+    >>> from sknano.core import pairwise, tabulate, take
+    >>> t = tabulate(lambda i: i)
+    >>> take(5, pairwise(t))
+    [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5)]
+    >>> take(5, pairwise(t))
+    [(6, 7), (7, 8), (8, 9), (9, 10), (10, 11)]
+
+    """
     a, b = tee(iterable)
     next(b, None)
     return zip(a, b)
 
 
 def grouper(iterable, n, fillvalue=None):
-    "Collect data into fixed-length chunks or blocks"
-    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+    """Collect data into fixed-length chunks or blocks.
+
+    Examples
+    --------
+    >>> from sknano.core import grouper
+    >>> list(grouper('ABCDEFG', 3, '?'))
+    [('A', 'B', 'C'), ('D', 'E', 'F'), ('G', '?', '?')]
+
+    """
     args = [iter(iterable)] * n
     return zip_longest(*args, fillvalue=fillvalue)
 
 
 def roundrobin(*iterables):
-    "roundrobin('ABC', 'D', 'EF') --> A D E B F C"
-    # Recipe credited to George Sakkis
+    """Yields an item from each iterable, alternating between them.
+
+    Recipe credited to George Sakkis
+
+    Examples
+    --------
+    >>> from sknano.core import roundrobin
+    >>> list(roundrobin('ABC', 'D', 'EF'))
+    ['A', 'D', 'E', 'B', 'F', 'C']
+
+    """
     pending = len(iterables)
     nexts = cycle(iter(it).__next__ for it in iterables)
     while pending:
@@ -151,28 +297,67 @@ def roundrobin(*iterables):
 
 
 def partition(pred, iterable):
-    "Use a predicate to partition entries into false entries and true entries"
-    # partition(is_odd, range(10)) --> 0 2 4 6 8   and  1 3 5 7 9
+    """Use a predicate to partition entries into false entries and true \
+    entries.
+
+    Parameters
+    ----------
+    pred : callable
+        callable function taking one argument and returning
+        :class:`~python:True` or :class:`~python:False`.
+    iterable : :class:`~python:collections.Iterable`
+
+    Returns
+    -------
+    :class:`~python:tuple` of :class:`~pyton:collections.Iterables`
+
+    Examples
+    --------
+    >>> from sknano.core import partition
+    >>> is_odd = lambda x: x % 2 != 0
+    >>> p = partition(is_odd, range(10))
+    >>> [print(i) for i in p]
+    [[0, 2, 4, 6, 8], [1, 3, 5, 7, 9]]
+
+    """
     t1, t2 = tee(iterable)
     return filterfalse(pred, t1), filter(pred, t2)
 
 
 def powerset(iterable):
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    """Yields all possible subsets of the iterable.
+
+    Examples
+    --------
+    >>> from sknano.core import powerset
+    >>> list(powerset([1, 2, 3]))
+    [(), (1,), (2,), (3,), (1, 2), (1, 3), (2, 3), (1, 2, 3)]
+
+    """
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
 
 
-def unique_elements(iterable, key=None):
-    """Return generator of unique elements in `iterable`, preserving order.
+def unique_everseen(iterable, key=None):
+    """Yields unique elements in `iterable`, preserving order, remembering \
+        all elements ever seen.
+
+    Parameters
+    ----------
+    iterable : :class:`~python:collections.Iterable`
+    key : {None, callable}, optional
+
+    Yields
+    ------
+    element in iterable
 
     Examples
     --------
-    >>> list(unique_elements('AAAABBBCCDAABBB'))
+    >>> list(unique_everseen('AAAABBBCCDAABBB'))
     ['A', 'B', 'C', 'D']
-    >>> list(unique_elements('ABBCcAD'))
+    >>> list(unique_everseen('ABBCcAD'))
     ['A', 'B', 'C', 'c', 'D']
-    >>> list(unique_elements('ABBCcAD', str.lower))
+    >>> list(unique_everseen('ABBCcAD', str.lower))
     ['A', 'B', 'C', 'D']
 
     """
@@ -190,6 +375,35 @@ def unique_elements(iterable, key=None):
                 yield element
 
 
+def unique_elements(iterable, key=None):
+    """Alias for :func:`~sknano.core.unique_everseen`."""
+    return unique_everseen(iterable, key)
+
+
+def unique_justseen(iterable, key=None):
+    """Yields unique elements in `iterable`, preserving order, remembering \
+        only the element just seen.
+
+    Parameters
+    ----------
+    iterable : :class:`~python:collections.Iterable`
+    key : {None, callable}, optional
+
+    Yields
+    ------
+    element in iterable
+
+    Examples
+    --------
+    >>> list(unique_justseen('AAAABBBCCDAABBB'))
+    ['A', 'B', 'C', 'D', 'A', 'B']
+    >>> list(unique_justseen('ABBCcAD', str.lower))
+    ['A', 'B', 'C', 'A', 'D']
+
+    """
+    return map(next, map(itemgetter(1), groupby(iterable, key)))
+
+
 def iter_except(func, exception, first=None):
     """Call a function repeatedly until an exception is raised.
 
@@ -200,12 +414,18 @@ def iter_except(func, exception, first=None):
     Parameters
     ----------
     func : `callable`
-    exception : `Exception`
+    exception : :class:`~python:Exception`
     first : `callable`
         for database APIs needing an initial cast to db.first()
 
     Examples
     --------
+
+    >>> from sknano.core import iter_except
+    >>> list(iter_except(list(range(3)).pop, IndexError))
+    [2, 1, 0]
+    >>>
+
     priority queue iterator:
 
     >>> # iter_except(functools.partial(heappop, h), IndexError)
@@ -252,11 +472,16 @@ def first_true(iterable, default=False, pred=None):
 
     Examples
     --------
-    >>> # first_true([a,b,c], x)
-    >>> # a or b or c or x
-    >>> # first_true([a,b], x, f)
-    >>> # a if f(a) else b if f(b) else x
-
+    >>> from sknano.core import first_true
+    >>> first_true([(), None, '', 0, 1])
+    1
+    >>> first_true([(), None, '', 0], 'donkey')
+    'donkey'
+    >>> is_odd = lambda x: x % 2 != 0
+    >>> first_true([2, 4, 6, 8, 11], default='donkey', pred=is_odd)
+    11
+    >>> first_true([2, 4, 6, 8], default='donkey', pred=is_odd)
+    'donkey'
     """
     return next(filter(pred, iterable), default)
 
--- sknano/core/tests/test_itertools.py
+++ sknano/core/tests/test_itertools.py
@@ -2,16 +2,146 @@
 from __future__ import absolute_import, division, print_function
 from __future__ import unicode_literals
 
-import nose
-from nose.tools import *
+from collections import OrderedDict
 
-from sknano.core import cyclic_pairs
+import nose
+from nose.tools import assert_equal
+
+from sknano.core import cyclic_pairs, take, tabulate, consume, nth, \
+    quantify, padnone, ncycles, dotproduct, flatten, repeatfunc, pairwise, \
+    grouper, roundrobin, partition, powerset, unique_everseen, \
+    unique_justseen, iter_except, first_true, random_product, \
+    random_permutation, random_combination, \
+    random_combination_with_replacement, ordinal_form
 
 
 def test1():
-    lst = ['a', 'b', 'c']
-    assert_equal(list(cyclic_pairs(lst)),
+    items = ['a', 'b', 'c']
+    assert_equal(list(cyclic_pairs(items)),
                  [('a', 'b'), ('b', 'c'), ('c', 'a')])
+
+    items = 'abc'
+    assert_equal(list(cyclic_pairs(items)),
+                 [('a', 'b'), ('b', 'c'), ('c', 'a')])
+
+
+def test2():
+    items = iter(range(1000))
+    assert_equal(take(10, items), list(range(10)))
+    assert_equal(take(10, items), list(range(10, 20)))
+
+
+def test3():
+    assert_equal(take(5, tabulate(ordinal_form)),
+                 [ordinal_form(i) for i in range(5)])
+    assert_equal(take(5, tabulate(ordinal_form, 10)),
+                 [ordinal_form(i) for i in range(10, 15)])
+
+
+def test4():
+    i = (x for x in range(10))
+    assert_equal(next(i), 0)
+    consume(i, 3)
+    assert_equal(next(i), 4)
+
+
+def test5():
+    t = tabulate(lambda i: i)
+    assert_equal(take(5, pairwise(t)),
+                 [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5)])
+    assert_equal(take(5, pairwise(t)),
+                 [(6, 7), (7, 8), (8, 9), (9, 10), (10, 11)])
+    assert_equal(take(5, pairwise(t)),
+                 [(12, 13), (13, 14), (14, 15), (15, 16), (16, 17)])
+
+
+def test6():
+    assert_equal(dotproduct(3 * [1], 3 * [1]), 3)
+
+
+def test7():
+    assert_equal(list(repeatfunc(lambda: 10, 3)), 3 * [10])
+    assert_equal(list(repeatfunc(lambda x: x ** 2, 3, 2)), 3 * [4])
+
+
+def test8():
+    assert_equal(list(ncycles("ABC", 3)),
+                 ['A', 'B', 'C', 'A', 'B', 'C', 'A', 'B', 'C'])
+
+
+def test9():
+    items = [1, 2, 3, [4, 5, [6, 7], 8], 9]
+    assert_equal(list(flatten(items)), list(range(1, 10)))
+
+
+def test10():
+    assert_equal(list(unique_everseen('AAABBBCCDAABBB')),
+                 ['A', 'B', 'C', 'D'])
+    assert_equal(list(unique_everseen('AAABBCcCAD')),
+                 ['A', 'B', 'C', 'c', 'D'])
+    assert_equal(list(unique_everseen('AAABBCcCAD', str.lower)),
+                 ['A', 'B', 'C', 'D'])
+
+
+def test11():
+    assert_equal(list(unique_justseen('AAABBBCCDAABBB')),
+                 ['A', 'B', 'C', 'D', 'A', 'B'])
+    assert_equal(list(unique_justseen('AAABBCcCAD', str.lower)),
+                 ['A', 'B', 'C', 'A', 'D'])
+
+
+def test12():
+    is_odd = lambda x: x % 2 != 0
+    p = partition(is_odd, range(10))
+    assert_equal([list(i) for i in p],
+                 [list(range(0, 10, 2)), list(range(1, 10, 2))])
+
+
+def test13():
+    assert_equal(list(powerset([1, 2, 3])),
+                 [(), (1,), (2,), (3,), (1, 2), (1, 3), (2, 3), (1, 2, 3)])
+
+
+def test14():
+    assert_equal(list(roundrobin('ABC', 'D', 'EF')),
+                 ['A', 'D', 'E', 'B', 'F', 'C'])
+
+
+def test15():
+    assert_equal(take(5, padnone(range(3))),
+                 [0, 1, 2, None, None])
+
+
+def test16():
+    assert_equal(quantify([True, False, True]), 2)
+
+
+def test17():
+    assert_equal(nth(range(10), 3), 3)
+    assert_equal(nth(range(10), 20, 'donkey'), 'donkey')
+
+
+def test18():
+    assert_equal(list(grouper('ABCDEFG', 3, '?')),
+                 [('A', 'B', 'C'), ('D', 'E', 'F'), ('G', '?', '?')])
+
+
+def test19():
+    assert_equal(list(iter_except(list(range(3)).pop, IndexError)),
+                 [2, 1, 0])
+
+    d = OrderedDict([('a', 5), ('b', 2), ('c', 3)])
+    assert_equal(list(iter_except(d.popitem, KeyError)),
+                 [('c', 3), ('b', 2), ('a', 5)])
+
+
+def test20():
+    assert_equal(first_true([(), None, '', 0, 1]), 1)
+    assert_equal(first_true([(), None, '', 0], 'donkey'), 'donkey')
+    is_odd = lambda x: x % 2 != 0
+    assert_equal(first_true([2, 4, 6, 8, 11], pred=is_odd), 11)
+    assert_equal(first_true([2, 4, 6, 8], default='donkey', pred=is_odd),
+                 'donkey')
 
 
 if __name__ == '__main__':
