bb15eb7945a7f58c4851c71375fc73ef57d10fe6
Andrew Merrill <androomerrill@gmail.com>
2016-02-01 13:41:37 -0500

    updating sknano script

--- sknano/scripts/sknano.py
+++ sknano/scripts/sknano.py
@@ -24,11 +24,12 @@ from __future__ import absolute_import, division, print_function, \
     unicode_literals
 __docformat__ = 'restructuredtext en'
 
-import argparse
+# import argparse
 # import importlib
 import sys
 
-from ._parser import add_default_arguments
+# from ._parser import add_default_arguments
+from .nanogen import nanogen_parser, nanogen
 
 __all__ = ['sknano', 'sknano_parser']
 
@@ -36,18 +37,20 @@ __all__ = ['sknano', 'sknano_parser']
 def sknano_parser():
     """:mod:`~sknano.scripts.sknano` script \
         :class:`~python:argparse.ArgumentParser`."""
-    parser = argparse.ArgumentParser()
-    parser = add_default_arguments(parser)
+    return nanogen_parser()
 
 
 def sknano(**kwargs):
     """:mod:`~sknano.scripts.sknano` script function."""
-    pass
+    nanogen(**kwargs)
 
 
 def main():
     args = sknano_parser().parse_args()
-    sknano(**vars(args))
+    if hasattr(args, 'generator_class'):
+        sknano(**vars(args))
+    else:
+        sknano_parser().print_help()
 
 
 if __name__ == '__main__':
