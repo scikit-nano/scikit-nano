55a0dae868d79dad785a2cdb872aa1e246f25c2f
Andrew Merrill <androomerrill@gmail.com>
2016-02-18 22:09:38 -0500

    updating `analyze_network` method

--- sknano/core/atoms/mixins/_ring_atoms.py
+++ sknano/core/atoms/mixins/_ring_atoms.py
@@ -11,11 +11,16 @@ from __future__ import absolute_import, division, print_function
 from __future__ import unicode_literals
 __docformat__ = 'restructuredtext en'
 
-from collections import Counter, OrderedDict, deque
+from collections import Counter, OrderedDict
 
 import numpy as np
 
+from sknano.core import timethis
 # from sknano.core.math import Vector
+
+# from sknano.core.analysis import _ring_finder
+from sknano.core.analysis import find_rings
+
 
 __all__ = ['RingAtomMixin', 'RingAtomsMixin']
 
@@ -48,7 +53,6 @@ class RingAtomsMixin:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.ring_counter = Counter()
-        # self.rings = []
         self.rings = OrderedDict()
 
     @property
@@ -79,92 +83,64 @@ class RingAtomsMixin:
         rings[n].append(self.__class__([self[abs(nidx)] for nidx in nodes],
                                        update_item_class=False, **self.kwargs))
 
-    def analyze_network(self, cutoff=np.inf, maxlength=None, TOL=0.0001,
-                        retcodes=None):
-        """Find the shortest-path rings.
+    def _update_ring_counter(self, ring_counts):
+        cntr = self.ring_counter
+        for i, counts in enumerate(ring_counts):
+            if counts > 0:
+                cntr[i] = counts
+
+    def _update_rings(self, nodes_list):
+        for nodes in nodes_list:
+            self.add_ring(nodes)
+
+    def reset_attrs(self):
+        """Reset the :class:`~RingAtomsMixin` class attributes then call \
+            super class method."""
+        self.reset_ring_atoms_attrs()
+        super().reset_attrs()
+
+    def reset_ring_atoms_attrs(self):
+        """Reset the :class:`~RingAtomsMixin` class attributes."""
+        self.ring_counter = Counter()
+        self.rings = OrderedDict()
+        [setattr(atom, 'Rn', 0) for atom in self]
+
+    @timethis
+    def analyze_network(self, cutoff=np.inf, max_ring_size=None, eps=0.0001,
+                        retcodes=None, pyversion=False):
+        """Analyze the network connectivity.
 
         Parameters
         ----------
         cutoff : :class:`~python:float`, optional
-        maxlength : :class:`~python:int`, optional
-        TOL : :class:`~python:float`, optional
+        max_ring_size : :class:`~python:int`, optional
+        eps : :class:`~python:float`, optional
         retcodes : :class:`~python:tuple`, optional
 
         """
-        if maxlength is None:
-            maxlength = -1
+        if max_ring_size is None:
+            max_ring_size = -1
+        self.reset_ring_atoms_attrs()
+        if self.verbose:
+            print('Analyzing the network connectivity...')
+
+        if not self.neighbors_analyzed:
+            self.update_neighbors(cutoffs=[cutoff])
 
         self.update_neighbor_lists()
+        Natoms = self.Natoms
+        NNN = self.NNN
         nn_idx = self.nn_idx
-        seed = self.nn_seed_list
-        r_nn = self.nn_vectors
-        visited = np.zeros(self.NNN, dtype=bool)
-        amat = self.nn_adjacency_matrix
-        cntr = self.ring_counter
+        nn_seed = self.nn_seed
+        nn_amat = self.nn_adjacency_matrix
+        nn_vecs = self.nn_vectors
 
-        def search_paths(root=None, node=None, d=None):
-            """Search for closed paths starting from `root` atom.
+        ring_counts, nodes_list = \
+            find_rings(Natoms, NNN, nn_idx, nn_seed, nn_amat, nn_vecs,
+                       max_ring_size, eps, pyversion)
 
-            Parameters
-            ----------
-            root, node : :class:`~python:int`
-            d : array_like
-
-            """
-            q = deque([(root, [node], d)])
-            while q:
-                prev, nodes, d = q.popleft()
-                node = nodes[-1]
-                if node > 0:
-                    i = node
-                    for si in range(seed[i], seed[i+1]):
-                        ni = nn_idx[si]
-                        if not visited[si] and ni != prev:
-                            di = amat[root][i]
-                            dn = amat[root][ni]
-                            if ((dn == di + 1) and
-                                (len(nodes) < (maxlength - 1) / 2 or
-                                 maxlength < 0)):
-                                q.append((node, nodes + [ni], d + r_nn[si]))
-                            elif ((dn == di) or (dn == di - 1)):
-                                q.append((node, nodes + [-ni], d + r_nn[si]))
-                else:
-                    i = -node
-                    for si in range(seed[i], seed[i + 1]):
-                        ni = nn_idx[si]
-                        if not visited[si] and ni != prev:
-                            di = amat[root][i]
-                            dn = amat[root][ni]
-                            if ni == root:
-                                dr = r_nn[si] + d
-                                if dr.norm < TOL:
-                                    is_sp = True
-                                    nodes.append(root)
-                                    ring_size = len(nodes)
-                                    for n in range(ring_size):
-                                        for m in range(n + 1, ring_size):
-                                            s = m - n
-                                            if s > ring_size / 2:
-                                                s = ring_size - s
-                                            if amat[abs(nodes[m])][
-                                                    abs(nodes[n])] != s:
-                                                is_sp = False
-                                    if is_sp:
-                                        self.add_ring(nodes)
-                                        cntr[ring_size] += 1
-                            elif dn == di - 1:
-                                q.append((node, nodes + [-ni], d + r_nn[si]))
-
-        for i, atom in enumerate(self):
-            for si in range(seed[i], seed[i+1]):
-                ni = nn_idx[si]
-                if i < ni:
-                    visited[si] = True
-                    for nsi in range(seed[ni], seed[ni+1]):
-                        nni = nn_idx[nsi]
-                        if nni == i:
-                            visited[nsi] = True
-                    search_paths(root=i, node=ni, d=r_nn[si])
+        self._update_ring_counter(ring_counts)
+        self._update_rings(nodes_list)
 
         if retcodes is not None and isinstance(retcodes, (tuple, list)):
             return tuple([getattr(self, attr, None) for attr in retcodes])
