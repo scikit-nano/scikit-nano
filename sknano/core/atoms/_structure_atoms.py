31de0edd34abf1d0ad7481cb21e1f6bebc9d0d82
Andrew Merrill <androomerrill@gmail.com>
2016-02-21 14:20:33 -0500

    Removing the `CNAtom/CNAtoms` classes from the MRO.
    Also moving the `VelocityAtom/VelocityAtoms` classes to the
    `MDAtom/MDAtoms` parent classes.

--- sknano/core/atoms/_structure_atoms.py
+++ sknano/core/atoms/_structure_atoms.py
@@ -14,7 +14,7 @@ __docformat__ = 'restructuredtext en'
 
 # from operator import attrgetter
 
-from ._cn_atoms import CNAtom, CNAtoms
+# from ._cn_atoms import CNAtom, CNAtoms
 from ._id_atoms import IDAtom, IDAtoms
 from ._charged_atoms import ChargedAtom, ChargedAtoms
 from ._image_atoms import ImageAtom, ImageAtoms
@@ -22,33 +22,26 @@ from ._type_atoms import TypeAtom, TypeAtoms
 from ._neighbor_atoms import NeighborAtom, NeighborAtoms
 from ._lattice_atoms import LatticeAtom, LatticeAtoms
 from ._xyz_atoms import XYZAtom, XYZAtoms
-from ._velocity_atoms import VelocityAtom, VelocityAtoms
-from ._topology import AtomTopologyMixin, AtomsTopologyMixin
+# from ._velocity_atoms import VelocityAtom, VelocityAtoms
 from .mixins import AtomAdapterMixin, AtomsAdapterMixin, \
-    POAVAtomMixin, POAVAtomsMixin, RingAtomMixin, RingAtomsMixin
+    AtomTopologyMixin, AtomsTopologyMixin, POAVAtomMixin, POAVAtomsMixin, \
+    RingAtomMixin, RingAtomsMixin
 
 __all__ = ['StructureAtom', 'StructureAtoms']
 
 
 class StructureAtom(AtomAdapterMixin, POAVAtomMixin, RingAtomMixin,
-                    AtomTopologyMixin, NeighborAtom,
-                    CNAtom, VelocityAtom, ImageAtom, LatticeAtom,
+                    AtomTopologyMixin, NeighborAtom, ImageAtom, LatticeAtom,
                     XYZAtom, ChargedAtom, TypeAtom, IDAtom):
     """An `Atom` class for structure analysis."""
     pass
 
 
 class StructureAtoms(AtomsAdapterMixin, POAVAtomsMixin, RingAtomsMixin,
-                     AtomsTopologyMixin, NeighborAtoms,
-                     CNAtoms, VelocityAtoms, ImageAtoms,
+                     AtomsTopologyMixin, NeighborAtoms, ImageAtoms,
                      LatticeAtoms, XYZAtoms, ChargedAtoms, TypeAtoms, IDAtoms):
     """An `Atoms` sub-class for structure analysis."""
 
     @property
     def __atom_class__(self):
         return StructureAtom
-
-    # def sort(self, key=attrgetter('CN', 'v', 'i', 'r', 'q', 'type', 'mol',
-    #                               'id', 'mass', 'Z', 'element'),
-    #          reverse=False):
-    #     super().sort(key=key, reverse=reverse)
