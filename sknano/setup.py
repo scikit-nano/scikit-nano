a918d547fd709500c400a33fc14348016fea098d
Andrew Merrill <androomerrill@gmail.com>
2016-03-01 10:31:21 -0500

    updating setup.py configuration

--- sknano/setup.py
+++ sknano/setup.py
@@ -14,7 +14,7 @@ def configuration(parent_package='', top_path=None):
     config.add_subpackage('testing')
     config.add_subpackage('utils')
     config.add_data_dir('data')
-    #config.make_config_py()
+    config.make_config_py()
     return config
 
 if __name__ == '__main__':
