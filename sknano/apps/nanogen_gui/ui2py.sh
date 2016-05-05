5ebf223e298c3af00ff137a609853f04c5267686
Andrew Merrill <androomerrill@gmail.com>
2016-05-05 10:35:20 -0400

    updating ui2py script

--- sknano/apps/nanogen_gui/ui2py.sh
+++ sknano/apps/nanogen_gui/ui2py.sh
@@ -5,7 +5,7 @@ pyuic4 swnt_generator.ui > _pyqt4_ui_swnt_generator.py
 pyuic4 mwnt_generator.ui > _pyqt4_ui_mwnt_generator.py
 pyuic4 graphene_generator.ui > _pyqt4_ui_graphene_generator.py
 pyuic4 fullerene_generator.ui > _pyqt4_ui_fullerene_generator.py
-pyuic4 bulk_structure_generator.ui > _pyqt4_ui_bulk_structure_generator.py
+pyuic4 xtal_structure_generator.ui > _pyqt4_ui_xtal_structure_generator.py
 pyuic4 mwnt_Ch_list_item_dialog.ui > _pyqt4_ui_mwnt_Ch_list_item_dialog.py
 
 pyuic5 nanogen.ui > _pyqt5_ui_nanogen.py
@@ -13,5 +13,5 @@ pyuic5 swnt_generator.ui > _pyqt5_ui_swnt_generator.py
 pyuic5 mwnt_generator.ui > _pyqt5_ui_mwnt_generator.py
 pyuic5 graphene_generator.ui > _pyqt5_ui_graphene_generator.py
 pyuic5 fullerene_generator.ui > _pyqt5_ui_fullerene_generator.py
-pyuic5 bulk_structure_generator.ui > _pyqt5_ui_bulk_structure_generator.py
+pyuic5 xtal_structure_generator.ui > _pyqt5_ui_xtal_structure_generator.py
 pyuic5 mwnt_Ch_list_item_dialog.ui > _pyqt5_ui_mwnt_Ch_list_item_dialog.py
