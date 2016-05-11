025f9a8fb2dbbbe262c4eaea734bc4fdde9ba8a9
Andrew Merrill <androomerrill@gmail.com>
2016-05-11 07:23:02 -0400

    updating __init__ module imports

--- sknano/testing/__init__.py
+++ sknano/testing/__init__.py
@@ -4,7 +4,5 @@ from __future__ import absolute_import, division, print_function, \
 __docformat__ = 'restructuredtext en'
 
 from .funcs import generate_atoms, generate_structure
-from .timer import Timer
-from .tools import AtomsTestFixture, GeneratorTestFixture, IOTestFixture, \
-    TempfileTestFixture, DUMPTestFixture, GeometricRegionsTestFixture, \
-    Geometric2DRegionsTestFixture, Geometric3DRegionsTestFixture
+from .timer import *
+from .tools import *
