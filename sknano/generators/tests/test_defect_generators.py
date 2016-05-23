ea8ca111b7814d559efe17e74e85b232e2425bfc
Andrew Merrill <androomerrill@gmail.com>
2016-05-23 09:37:56 -0400

    updating unit tests

--- sknano/generators/tests/test_defect_generators.py
+++ sknano/generators/tests/test_defect_generators.py
@@ -6,11 +6,11 @@ from __future__ import unicode_literals
 from pkg_resources import resource_filename
 
 import nose
-from nose.tools import *
+from nose.tools import assert_raises, assert_true, assert_equal
 
 import numpy as np
 
-from sknano.generators import CrossLinkedDefectGenerator
+from sknano.generators import CrossLinkedDefectGenerator, VacancyGenerator
 from sknano.io import XYZReader
 from sknano.testing import GeneratorTestFixture, IOTestFixture
 
@@ -20,7 +20,6 @@ class Tests(GeneratorTestFixture, IOTestFixture):
     def test1(self):
         generator = CrossLinkedDefectGenerator()
         assert_true(isinstance(generator, CrossLinkedDefectGenerator))
-        assert_true(generator.structure is None)
         print(generator)
 
     def test2(self):
@@ -36,6 +35,22 @@ class Tests(GeneratorTestFixture, IOTestFixture):
         assert_true(isinstance(generator, CrossLinkedDefectGenerator))
         assert_equal(generator.structure, structure)
         print(generator)
+
+    def test4(self):
+        structure = self.swnt.structure
+        with assert_raises(TypeError):
+            VacancyGenerator(structure)
+
+    def test5(self):
+        structure = self.swnt.structure
+        generator = VacancyGenerator(structure, Nsites=5)
+        assert_equal(generator.Nsites, 5)
+
+    def test6(self):
+        structure = self.swnt.structure
+        generator = VacancyGenerator(structure, Nsites=5, Nvacs_per_site=3)
+        assert_equal(generator.Nsites, 5)
+        assert_equal(generator.Nvacs_per_site, 3)
 
 
 if __name__ == '__main__':
