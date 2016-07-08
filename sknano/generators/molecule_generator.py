1f29bcf531fbb54adc83cf44a1681db0acc541ae
Andrew Merrill <androomerrill@gmail.com>
2016-07-08 09:03:33 -0400

    adding molecule generator for future dev

---
+++ sknano/generators/molecule_generator.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+=================================================================================
+Molecule generators (:mod:`sknano.generators.molecule_generator`)
+=================================================================================
+
+.. currentmodule:: sknano.generators.molecule_generator
+
+.. todo::
+
+   Add methods to perform fractional translation and cartesian translation
+   before structure generation.
+
+.. todo::
+
+   Handle different units in output coordinates.
+
+"""
+from __future__ import absolute_import, division, print_function, \
+    unicode_literals
+
+__docformat__ = 'restructuredtext en'
+
+# from sknano.core import pluralize
+# from sknano.core.math import Point, Vector
+# from sknano.core.structures import Molecule
+# from .base import CrystalStructureGenerator
+
+__all__ = ['MoleculeGenerator']
+
+
+class MoleculeGenerator:
+    pass
